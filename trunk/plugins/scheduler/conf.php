7a2b0fedc8c6865d39994fd6e9e65265a2abd184
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-07-13 11:03:50 +0000

    minor comment change

--- trunk/plugins/scheduler/conf.php
+++ trunk/plugins/scheduler/conf.php
@@ -6,7 +6,7 @@
 
 	$isAutoStart = true;	// if false, then you need start plugin mannualy.
 				// For do this, add to rtorrent conf file something like this:
-				// schedule = trafic,10,00:15:00,"execute={sh,-c,php full_path_to_update.php& exit 0}"
+				// schedule = scheduler,10,00:15:00,"execute={sh,-c,php full_path_to_update.php& exit 0}"
 
 	$updateInterval = 60;	// in minutes, 1-6,10,12,15,20,30 or 60
 
