0ed14ab88098f259885665d01aed0fe19fb14f24
novik65 <Novik@users.noreply.github.com>
2009-07-27 12:21:17 +0000

    minor plugin fix

--- plugins/rss/init.php
+++ plugins/rss/init.php
@@ -30,7 +30,9 @@ if($isAutoStart)
 		0,$tm["mon"],$tm["mday"],$tm["year"])-$tm[0];
 	$interval = $updateInterval*60;
 	if(!$pathToPHP || ($pathToPHP==""))
-		$pathToPHP = "php";
+		$php = "php";
+	else
+		$php = $pathToPHP;
 	send2RPC('<?xml version="1.0" encoding="UTF-8"?>'.
 		'<methodCall>'.
 		'<methodName>schedule</methodName>'.
@@ -38,7 +40,7 @@ if($isAutoStart)
 		'<param><value><string>rss</string></value></param>'.
 		'<param><value><string>'.$startAt.'</string></value></param>'.
 		'<param><value><string>'.$interval.'</string></value></param>'.
-		'<param><value><string>execute={sh,-c,'.$pathToPHP.' '.$theSettings->path.'plugins/rss/update.php'.'&amp; exit 0}</string></value></param>'.
+		'<param><value><string>execute={sh,-c,'.$php.' '.$theSettings->path.'plugins/rss/update.php'.'&amp; exit 0}</string></value></param>'.
 		'</params>'.
 		'</methodCall>');
 	$dir = getcwd();
