a333adbde8915b831fde11e33ef8e75f5d458840
novik65 <Novik@users.noreply.github.com>
2009-08-11 11:01:08 +0000

    Tagging the 1.0 version of plugin scheduler.

---
+++ scheduler/1.0/lang/ru.js
@@ -0,0 +1,25 @@
+﻿
+
+ WUILang.scheduler			= "Расписание";
+ WUILang.schedulerOn			= "Включить расписание";
+ WUILang.schedulerGraph			= "График расписания";
+ WUILang.schShortWeek			= [ "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс" ];
+ WUILang.schFullWeek			= [ "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" ];
+
+ WUILang.schUnlimited			= "Без ограничений";
+ WUILang.schLimited			= "Ограничение";
+ WUILang.schTurnOff			= "Стоп";
+ WUILang.schSeedingOnly			= "Только отдача";
+
+ WUILang.schUnlimitedDesc		= "Без ограничений - никаких ограничений нет";
+ WUILang.schLimitedDesc			= "Ограничение - использовать запланированное ограничение скорости";
+ WUILang.schTurnOffDesc			= "Стоп - останавливать все торренты";
+ WUILang.schSeedingOnlyDesc		= "Только отдача - только сидировать";
+
+ WUILang.schLimitedUL			= "Ограничение раздачи";
+ WUILang.schLimitedDL			= "Ограничение закачки";
+ 
+ WUILang.schedPHPNotFound	= "Плагин scheduler: пользователю rTorrent не доступен интерпретатор php. Плагин не будет работать.";
+ WUILang.schedUpdaterNotAvailable= "Плагин scheduler: rTorrent должен иметь доступ на чтение к файлу plugins/scheduler/update.php. Плагин не будет работать.";
+
+ utWebUI.schedulerCreate();
