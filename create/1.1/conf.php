b33aeb907498b1903e9b2ac94d66415774018253
novik65 <Novik@users.noreply.github.com>
2009-08-11 10:56:39 +0000

    Tagging the 1.1 version of plugin create.

---
+++ create/1.1/conf.php
@@ -0,0 +1,8 @@
+<?php
+	// configuration parameters
+
+	$useExternal = false;           // If true, will be used external program - createtorrent. 
+					// You can download it from http://www.createtorrent.com/
+	$pathToCreatetorrent = '';	// Something like /bin/createtorrent. If empty, will be founded in PATH.
+
+?>
