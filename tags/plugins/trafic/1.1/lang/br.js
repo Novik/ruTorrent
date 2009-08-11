e463dfa6961a4ee1ac76b3a647ee6bf36709e326
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-08-11 11:03:36 +0000

    Tagging the 1.1 version of plugin trafic.

---
+++ tags/plugins/trafic/1.1/lang/br.js
@@ -0,0 +1,12 @@
+﻿
+ 
+ WUILang.traf 		= "Traffic";
+ WUILang.perDay		= "Per day";
+ WUILang.perMonth 	= "Per month";
+ WUILang.perYear 	= "Per year";
+ WUILang.allTrackers	= "All trackers";
+ WUILang.ClearButton	= "Clear";
+ WUILang.ClearQuest 	= "Do you really want to clear statistics for the selected tracker(s)?";
+ WUILang.trafPHPNotFound	= "Traffic plugin: rTorrent's user can't access php interpreter. Plugin will not work.";
+ WUILang.trafStatsNotAvailable	= "Traffic plugin: rTorrent's user can't access directories plugins/trafic/stats and plugins/trafic/stats/trackers for read/write/execute. Plugin will not work.";
+ WUILang.trafUpdaterNotAvailable= "Traffic plugin: rTorrent's user can't access file plugins/trafic/update.php for read. Plugin will not work.";
