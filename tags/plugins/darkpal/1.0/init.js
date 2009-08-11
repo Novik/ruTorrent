27fc29b2ccc742e805f6224fd8c608b4124ed06f
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-08-11 10:57:13 +0000

    Tagging the 1.0 version of plugin darkpal.

---
+++ tags/plugins/darkpal/1.0/init.js
@@ -0,0 +1,10 @@
+var setNewPalette = function()
+{
+	var plugin = new rPlugin("darkpal");
+	plugin.loadCSS("style");
+	plugin.loadCSS("stable");
+	utWebUI.trtTable.setPaletteByURL("plugins/darkpal");
+	utWebUI.flsTable.setPaletteByURL("plugins/darkpal");
+	utWebUI.trkTable.setPaletteByURL("plugins/darkpal");
+	utWebUI.prsTable.setPaletteByURL("plugins/darkpal");
+} .apply(this);
