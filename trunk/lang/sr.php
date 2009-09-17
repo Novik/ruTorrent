f7b5ee2369a7d2d2d3b0d761b713d623fe1b9f13
zcsala021 <zcsala021@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-09-17 20:54:56 +0000

    Initial import

---
+++ trunk/lang/sr.php
@@ -0,0 +1,28 @@
+<?php
+
+$Countries = array (
+    "Australia" => "Аустралија",
+    "Austria" => "Аустрија",
+    "Bosnia and Herzegovina" => "Босна и Херцеговина",
+    "Botswana" => "Боцвана",
+    "Canada" => "Канада",
+    "Croatia" => "Хрватска",
+    "Finland" => "Финска",
+    "France" => "Француска",
+    "Germany" => "Немачка",
+    "Macedonia" => "Македонија",
+    "Montenegro" => "Црна Гора",
+    "Netherlands" => "Холандија",
+    "Norway" => "Норвешка",
+    "Romania" => "Румунија",
+    "Serbia" => "Србија",
+    "Slovakia" => "Словачка",
+    "Slovenia" => "Словенија",
+    "Sweden" => "Шведска",
+    "Switzerland" => "Швајцарска",
+    "United States" => "С.А.Д.",
+    "Vatican" => "Ватикан",
+    "Zimbabwe" => "Зимбабве"
+);
+
+?>
