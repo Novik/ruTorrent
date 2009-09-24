de53ef5eff849f564ffb1d92307dcdeae4b44f8f
novik65 <Novik@users.noreply.github.com>
2009-09-24 10:58:56 +0000

    seedingtime: initial import

---
+++ plugins/seedingtime/lang/fi.js
@@ -0,0 +1,6 @@
+﻿
+ 
+ WUILang.seedingTimeBadVersion	= "SeedingTime plugin: this version of rTorrent does't support this plugin. Plugin will not work.";
+ WUILang.seedingTime		= "SeedingTime";
+
+utWebUI.seedingTimeCreate();
