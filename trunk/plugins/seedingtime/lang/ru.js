c58ab49c2517c9b58d07bdc7f4e31195d410f26c
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-09-24 10:58:56 +0000

    seedingtime: initial import

---
+++ trunk/plugins/seedingtime/lang/ru.js
@@ -0,0 +1,6 @@
+﻿
+ 
+ WUILang.seedingTimeBadVersion	= "Плагин SeedingTime: для работы плагина необходима версия rTorrent >= 0.8.5. Плагин не будет работать.";
+ WUILang.seedingTime		= "Завершен";
+
+utWebUI.seedingTimeCreate();
