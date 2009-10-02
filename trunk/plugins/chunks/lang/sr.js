12d9732b4b9fb5d188d8b870e201ce238bfd0b27
zcsala021 <zcsala021@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-10-02 22:32:42 +0000

    chunks: initial import

---
+++ trunk/plugins/chunks/lang/sr.js
@@ -0,0 +1,7 @@
+
+WUILang.chunksNoChunksFound = "Нема парчади!";
+WUILang.chunksRows = "врста";
+WUILang.chunksColumns = "колона";
+WUILang.chunksChunks = "парчади";
+WUILang.chunksSize = "величина парчета";
+WUILang.chunksNotSupported = "Морате да имате rTorrent >= 0.8.5 да бисте видели како парчад напредују! Верзија вашег клијента је ";
