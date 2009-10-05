0f459f4cc93e4846ba7f451fae5a1db044dbc396
novik65 <Novik@users.noreply.github.com>
2009-10-05 11:49:11 +0000

    make tags

---
+++ throttle/1.2/lang/sr.js
@@ -0,0 +1,9 @@
+﻿
+ WUILang.throttleUnsupported    = "Додатак Throttle: this version of rTorrent does't support throttle. Plugin will not work.";
+ WUILang.throttles              = "Канали";
+ WUILang.throttle               = "Канал";
+ WUILang.mnuThrottle            = "Постави канал";
+ WUILang.mnuUnlimited           = "Нема канала";
+ WUILang.channelName            = "Име";
+
+utWebUI.throttleCreate();
