98b682655f6b57b6d432d224b50fcb300e1ec7d8
novik65 <Novik@users.noreply.github.com>
2009-10-05 11:47:30 +0000

    make tags

---
+++ ratio/1.0/lang/ru.js
@@ -0,0 +1,17 @@
+﻿
+ 
+ WUILang.ratioUnsupported	= "Плагин ratio: эта версия rTorrent не поддерживает индивидуальное управление ratio. Плагин не будет работать.";
+ WUILang.ratios			= "Группы ratio";
+ WUILang.ratio			= "ГруппаRatio";
+ WUILang.mnuRatio		= "Установить ratio";
+ WUILang.mnuRatioUnlimited	= "Нет";
+ WUILang.ratioName 		= "Имя";
+ WUILang.minRatio 		= "Min";
+ WUILang.maxRatio 		= "Max";
+ WUILang.ratioUpload 		= "UL";
+ WUILang.ratioAction 		= "Действие";
+ WUILang.ratioStop 		= "Стоп";
+ WUILang.ratioStopAndRemove 	= "Стоп & очистить";
+ WUILang.ratioErase 		= "Удалить";
+
+utWebUI.ratioCreate();
