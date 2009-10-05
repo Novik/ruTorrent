88076fb982a2d51c9536219583b6ae564195dcfb
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-10-05 11:48:06 +0000

    make tags

---
+++ tags/plugins/create/1.2/lang/ru.js
@@ -0,0 +1,15 @@
+﻿
+ 
+ WUILang.mnu_create 		= "Новый торрент...";
+ WUILang.CreateNewTorrent 	= "Новый торрент";
+ WUILang.SelectSource 		= "Выбор источника";
+ WUILang.TorrentProperties 	= "Настройка торрента";
+ WUILang.PieceSize 		= "Размер части";
+ WUILang.Other 			= "Другое";
+ WUILang.StartSeeding 		= "Начать раздачу";
+ WUILang.PrivateTorrent		= "Частный торрент";
+ WUILang.CreateAndSaveAs	= "Создать и сохранить в...";
+ WUILang.BadTorrentData		= "Вы должны заполнить обязательные поля.";
+ WUILang.createExternalNotFound = "Плагин create: веб-серверу не доступна внешняя программа";
+
+utWebUI.initCreate();
