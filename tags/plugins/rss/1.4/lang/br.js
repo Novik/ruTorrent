221e6eb92ceb1299f769c0fff6b030f7a8a404b9
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-10-05 11:52:32 +0000

    make tags

---
+++ tags/plugins/rss/1.4/lang/br.js
@@ -0,0 +1,42 @@
+﻿
+ 
+ WUILang.addRSS		= "Add RSS feed";
+ WUILang.feedURL	= "Feed URL";
+ WUILang.alias		= "Custom Alias";
+ WUILang.rssAuto	= "Automatically download items published in feed";
+ WUILang.allFeeds	= "All Feeds";
+ WUILang.incorrectURL	= "Please, specify correct URL feed."
+ WUILang.cantFetchRSS	= "Error loading feed.";
+ WUILang.rssAlreadyExist	= "Feed already exists.";
+ WUILang.rssDontExist		= "Feed doesn`t exist.";
+ WUILang.rssCantLoadTorrent	= "Error loading torrent.";
+ WUILang.rssStatus	= "RSS";
+ WUILang.rssStatusLoaded= "Already loaded";
+ WUILang.rssMenuLoad	= "Load";
+ WUILang.rssMenuOpen	= "Open in browser";
+ WUILang.rssMenuClearHistory	= "Clear history";
+ WUILang.rssMenuAddToFilter	= "Add to favorites";
+ WUILang.rssMenuManager	= "RSS manager";
+ WUILang.rssMenuRefresh	= "Update feed";
+ WUILang.rssMenuDisable	= "Disable feed";
+ WUILang.rssMenuEnable	= "Enable feed";
+ WUILang.rssMenuEdit	= "Edit feed";
+ WUILang.rssMenuDelete	= "Delete feed";
+ WUILang.rssDeletePrompt	= "Do you really want to delete selected feed?";
+ WUILang.rssNewFilter	= "New filter";
+ WUILang.rssFilter	= "Filter";
+ WUILang.rssAddFilter	= "Add";
+ WUILang.rssDelFilter	= "Delete";
+ WUILang.rssCheckFilter	= "?";
+ WUILang.rssFiltersLegend	= "Filter settings";
+ WUILang.rssIncorrectFilter	= "Filter is incorrect.";
+ WUILang.foundedByFilter	= "Matches";
+ WUILang.rssExclude	= "Exclude";
+ WUILang.rssStatusError	= "Load error";
+ WUILang.rssPHPNotFound	= "RSS plugin: rTorrent's user can't access php interpreter. Plugin will not work.";
+ WUILang.rssCurlNotFound= "RSS plugin: rTorrent's user can't access curl program. You will not be able to work with https feeds.";
+ WUILang.rssCurlNotFound1	= "RSS plugin: Web server user can't access curl program. You will not be able to work with https feeds.";
+ WUILang.rssCacheNotAvailable 	= "RSS plugin: rTorrent's user can't access directory plugins/rss/cache for read/write/execute. Plugin will not work.";
+ WUILang.rssUpdaterNotAvailable = "RSS plugin: rTorrent's user can't access file plugins/rss/update.php for read. Plugin will not work.";
+
+utWebUI.initRSS();
