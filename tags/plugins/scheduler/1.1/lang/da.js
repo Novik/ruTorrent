b00f1c766194bc063cc54fb7695846350f0ce951
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-10-05 11:48:41 +0000

    make tags

---
+++ tags/plugins/scheduler/1.1/lang/da.js
@@ -0,0 +1,24 @@
+﻿
+ WUILang.scheduler			= "Scheduler";
+ WUILang.schedulerOn			= "Enable Scheduler";
+ WUILang.schedulerGraph			= "Scheduler Table";
+ WUILang.schShortWeek			= [ "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" ];
+ WUILang.schFullWeek			= [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ];
+
+ WUILang.schUnlimited			= "Unlimited";
+ WUILang.schLimited			= "Limited";
+ WUILang.schTurnOff			= "Turn Off";
+ WUILang.schSeedingOnly			= "Seeding Only";
+
+ WUILang.schUnlimitedDesc		= "Unlimited - Global limit is absent";
+ WUILang.schLimitedDesc			= "Limited - Uses scheduler-specific bandwidth limit";
+ WUILang.schTurnOffDesc			= "Turn Off - Stops all torrents";
+ WUILang.schSeedingOnlyDesc		= "Seeding Only - Only upload data";
+
+ WUILang.schLimitedUL			= "Limited UL";
+ WUILang.schLimitedDL			= "Limited DL";
+
+ WUILang.schedPHPNotFound		= "Scheduler plugin: rTorrent's user can't access php interpreter. Plugin will not work.";
+ WUILang.schedUpdaterNotAvailable	= "Scheduler plugin: rTorrent's user can't access file plugins/scheduler/update.php for read. Plugin will not work.";
+
+ utWebUI.schedulerCreate();
