fc732f6651b38baba99741160abc862e18590187
novik65 <Novik@users.noreply.github.com>
2009-10-05 11:46:07 +0000

    make tags

---
+++ seedingtime/1.0/lang/sr.js
@@ -0,0 +1,6 @@
+﻿
+ 
+ WUILang.seedingTimeBadVersion	= "Додатак SeedingTime: Верзија rTorrent-а коју ви користите не подржава овај додатак.";
+ WUILang.seedingTime		= "Завршено";
+
+utWebUI.seedingTimeCreate();
