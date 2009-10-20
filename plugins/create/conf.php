576fcca0935d68ef63ecd28e92eac3975422f6ff
novik65 <Novik@users.noreply.github.com>
2009-10-20 11:47:44 +0000

    create: add external programs, add large piece size

--- plugins/create/conf.php
+++ plugins/create/conf.php
@@ -5,6 +5,8 @@
 						// false - use internal realization (may be too slow for large files)
 						// "transmissioncli" - use program transmissioncli (see http://www.transmissionbt.com/)
 						// "createtorrent" - use program createtorrent (see http://www.createtorrent.com)
+						// "mktorrent" - use program createtorrent (see http://mktorrent.sourceforge.net)
+						// "buildtorrent" - use program buildtorrent (see http://claudiusmaximus.goto10.org/cm/torrent.html)
 	$pathToCreatetorrent = '';		// Something like /bin/createtorrent, or /bin/transmissioncli. If empty, program will be founded in PATH.
 
 ?>
