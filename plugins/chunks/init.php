a0918fb00c412e4d69538cbc32e3686b0c806746
zcsala021 <zcsala021@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-10-27 20:56:28 +0000

    chunks: Simplified check of rTorrent version

--- plugins/chunks/init.php
+++ plugins/chunks/init.php
@@ -1,26 +1,10 @@
 <?php
-    $req = new rXMLRPCRequest( new rXMLRPCCommand("system.client_version") );
-    if ( $req->run() ) {
-        $Version = $req->strings[0];
-        $jResult .= "utWebUI.rTorrentVersion = '" . $Version . "'; ";
-        $Parts = explode('.', $Version );
-        if ( $Parts[0] == 0 ) {
-            if ( $Parts[1] >= 8 ) {
-                if ( $Parts[2] >= 5 ) {
-                    $jResult .= "utWebUI.ChunksEnabled = true; ";
-                } else {
-                    $jResult .= "utWebUI.ChunksEnabled = false; ";
-                }
-            } else {
-                $jResult .= "utWebUI.ChunksEnabled = false; ";
-            }
-        } else {
-            $jResult .= "utWebUI.ChunksEnabled = true; ";
-        }
-        $theSettings->registerPlugin("chunks");
+    if ( $theSettings->iVersion >= 0x805 ) {
+        $jResult .= "utWebUI.ChunksEnabled = true; ";
+        $theSettings->registerPlugin( "chunks" );
     } else {
-        // Cannot run XMLRPC command required to determine version
-        // of rTorrent, plugin will NOT be installed
+        // Inappropriate version of rTorrent, plugin will NOT be installed
         $jResult .= "utWebUI.ChunksEnabled = false; ";
+        $jResult .= "utWebUI.rTorrentVersion = '" . $theSettings->version . "'; ";
     }
 ?>
