4a5061506c326db6874fdcb5c976a67c835245ae
zcsala021 <zcsala021@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-10-30 20:39:53 +0000

    geoip: Translation update

--- trunk/plugins/geoip/lang/ru.js
+++ trunk/plugins/geoip/lang/ru.js
@@ -2,3 +2,104 @@
 utWebUI.prsColumns[ 0 ][ "text" ] = "Страна";
 utWebUI.prsColumns[ 1 ][ "text" ] = "IP";
 
+WUILang.country = new Array();
+
+WUILang.country[ "ae" ] = "United Arab Emirates";
+WUILang.country[ "ar" ] = "Argentina";
+WUILang.country[ "at" ] = "Austria";
+WUILang.country[ "au" ] = "Australia";
+
+WUILang.country[ "ba" ] = "Bosnia and Herzegovina";
+WUILang.country[ "bd" ] = "Bangladesh";
+WUILang.country[ "be" ] = "Belgium";
+WUILang.country[ "bg" ] = "Bulgaria";
+WUILang.country[ "bm" ] = "Bermuda";
+WUILang.country[ "bn" ] = "Brunei Darussalam";
+WUILang.country[ "br" ] = "Brazil";
+WUILang.country[ "bw" ] = "Botswana";
+
+WUILang.country[ "ca" ] = "Canada";
+WUILang.country[ "ch" ] = "Switzerland";
+WUILang.country[ "cl" ] = "Chile";
+WUILang.country[ "cn" ] = "China";
+WUILang.country[ "co" ] = "Colombia";
+WUILang.country[ "cy" ] = "Cyprus";
+WUILang.country[ "cz" ] = "Czech Republic";
+
+WUILang.country[ "dk" ] = "Denmark";
+WUILang.country[ "de" ] = "Germany";
+WUILang.country[ "do" ] = "Dominican Republic";
+
+WUILang.country[ "es" ] = "Spain";
+WUILang.country[ "eg" ] = "Egypt";
+WUILang.country[ "ee" ] = "Estonia";
+
+WUILang.country[ "fi" ] = "Finland";
+WUILang.country[ "fr" ] = "France";
+
+WUILang.country[ "gb" ] = "United Kingdom";
+WUILang.country[ "gr" ] = "Greece";
+
+WUILang.country[ "hk" ] = "Hong Kong";
+WUILang.country[ "hr" ] = "Croatia";
+WUILang.country[ "hu" ] = "Hungary";
+
+WUILang.country[ "ie" ] = "Ireland";
+WUILang.country[ "il" ] = "Israel";
+WUILang.country[ "in" ] = "India";
+WUILang.country[ "is" ] = "Iceland";
+WUILang.country[ "it" ] = "Italy";
+
+WUILang.country[ "jo" ] = "Jordan";
+WUILang.country[ "jp" ] = "Japan";
+
+WUILang.country[ "kp" ] = "Korea, Democratic People's Republic of";
+WUILang.country[ "kr" ] = "Korea, Republic of";
+
+WUILang.country[ "lv" ] = "Latvia";
+WUILang.country[ "lt" ] = "Lithuania";
+WUILang.country[ "lu" ] = "Luxembourg";
+
+WUILang.country[ "md" ] = "Moldova, Republic of";
+WUILang.country[ "me" ] = "Montenegro";
+WUILang.country[ "mk" ] = "Macedonia";
+WUILang.country[ "mx" ] = "Mexico";
+WUILang.country[ "my" ] = "Malaysia";
+
+WUILang.country[ "nl" ] = "Netherlands";
+WUILang.country[ "no" ] = "Norway";
+WUILang.country[ "nz" ] = "New Zealand";
+
+WUILang.country[ "ph" ] = "Philippines";
+WUILang.country[ "pl" ] = "Poland";
+WUILang.country[ "pt" ] = "Portugal";
+
+WUILang.country[ "ro" ] = "Romania";
+WUILang.country[ "rs" ] = "Serbia";
+WUILang.country[ "ru" ] = "Russian Federation";
+
+WUILang.country[ "sa" ] = "Saudi Arabia";
+WUILang.country[ "se" ] = "Sweden";
+WUILang.country[ "sg" ] = "Singapore";
+WUILang.country[ "si" ] = "Slovenia";
+WUILang.country[ "sl" ] = "Slovakia";
+WUILang.country[ "sr" ] = "Suriname";
+
+WUILang.country[ "th" ] = "Thailand";
+WUILang.country[ "tw" ] = "Taiwan, Province of China";
+WUILang.country[ "tr" ] = "Turkey";
+WUILang.country[ "tt" ] = "Trinidad and Tobago";
+
+WUILang.country[ "ua" ] = "Ukraine";
+WUILang.country[ "uk" ] = "United Kingdom";
+WUILang.country[ "unknown" ] = "Unknown";
+WUILang.country[ "us" ] = "United States";
+WUILang.country[ "uy" ] = "Uruguay";
+
+WUILang.country[ "va" ] = "Vatican";
+WUILang.country[ "ve" ] = "Venezuela";
+WUILang.country[ "vn" ] = "Vietnam";
+
+WUILang.country[ "zw" ] = "Zimbabwe";
+
+
