5b9a8e12a9c07ce3b0bc0e9ecdbd5f5603b90989
zcsala021 <zcsala021@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-10-30 20:41:36 +0000

    geoip: Moved determining of a country name to the client side

--- plugins/geoip/lookup.php
+++ plugins/geoip/lookup.php
@@ -1,45 +1,23 @@
 <?php
     $Action = $_GET[ 'action' ];
     $IP     = $_GET[ "ip" ];
-    $Lang   = $_GET[ "lang" ];
     
     if ( !isset($Action) || !isset($IP) ) {
         echo "null";
         return;
     }
-    if ( !isset($Lang) ) {
-        $Lang = "en";
-    }
-    if ( ! include_once( "lang/" . $Lang . ".php" ) ) {
-        // No such language file available, default to English
-        $Lang = "en";
-    }
-    
+ 
     if ( $Action == "dns" ) {
-        // Support for IP resolution
+        // TODO: support for IP resolution
         echo( gethostbyaddr($IP) );
     } else if ( $Action == "geoip" ) {
-        $CtrN = geoip_country_name_by_name( $IP );
         $CtrC = geoip_country_code_by_name( $IP );
         if ( ! isset($CtrC) || $CtrC == "" ) {
             $CtrC = "unknown";
         } else {
             $CtrC = strtolower( $CtrC );
         }
-        if ( $Lang != "en" ) {
-            $Res = $Countries[ $CtrN ];
-            if ( ! isset($Res) || $Res == "" ) {
-                // No localized name for given country
-                $Res = $CtrN;
-            }
-        } else {
-            if ( ! isset($CtrN) || $CtrN == "" ) {
-                $Res = "Unknown";
-            } else {
-                $Res = $CtrN;
-            }
-        }
-        echo $CtrC . "|" . $Res;
+        echo $CtrC;
     } else {
         echo "Unknown action requested: " . $Action;
     }
