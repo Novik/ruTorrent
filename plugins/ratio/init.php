57d96c744498af5bdfc49d37b416df7cd527dde3
novik65 <Novik@users.noreply.github.com>
2009-11-24 10:49:53 +0000

    ratio: when $autoStart=false ratio data now obtained from rtorrent

--- plugins/ratio/init.php
+++ plugins/ratio/init.php
@@ -2,7 +2,7 @@
 require_once( 'plugins/ratio/ratio.php');
 
 $rat = rRatio::load();
-if(($theSettings->iVersion<0x804) || !$rat->flush() || ($isAutoStart && !$rat->correct()))
+if(($theSettings->iVersion<0x804) || ($isAutoStart && !$rat->flush() && !$rat->correct()) || (!$isAutoStart && !$rat->obtain()) )
 	$jResult.="utWebUI.showRatioError('WUILang.ratioUnsupported'); utWebUI.ratioSupported = false; ";
 else
 {
