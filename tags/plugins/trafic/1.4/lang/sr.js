d1159bfbd5cf9df454a3caf2d27c89768dc8bc0e
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-11-30 09:17:15 +0000

    make tags

---
+++ tags/plugins/trafic/1.4/lang/sr.js
@@ -0,0 +1,12 @@
+﻿
+ 
+ WUILang.traf 		= "Саобраћај";
+ WUILang.perDay		= "Дневно";
+ WUILang.perMonth 	= "Месечно";
+ WUILang.perYear 	= "Годишње";
+ WUILang.allTrackers	= "Сви тракери";
+ WUILang.ClearButton	= "Очисти";
+ WUILang.ClearQuest 	= "Do you really want to clear statistics for the selected tracker(s)?";
+  WUILang.trafPHPNotFound	= "Traffic plugin: rTorrent's user can't access php interpreter. Plugin will not work.";
+ WUILang.trafStatsNotAvailable	= "Traffic plugin: rTorrent's user can't access directories plugins/trafic/stats and plugins/trafic/stats/trackers for read/write/execute. Plugin will not work.";
+ WUILang.trafUpdaterNotAvailable= "Traffic plugin: rTorrent's user can't access file plugins/trafic/update.php for read. Plugin will not work.";
