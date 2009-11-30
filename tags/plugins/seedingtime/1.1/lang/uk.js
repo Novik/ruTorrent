ebfa3b04b43cfd5999128fdf599f76bfaa642aa3
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-11-30 09:20:10 +0000

    make tags

---
+++ tags/plugins/seedingtime/1.1/lang/uk.js
@@ -0,0 +1,6 @@
+﻿
+ 
+ WUILang.seedingTimeBadVersion	= "SeedingTime plugin: this version of rTorrent does't support this plugin. Plugin will not work.";
+ WUILang.seedingTime		= "Finished";
+
+utWebUI.seedingTimeCreate();
