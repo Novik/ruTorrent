2d3f6f94bd729c355b78b956f558171990d56602
dmrom00 <dmrom00@users.noreply.github.com>
2009-12-06 19:37:09 +0000

    AutoTools: refactoring

--- plugins/autotools/label.php
+++ plugins/autotools/label.php
@@ -2,7 +2,7 @@
 
 require_once( "../../util.php" );
 require_once( "../../xmlrpc.php" );
-require_once( "at_utils.php" );
+require_once( "util_rt.php" );
 require_once( "autotools.php" );
 require_once( "conf.php" );
 
@@ -10,68 +10,81 @@ require_once( "conf.php" );
 function Debug( $str )
 {
 	global $autodebug_enabled;
-	if( $autodebug_enabled ) toLog( $str );
+	if( $autodebug_enabled ) rtDbg( "AutoLabel: ".$str );
 }
 
 Debug( "" );
-Debug( "--- label.php begin ---" );
+Debug( "--- begin ---" );
 
+$is_ok = true;
 if( count( $argv ) < 1 )
 {
-	Debug( "Called without arguments (hash wanted)" );
-	exit;
+	Debug( "called without arguments (hash wanted)" );
+	$is_ok = false;
 }
-$hash = $argv[1];
+else $hash = $argv[1];
 
 // Read configuration
-$at = rAutoTools::load();
-Debug( "AutoLabel state : ".$at->enable_label );
-if( !$at->enable_label )
-	exit;
-
-$req = new rXMLRPCRequest( array (
-	new rXMLRPCCommand( "get_directory" ),
-	new rXMLRPCCommand( "d.get_directory", $hash ),
-	new rXMLRPCCommand( "d.get_custom3",   $hash ),
-	new rXMLRPCCommand( "d.is_multi_file", $hash ),
-) );
-if( $req->run() && !$req->fault )
+if( $is_ok )
 {
-	//$i = 0;
-	//foreach( $req->strings as $str ) Debug( $i++.' '.$str );
-	//$i = 0;
-	//foreach( $req->i8s as $str ) Debug( $i++.' '.$str );
+	$at = rAutoTools::load();
+	Debug( "enabled         : ".$at->enable_label );
+	if( !$at->enable_label )
+		$is_ok = false;
+}
 
-	$is_multy_file = ( $req->i8s[0] != 0 );
-	$default_dir = trim( $req->strings[0] );
-	$torrent_dir = trim( $req->strings[1] );
-	$custom3     = trim( $req->strings[2] );
-	Debug( "get_directory   : ".$default_dir );
-	Debug( "d.get_directory : ".$torrent_dir );
-	Debug( "d.get_custom3   : ".$custom3 );
-	Debug( "d.is_multy_file : ".$is_multy_file );
-	// "$custom3 == 1" -> torrent is NOT NEW (modified by another plugin)
-	if( $default_dir == '' || $torrent_dir == '' || $custom3 == '1' )
-		exit;
-
-	if( $is_multy_file )
-		$torrent_dir = RemoveLastToken( $torrent_dir, '/' );
-	$label = GetRelativePath( $default_dir, $torrent_dir );
-	Debug( "Label           : ".$label );
-	if( $label != '' && $label != './' )
+// Get info from rTorrent
+if( $is_ok )
+{
+	$req = new rXMLRPCRequest( array (
+		new rXMLRPCCommand( "get_directory" ),
+		new rXMLRPCCommand( "d.get_directory", $hash ),
+		new rXMLRPCCommand( "d.get_custom3",   $hash ),
+		new rXMLRPCCommand( "d.is_multi_file", $hash ),
+	) );
+	if( $req->run() && !$req->fault )
 	{
-		$cmd = new rXMLRPCCommand( "d.set_custom1", array (
-			$hash,
-			rawurlencode( $label )
-		) );
-		$req = new rXMLRPCRequest( $cmd );
-		$req->run();
+		//$i = 0;
+		//foreach( $req->strings as $str ) Debug( $i++.' '.$str );
+		//$i = 0;
+		//foreach( $req->i8s as $str ) Debug( $i++.' '.$str );
+
+		$is_multy_file = ( $req->i8s[0] != 0 );
+		$default_dir = trim( $req->strings[0] );
+		$torrent_dir = trim( $req->strings[1] );
+		$custom3     = trim( $req->strings[2] );
+		Debug( "get_directory   : ".$default_dir );
+		Debug( "d.get_directory : ".$torrent_dir );
+		Debug( "d.get_custom3   : ".$custom3 );
+		Debug( "d.is_multy_file : ".$is_multy_file );
+		if( $default_dir == '' || $torrent_dir == '' )
+		{
+			Debug( "base paths are not set" );
+			$is_ok = false;
+		}
+		elseif( $custom3 == '1' )
+		{
+			Debug( "torrent is NOT NEW (modified by another plugin)" );
+			$is_ok = false;
+		}
+	}
+	else {
+		Debug( "rXMLRPCRequest() fail!" );
+		$is_ok = false;
 	}
 }
-else {
-	Debug( "rXMLRPCRequest() fail!" );
+
+// Set a label
+if( $is_ok )
+{
+	if( $is_multy_file )
+		$torrent_dir = rtRemoveLastToken( $torrent_dir, '/' );
+	$label = rtGetRelativePath( $default_dir, $torrent_dir );
+	Debug( "label           : \"".$label."\"" );
+	if( $label != '' && $label != './' )
+		rtExec( "d.set_custom1", array( $hash, rawurlencode( $label ) ) );
 }
 
-Debug( "--- label.php end ---" );
+Debug( "--- end ---" );
 
 ?>
