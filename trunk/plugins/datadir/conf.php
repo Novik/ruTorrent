dc4cb0b9d3a18eae835e7690feab55ce7bae0ab2
dmrom00 <dmrom00@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-12-09 09:25:36 +0000

    DataDir: operations with group of selected files

--- trunk/plugins/datadir/conf.php
+++ trunk/plugins/datadir/conf.php
@@ -8,6 +8,6 @@
 	$datadir_runmode="rtorrent";
 
 	// set "true" to enable debug output
-	$datadir_debug_enabled = true;
+	$datadir_debug_enabled = false;
 
 ?>
--- trunk/plugins/datadir/setdir.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd $(dirname $0)
-
-"$1" setdir.php "$2" "$3" "$4" &
-
-echo "$1 setdir.php \"$2\" \"$3\" \"$4\"" > /tmp/setdir.log
-
