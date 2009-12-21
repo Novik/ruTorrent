19b763600794468b6dee79ccfd4ee6c59d91557e
zcsala021 <zcsala021@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-12-21 22:56:45 +0000

    geoip: Fixed incorrect display of column 'Country'

--- trunk/plugins/geoip/init.php
+++ trunk/plugins/geoip/init.php
@@ -1,7 +1,7 @@
 <?php
 
 if ( function_exists("geoip_database_info") ) {
-    $jResult .= "utWebUI.prsColumns.unshift( {'text' : 'Country', 'width' : '60px', 'type' : TYPE_STRING}); ";
+    $jResult .= "utWebUI.prsColumns.unshift( {'text' : utWebUI.prsColumns[ 0 ][ 'text' ], 'width' : '60px', 'type' : TYPE_STRING}); ";
     $jResult .= "utWebUI.GeoIPSupported = true; ";
     $theSettings->registerPlugin("geoip");
 } else {
