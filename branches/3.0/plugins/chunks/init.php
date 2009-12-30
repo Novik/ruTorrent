d8f0438496834826136e27f72d3c6f58146689a9
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2009-12-30 13:07:30 +0000

    beta3: chunks fix

--- branches/3.0/plugins/chunks/init.php
+++ branches/3.0/plugins/chunks/init.php
@@ -1,9 +1,6 @@
 <?php
-    if ( $theSettings->iVersion >= 0x805 ) {
-        $theSettings->registerPlugin( "chunks" );
-    } else {
-        // Inappropriate version of rTorrent, plugin will NOT be installed
-        $jResult .= "plugin.disable();";
-        $jResult .= "theWebUI.rTorrentVersion = '" . $theSettings->version . "'; ";
-    }
+	if ( $theSettings->iVersion >= 0x805 )
+		$theSettings->registerPlugin( "chunks" );
+	else
+		$jResult .= "plugin.disable();";
 ?>
