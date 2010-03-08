c8d723ea3d8868b36c91a39a51764fffc23f5bc6
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-03-08 11:51:36 +0000

    beta3: fix check of rtorrent version in plugin "seedingtime".

--- branches/3.0/plugins/seedingtime/init.php
+++ branches/3.0/plugins/seedingtime/init.php
@@ -1,6 +1,6 @@
 <?php
 
-if( $theSettings->iVersion >= 0x805 )
+if( $theSettings->iVersion >= 0x806 )
 {
 	$req = new rXMLRPCRequest( new rXMLRPCCommand( "system.method.set_key", 
 		array("event.download.finished","seedingtime".getUser(),'d.set_custom=seedingtime,"$execute_capture={date,+%s}"') ) );
