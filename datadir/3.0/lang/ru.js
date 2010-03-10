8b5ac2061274e82ccc1629f9e12a29af37ed901b
novik65 <Novik@users.noreply.github.com>
2010-03-10 12:48:23 +0000

    make tags

---
+++ datadir/3.0/lang/ru.js
@@ -0,0 +1,9 @@
+﻿
+
+ theUILang.DataDir		= "Сохранить в";
+ theUILang.DataDirMove		= "Перенести файлы данных";
+ theUILang.datadirDlgCaption	= "Настройка каталога данных";
+ theUILang.datadirDirNotFound     = "Плагин DataDir: Недопустимый каталог";
+ theUILang.datadirSetDirFail      = "Плагин DataDir: Операция не выполнена";
+
+thePlugins.get("datadir").langLoaded();
