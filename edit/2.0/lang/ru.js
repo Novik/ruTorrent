984f0bd6dbcaac18a64d848693c385382b158aee
novik65 <Novik@users.noreply.github.com>
2010-03-10 12:20:42 +0000

    make tags

---
+++ edit/2.0/lang/ru.js
@@ -0,0 +1,10 @@
+﻿
+ 
+ WUILang.EditTrackers 		= "Редактировать...";
+ WUILang.EditTorrentProperties 	= "Настройка торрента";
+ WUILang.errorAddTorrent	= "Ошибка добавления торрента";
+ WUILang.errorWriteTorrent	= "Ошибка записи файла";
+ WUILang.errorReadTorrent	= "Ошибка чтения файла";
+ WUILang.cantFindTorrent	= "Исходный торрент файл для данной закачки отсутствует.";
+
+utWebUI.editCreate();
