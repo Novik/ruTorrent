3f856d252c44cc008e4132ad9428f48ec6d5364d
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-03-10 12:19:32 +0000

    make tags

---
+++ tags/plugins/create/2.0/lang/nl.js
@@ -0,0 +1,15 @@
+﻿
+ 
+ WUILang.mnu_create 		= "Create torrent...";
+ WUILang.CreateNewTorrent 	= "Create New Torrent";
+ WUILang.SelectSource 		= "Select Source";
+ WUILang.TorrentProperties 	= "Torrent Properties";
+ WUILang.PieceSize 		= "Piece size";
+ WUILang.Other 			= "Other";
+ WUILang.StartSeeding 		= "Start seeding";
+ WUILang.PrivateTorrent		= "Private torrent";
+ WUILang.CreateAndSaveAs	= "Create and save as...";
+ WUILang.BadTorrentData		= "You must fill all required fields!";
+ WUILang.createExternalNotFound = "Create plugin: Plugin will not work. Web-server can't access to external program";
+
+utWebUI.initCreate();
