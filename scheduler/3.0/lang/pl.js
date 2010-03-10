c4a5fd48aba5b2d741b740e051083ba2c1a0fd02
novik65 <Novik@users.noreply.github.com>
2010-03-10 12:50:11 +0000

    make tags

---
+++ scheduler/3.0/lang/pl.js
@@ -0,0 +1,25 @@
+﻿
+
+ theUILang.scheduler			= "Harmonogram";
+ theUILang.schedulerOn			= "Włącz Harmonogram";
+ theUILang.schedulerGraph			= "Tablica harmonogramu";
+ theUILang.schShortWeek			= [ "pon.", "wt.", "śr.", "czw.", "pt.", "sob.", "ndz." ];
+ theUILang.schFullWeek			= [ "poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela" ];
+
+ theUILang.schUnlimited			= "Nielimitowany";
+ theUILang.schLimited			= "Limit ";
+ theUILang.schTurnOff			= "Wyłącz";
+ theUILang.schSeedingOnly			= "Tylko wysyłanie";
+
+ theUILang.schUnlimitedDesc		= "Nielimitowany - Ogólny limit nie istnieje";
+ theUILang.schLimitedDesc			= "Limitowany - Użyty Harmonogram do ograniczenia transferu";
+ theUILang.schTurnOffDesc			= "Wyłącz - Zatrzymuje wszystkie torrenty";
+ theUILang.schSeedingOnlyDesc		= "Tylko wysyłanie danych";
+
+ theUILang.schLimitedUL			= "Limit wysyłania";
+ theUILang.schLimitedDL			= "Limit pobierania:";
+
+ theUILang.schedPHPNotFound		= "Wtyczka Harmonogram: użytkownik, który uruchomił rTorrent nie ma dostępu do interpretera PHP. Wtyczka nie będzie działać.";
+ theUILang.schedUpdaterNotAvailable	= "Wtyczka Harmonogram: użytkownik, który uruchomił rTorrent nie prawa odczytu pliku plugins/Harmonogram/update.php . Wtyczka nie będzie działać.";
+
+thePlugins.get("scheduler").langLoaded();
