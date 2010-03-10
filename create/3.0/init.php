13b3dac0039f7d63025b6d9b7725dc5eb49ede13
novik65 <Novik@users.noreply.github.com>
2010-03-10 12:47:21 +0000

    make tags

---
+++ create/3.0/init.php
@@ -0,0 +1,18 @@
+<?php
+
+eval(getPluginConf('create'));
+
+if($useExternal!==false)
+{
+	if($do_diagnostic && empty($pathToCreatetorrent) && (findEXE($useExternal)==false))
+		$jResult.="plugin.disable(); plugin.showError('theUILang.createExternalNotFound+\' (".$useExternal.").\'');";
+	else
+	{
+		if($useExternal === "transmissioncli")
+			$jResult.="plugin.hidePieceSize = true;";
+		$theSettings->registerPlugin("create");
+	}
+}
+else
+	$theSettings->registerPlugin("create");
+?>
