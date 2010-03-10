5c3904f2e38b583816ffe119af7d2f9a73c432ea
novik65 <Novik@users.noreply.github.com>
2010-03-10 12:49:25 +0000

    make tags

---
+++ ratio/3.0/ratio.php
@@ -0,0 +1,146 @@
+<?php
+
+require_once( dirname(__FILE__)."/../../php/xmlrpc.php" );
+require_once( $rootPath.'/php/cache.php');
+eval(getPluginConf('ratio'));
+
+define('RAT_STOP',0);
+define('RAT_STOP_AND_REMOVE',1);
+define('RAT_ERASE',2);
+
+class rRatio
+{
+	public $hash = "ratio.dat";
+	public $rat = array();
+
+	static public function load()
+	{
+		$cache = new rCache();
+		$rt = new rRatio();
+		if(!$cache->get($rt))
+			$rt->fillArray();
+		return($rt);
+	}
+	public function fillArray()
+	{
+		$this->rat = array();
+	        for($i=0; $i<MAX_RATIO; $i++)
+			$this->rat[] = array( "action"=>RAT_STOP, "min"=>100, "max"=>300, "upload"=>20, "name"=>"ratio".$i );
+	}
+	public function isCorrect($no)
+	{
+		return( ($no<count($this->rat)) &&
+		        ($this->rat[$no]["name"]!=""));
+
+	}
+	public function correct()
+	{
+		$cmd = new rXMLRPCCommand("d.multicall",array("default","d.get_hash="));
+		for($i=0; $i<MAX_RATIO; $i++)
+			$cmd->addParameters( array( "d.views.has=rat_".$i, "view.set_not_visible=rat_".$i ) );
+		$req = new rXMLRPCRequest($cmd);
+		$req->setParseByTypes();
+		if($req->run() && !$req->fault)
+		{
+			$req1 = new rXMLRPCRequest();
+			foreach($req->strings as $no=>$hash)
+			{
+			        for($i=0; $i<MAX_RATIO; $i++)
+			        {
+					if($req->i8s[$no*MAX_RATIO*2+$i*2]==1)
+						$req1->addCommand(new rXMLRPCCommand("view.set_visible",array($hash,"rat_".$i)));
+				}						
+			}
+			return(($req1->getCommandsCount()==0) || ($req1->run() && !$req1->fault));
+		}
+		return(false);
+	}
+	public function obtain()
+	{
+        	return($this->flush() && $this->correct());
+	}
+	public function flush()
+	{
+		$req1 = new rXMLRPCRequest(new rXMLRPCCommand("view_list"));
+		if($req1->run() && !$req1->fault)
+		{
+			$req = new rXMLRPCRequest();
+			for($i=0; $i<MAX_RATIO; $i++)
+			{
+				$rat = $this->rat[$i];
+				if(!in_array("rat_".$i,$req1->val))
+					$req->addCommand(new rXMLRPCCommand("group.insert_persistent_view", array("", "rat_".$i)));
+				if($this->isCorrect($i))
+				{
+					$req->addCommand(new rXMLRPCCommand("group.rat_".$i.".ratio.enable",array("")));
+					$req->addCommand(new rXMLRPCCommand("group.rat_".$i.".ratio.min.set",$rat["min"]));
+					$req->addCommand(new rXMLRPCCommand("group.rat_".$i.".ratio.max.set",$rat["max"]));
+					$req->addCommand(new rXMLRPCCommand("group.rat_".$i.".ratio.upload.set",floatval($rat["upload"]*1024*1024)));
+					switch($rat["action"])
+					{
+						case RAT_STOP:
+						{
+							$req->addCommand(new rXMLRPCCommand("system.method.set", array("group.rat_".$i.".ratio.command", "d.stop=; d.close=")));
+							break;
+						}
+						case RAT_STOP_AND_REMOVE:
+						{
+							$req->addCommand(new rXMLRPCCommand("system.method.set", array("group.rat_".$i.".ratio.command", "d.stop=; d.close=; view.set_not_visible=rat_".$i."; d.views.remove=rat_".$i)));
+							break;
+						}
+						case RAT_ERASE:
+						{
+							$req->addCommand(new rXMLRPCCommand("system.method.set", array("group.rat_".$i.".ratio.command", "d.stop=; d.close=; d.erase=")));
+							break;
+						}
+					}
+				}
+			}
+			return(($req->getCommandsCount()==0) || ($req->run() && !$req->fault));
+		}
+		return(false);
+	}
+	public function store()
+	{
+		$cache = new rCache();
+		return($cache->set($this));
+	}
+
+	public function set()
+	{
+		$this->rat = array();
+		for($i = 0; $i<MAX_RATIO; $i++)
+		{
+			$arr = array( "action"=>RAT_STOP, "min"=>100, "max"=>300, "upload"=>20, "name"=>"" );
+			if(isset($_REQUEST['rat_action'.$i]))
+				$arr["action"] = intval($_REQUEST['rat_action'.$i]);
+			if(isset($_REQUEST['rat_min'.$i]))
+			        $arr["min"] = intval($_REQUEST['rat_min'.$i]);
+			if(isset($_REQUEST['rat_max'.$i]))
+			        $arr["max"] = intval($_REQUEST['rat_max'.$i]);
+			if(isset($_REQUEST['rat_upload'.$i]))
+			        $arr["upload"] = intval($_REQUEST['rat_upload'.$i]);
+			if(isset($_REQUEST['rat_name'.$i]))
+			{
+			        $v = trim($_REQUEST['rat_name'.$i]);
+			        if($v!='')
+					$arr["name"] = $v;
+			}
+			$this->rat[] = $arr;
+		}
+                $this->store();
+		$this->flush();
+	}
+	public function get()
+	{
+		$ret = "theWebUI.ratios = [";
+		foreach($this->rat as $item)
+			$ret.="{ action: ".$item["action"].", min: ".$item["min"].", max: ".$item["max"].", upload: ".$item["upload"].", name : ".quoteAndDeslashEachItem($item["name"])." },";
+		$len = strlen($ret);
+		if($ret[$len-1]==',')
+			$ret = substr($ret,0,$len-1);
+		return($ret."];\ntheWebUI.maxRatio = ".MAX_RATIO.";\n");
+	}
+}
+
+?>
