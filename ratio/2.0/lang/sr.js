41b54122130d672eefd756a89e9a670f7f212098
novik65 <Novik@users.noreply.github.com>
2010-03-10 12:21:29 +0000

    make tags

---
+++ ratio/2.0/lang/sr.js
@@ -0,0 +1,17 @@
+﻿
+ 
+ WUILang.ratioUnsupported	= "Додатак Ratio: this version of rTorrent does't support ratio. Plugin will not work.";
+ WUILang.ratios			    = "Односне групе";
+ WUILang.ratio			    = "Односна група";
+ WUILang.mnuRatio		    = "Поставити односну групу";
+ WUILang.mnuRatioUnlimited	= "Нема односа";
+ WUILang.ratioName 		    = "Име";
+ WUILang.minRatio 		    = "Минимално";
+ WUILang.maxRatio 		    = "Максимално";
+ WUILang.ratioUpload 		= "Узтовар";
+ WUILang.ratioAction 		= "Акција";
+ WUILang.ratioStop 		    = "Зауставити";
+ WUILang.ratioStopAndRemove = "Зауставити и обрисати групу";
+ WUILang.ratioErase 		= "Уклонити";
+
+utWebUI.ratioCreate();
