379d9d9ee4def53de4284656f2a3d49b2532d393
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-03-10 12:22:48 +0000

    make tags

---
+++ tags/plugins/scheduler/2.0/lang/sr.js
@@ -0,0 +1,24 @@
+﻿
+ WUILang.scheduler			        = "Scheduler";
+ WUILang.schedulerOn			    = "Омогући Scheduler";
+ WUILang.schedulerGraph			    = "Scheduler-ова табела";
+ WUILang.schShortWeek			    = [ "пон", "уто", "сре", "чет", "пет", "суб", "нед" ];
+ WUILang.schFullWeek			    = [ "понедељак", "уторак", "среда", "четвртак", "петак", "субота", "недеља" ];
+
+ WUILang.schUnlimited			    = "Неограничено";
+ WUILang.schLimited			        = "Ограничено";
+ WUILang.schTurnOff			        = "Искључено";
+ WUILang.schSeedingOnly			    = "Само сејање";
+
+ WUILang.schUnlimitedDesc		    = "Неограничено - Нема глобалног ограничења";
+ WUILang.schLimitedDesc			    = "Ограничено - Uses scheduler-specific bandwidth limit";
+ WUILang.schTurnOffDesc			    = "Искључено - Зауставља све торенте";
+ WUILang.schSeedingOnlyDesc		    = "Само сејање - Само узтовар података";
+
+ WUILang.schLimitedUL               = "Ограничен узтовар";
+ WUILang.schLimitedDL               = "Ограничен низтовар";
+
+ WUILang.schedPHPNotFound           = "Додатак Scheduler: Корисник rTorrent-а не може да приступи РНР интерпретеру. Додатак неће радити.";
+ WUILang.schedUpdaterNotAvailable   = "Додатак Scheduler: Корисник rTorrent-а не може да приступи датотеци plugins/scheduler/update.php ради читања. Додатак неће радити.";
+
+ utWebUI.schedulerCreate();
