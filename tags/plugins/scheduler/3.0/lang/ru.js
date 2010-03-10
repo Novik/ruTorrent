e17235c529d92939ad41e4ece7e7c9f16c877a61
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-03-10 12:50:11 +0000

    make tags

---
+++ tags/plugins/scheduler/3.0/lang/ru.js
@@ -0,0 +1,25 @@
+﻿
+
+ theUILang.scheduler			= "Расписание";
+ theUILang.schedulerOn			= "Включить расписание";
+ theUILang.schedulerGraph			= "График расписания";
+ theUILang.schShortWeek			= [ "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс" ];
+ theUILang.schFullWeek			= [ "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" ];
+
+ theUILang.schUnlimited			= "Без ограничений";
+ theUILang.schLimited			= "Ограничение";
+ theUILang.schTurnOff			= "Стоп";
+ theUILang.schSeedingOnly			= "Только отдача";
+
+ theUILang.schUnlimitedDesc		= "Без ограничений - никаких ограничений нет";
+ theUILang.schLimitedDesc			= "Ограничение - использовать запланированное ограничение скорости";
+ theUILang.schTurnOffDesc			= "Стоп - останавливать все торренты";
+ theUILang.schSeedingOnlyDesc		= "Только отдача - только сидировать";
+
+ theUILang.schLimitedUL			= "Ограничение раздачи";
+ theUILang.schLimitedDL			= "Ограничение закачки";
+ 
+ theUILang.schedPHPNotFound	= "Плагин scheduler: пользователю rTorrent не доступен интерпретатор php. Плагин не будет работать.";
+ theUILang.schedUpdaterNotAvailable= "Плагин scheduler: rTorrent должен иметь доступ на чтение к файлу plugins/scheduler/update.php. Плагин не будет работать.";
+
+thePlugins.get("scheduler").langLoaded();
