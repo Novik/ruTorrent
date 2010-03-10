d9643e90dd55c503a66ac6665825cf921ebf5caf
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-03-10 12:21:51 +0000

    make tags

---
+++ tags/plugins/retrackers/2.0/lang/ru.js
@@ -0,0 +1,9 @@
+﻿
+ 
+ WUILang.retrackers		= "Ретрекеры";
+ WUILang.dontAddToPrivate	= "Не добавлять ретрекеры к частным раздачам";
+ WUILang.retrackersPHPNotFound		= "Плагин retrackers: пользователю rTorrent не доступен интерпретатор php. Плагин не будет работать.";
+ WUILang.retrackersRunNotAvailable	= "Плагин retrackers: rTorrent должен иметь доступ на чтение/выполнение к файлу plugins/retrackers/run.sh. Плагин не будет работать.";
+ WUILang.retrackersUpdaterNotAvailable	= "Плагин retrackers: rTorrent должен иметь доступ на чтение к файлу plugins/retrackers/update.php. Плагин не будет работать.";
+
+utWebUI.retrackersCreate();
