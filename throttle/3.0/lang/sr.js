6e516e607d47016b9b33628f8fa8cb6fe3b2a4b6
novik65 <Novik@users.noreply.github.com>
2010-03-10 12:51:14 +0000

    make tags

---
+++ throttle/3.0/lang/sr.js
@@ -0,0 +1,9 @@
+﻿
+ theUILang.throttleUnsupported    = "Додатак Throttle: this version of rTorrent doesn't support throttle. Plugin will not work.";
+ theUILang.throttles              = "Канали";
+ theUILang.throttle               = "Канал";
+ theUILang.mnuThrottle            = "Постави канал";
+ theUILang.mnuUnlimited           = "Нема канала";
+ theUILang.channelName            = "Име";
+
+thePlugins.get("throttle").langLoaded();
---
+++ throttle/3.0/lang/zh-tw.js
@@ -0,0 +1,10 @@
+﻿
+ 
+ theUILang.throttleUnsupported	= "Throttle plugin: this version of rTorrent doesn't support throttle. Plugin will not work.";
+ theUILang.throttles		= "Channels";
+ theUILang.throttle		= "Channel";
+ theUILang.mnuThrottle		= "Set channel";
+ theUILang.mnuUnlimited		= "No channel";
+ theUILang.channelName 		= "Name";
+
+thePlugins.get("throttle").langLoaded();
