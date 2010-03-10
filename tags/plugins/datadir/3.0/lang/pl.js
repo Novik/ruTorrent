a6774a6f9934e06a7ce58ef58cce11ac3cada036
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-03-10 12:48:23 +0000

    make tags

---
+++ tags/plugins/datadir/3.0/lang/pl.js
@@ -0,0 +1,9 @@
+﻿
+
+ theUILang.DataDir		= "Zapisz do";
+ theUILang.DataDirMove		= "Przenieś pliki danych";
+ theUILang.datadirDlgCaption	= "Katalog danych Torrent";
+ theUILang.datadirDirNotFound	= "Wtyczka DataDir: Zły katalog";
+ theUILang.datadirSetDirFail	= "Wtyczka DataDir: Operacja się nie udała";
+
+thePlugins.get("datadir").langLoaded();
---
+++ tags/plugins/datadir/3.0/lang/sr.js
@@ -0,0 +1,9 @@
+﻿
+
+ theUILang.DataDir		= "Сними у";
+ theUILang.DataDirMove		= "Move data files";
+ theUILang.datadirDlgCaption	= "Директоријум података за торент";
+ theUILang.datadirDirNotFound	= "Додатак DataDir: Погрешан директоријум";
+ theUILang.datadirSetDirFail	= "Додатак DataDir: rXMLRPCRequest() није успео";
+
+thePlugins.get("datadir").langLoaded();
