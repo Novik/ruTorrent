018589683e3be369542c1c524bbc00b1b80aeab4
novik65 <Novik@users.noreply.github.com>
2010-06-03 12:01:49 +0000

    plugin datadir fix

--- plugins/datadir/lang/fr.js
+++ plugins/datadir/lang/fr.js
@@ -13,5 +13,6 @@
  theUILang.datadirDlgCaption	= "R\u00e9pertoire du torrent";
  theUILang.datadirDirNotFound	= "Plugin 'DataDir': R\u00e9pertoire invalide";
  theUILang.datadirSetDirFail	= "Plugin 'DataDir': L'op\u00e9ration a \u00e9chou\u00e9e";
+ theUILang.datadirPHPNotFound	= "Plugin 'DataDir': rTorrent ne peut pas acc\u00e9der \u00e0 l'interpr\u00e9teur php. Le plugin ne fonctionnera pas.";
 
 thePlugins.get("datadir").langLoaded();
