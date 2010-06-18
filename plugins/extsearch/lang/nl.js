4c7b2789400fdec2b91203cb6766d542707ac209
novik65 <Novik@users.noreply.github.com>
2010-06-18 16:06:12 +0000

    Add plugin extsearch

---
+++ plugins/extsearch/lang/nl.js
@@ -0,0 +1,23 @@
+﻿
+ theUILang.tegMenuLoad	= "Load";
+ theUILang.tegMenuOpen	= "Open in browser";
+ theUILang.tegMenuDeleteItem = "Remove item";
+ theUILang.tegMenuDelete = "Remove tag";
+ theUILang.tegRefresh	= "Refresh tag";
+ theUILang.exsGlobal	= "Include to 'All' category";
+ theUILang.exsLimit	= "Result limit";
+ theUILang.exsEngines	= "Engines";
+ theUILang.exsSearch	= "Search";
+ theUILang.exsGlobalLimit = "Common limitations";
+ theUILang.excat	= "Category";
+ theUILang.excatall	= "All";
+ theUILang.excatmovies	= "Movies";
+ theUILang.excattv	= "TV";
+ theUILang.excatmusic	= "Music";
+ theUILang.excatgames	= "Games";
+ theUILang.excatanime	= "Anime";
+ theUILang.excatsoftware = "Software";
+ theUILang.excatpictures = "Pictures";
+ theUILang.excatbooks 	= "Books";
+
+thePlugins.get("extsearch").langLoaded();
