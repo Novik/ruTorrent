1f20625595e3c8dc23bb0dffaba58ea63bf98cbf
novik65 <Novik@users.noreply.github.com>
2010-07-23 07:54:54 +0000

    minor fix

--- plugins/extsearch/engines/TorrentReactor.php
+++ plugins/extsearch/engines/TorrentReactor.php
@@ -24,14 +24,14 @@ class TorrentReactorEngine extends commonEngine
 			$cli = $this->fetch( $url.'/search.php?search=&words='.$what.'&cid='.$cat.'&type=1&orderby=a.seeds&asc=0&skip='.($pg*35) );
 			if($cli==false || (strpos($cli->results, '<td colspan="8" bgcolor="#ffffff">No results.')!==false))
 				break;
-			$res = preg_match_all('/<tr class="torrent_.*">.*<a rel="nofollow" href="(?P<link>.*)"><img title=".*'.
+			$res = preg_match_all('/<tr class="torrent_.*">.*<a rel="nofollow" href="(?P<link>[^"]*torrentreactor.net\/download.php\?[^"]*)">.*'.
 				'<a href="\/torrents\/(?P<desc>.*)">(?P<name>.*)<\/a><\/td>.*<td>(?P<size>.*)<\/td>.*<td>(?P<seeds>.*)<\/td>.*<td>(?P<leech>.*)<\/td>.*'.
 				'<td><a title=".*">(?P<cat>.*)<\/a><\/td>/siU', $cli->results, $matches);
 			if($res)
 			{
 				for($i=0; $i<$res; $i++)
 				{
-					$link = $matches["link"][$i];
+					$link = self::removeTags($matches["link"][$i]);
 					if(!array_key_exists($link,$ret))
 					{
 						$item = $this->getNewEntry();
