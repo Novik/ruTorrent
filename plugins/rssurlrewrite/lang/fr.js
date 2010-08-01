8422800096b4276424797390886b500f282eff6d
nicobubulle <nicobubulle@users.noreply.github.com>
2010-08-01 08:04:11 +0000

    French Translation

--- plugins/rssurlrewrite/lang/fr.js
+++ plugins/rssurlrewrite/lang/fr.js
@@ -1,21 +1,30 @@
-
- theUILang.rssNewRule		= "New rule";
- theUILang.mnu_rssurlrewrite	= "Rules of URL replacement in RSS";
- theUILang.rssRulesManager	= "Rules manager";
- theUILang.rssAddRule		= "Add";
- theUILang.rssDelRule		= "Delete";
- theUILang.rssCheckRule		= "?";
- theUILang.rssRulesLegend	= "Rule settings";
- theUILang.rssSrcHref		= "If URL of torrent's load match pattern";
- theUILang.rssSrcGuid		= "If URL of torrent's description match pattern";
- theUILang.rssDstHref		= "then replace URL of torrent's load to";
- theUILang.rssDstGuid		= "then replace URL of torrent's description to";
- theUILang.rssRulesDebug	= "Rule debug";
- theUILang.rssTestString	= "Test";
- theUILang.rssTestResult	= "Result";
- theUILang.rssURLInfo		= "URL info";
- theUILang.rssURLGUID		= "Description URL";
- theUILang.rssURLHref           = "Load URL";
- theUILang.rssPatternError	= "Error in pattern string.";
+/*
+ * PLUGIN RSSURLREWRITE
+ * 
+ * File Name: fr.js
+ * 	French language file.
+ * 
+ * File Author:
+ *    Nicobubulle (nicobubulle@gmail.com)
+ */
+ 
+theUILang.rssNewRule           = "Nouvelle r\u00e8gle";
+theUILang.mnu_rssurlrewrite    = "R\u00e8gles de remplacement des URL dans les flux RSS";
+theUILang.rssRulesManager      = "Administration des r\u00e8gles";
+theUILang.rssAddRule           = "Ajouter";
+theUILang.rssDelRule           = "Supprimer";
+theUILang.rssCheckRule         = "?";
+theUILang.rssRulesLegend       = "Configuration de la r\u00e8gle";
+theUILang.rssSrcHref           = "Si l'URL du torrent \u00e0 charger correspond au mod\u00e8le";
+theUILang.rssSrcGuid           = "Si l'URL de la description du torrent correspond au mod\u00e8le";
+theUILang.rssDstHref           = "alors replacer l'URL du torrent \u00e0 charger par";
+theUILang.rssDstGuid           = "alors replacer l'URL de la description du torrent par";
+theUILang.rssRulesDebug        = "Debug";
+theUILang.rssTestString        = "Test";
+theUILang.rssTestResult        = "R\u00e9sultat";
+theUILang.rssURLInfo           = "Info URL";
+theUILang.rssURLGUID           = "URL de la description";
+theUILang.rssURLHref           = "URL \u00e0 charger";
+theUILang.rssPatternError      = "Erreur dans le mod\u00e8le.";
  
 thePlugins.get("rssurlrewrite").langLoaded();
