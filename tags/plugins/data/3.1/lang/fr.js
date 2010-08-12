cac4285e479ab1fe1bccc9d56efc830f951ac7d9
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-08-12 09:12:39 +0000

    make tags

---
+++ tags/plugins/data/3.1/lang/fr.js
@@ -0,0 +1,14 @@
+/*
+ * PLUGIN DATA
+ * 
+ * File Name: fr.js
+ * 	French language file.
+ * 
+ * File Author:
+ *    Nicobubulle (nicobubulle@gmail.com)
+ */
+ 
+ theUILang.getData		= "T\u00e9l\u00e9charger le fichier";
+ theUILang.cantAccessData	= "Le serveur web n'a pas acc\u00e8s a ce fichier.";
+
+thePlugins.get("data").langLoaded();
