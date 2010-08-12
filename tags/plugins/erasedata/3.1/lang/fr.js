44fba471b60ce5ca5eb0444e9238a35bb00fb61d
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-08-12 09:14:10 +0000

    make tags

---
+++ tags/plugins/erasedata/3.1/lang/fr.js
@@ -0,0 +1,13 @@
+/*
+ * PLUGIN ERASEDATA
+ *
+ * File Name: fr.js
+ * 	French language file.
+ *
+ * File Author:
+ *    Nicobubulle (nicobubulle@gmail.com)
+ */
+ 
+ theUILang.erasedataRunNotAvailable     = "Plugin 'Erasedata': rTorrent ne peut pas acc\u00e9der au fichier plugins/erasedata/cleanup.sh en Lecture/Execution. Le plugin ne fonctionnera pas.";
+
+thePlugins.get("erasedata").langLoaded();
