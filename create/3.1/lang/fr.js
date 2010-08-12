4860fabf02598e99f93c0af68c82146ec36a0d5f
novik65 <Novik@users.noreply.github.com>
2010-08-12 09:11:54 +0000

    make tags

---
+++ create/3.1/lang/fr.js
@@ -0,0 +1,31 @@
+/*
+ * PLUGIN CREATE
+ * 
+ * File Name: fr.js
+ * 	French language file.
+ * 
+ * File Author:
+ *    Nicobubulle (nicobubulle@gmail.com)
+ */
+ 
+ theUILang.mnu_create                   = "Cr\u00e9er un torrent...";
+ theUILang.CreateNewTorrent             = "Cr\u00e9er un nouveau Torrent";
+ theUILang.SelectSource                 = "S\u00e9lectionner la source";
+ theUILang.TorrentProperties            = "Propri\u00e9t\u00e9s du torrent";
+ theUILang.PieceSize                    = "Taille des pi\u00e8ces";
+ theUILang.Other                        = "Autre";
+ theUILang.StartSeeding                 = "Mettre en seed";
+ theUILang.PrivateTorrent               = "Tracker priv\u00e9";
+ theUILang.torrentCreate                = "Cr\u00e9er...";
+ theUILang.BadTorrentData               = "Vous devez remplir tous les champs requis";
+ theUILang.createExternalNotFound       = "Plugin 'Create' : Le plugin ne fonctionnera pas. Le serveur Web n'a pas acc\u00e8s au programme externe.";
+ theUILang.incorrectDirectory           = "Mauvais r\u00e9pertoire";
+ theUILang.cantExecExternal             = "Ne peut pas ex\u00e9cuter le programme externe";
+ theUILang.createConsole                = "Console";
+ theUILang.createErrors                 = "Erreurs";
+ theUILang.torrentSave                  = "Enregistrer";
+ theUILang.torrentKill                  = "Stop";
+ theUILang.torrentKilled                = "Le processus a \u00e9t\u00e9 interrompu.";
+
+
+thePlugins.get("create").langLoaded();
