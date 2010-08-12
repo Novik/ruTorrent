fcd7e46379be477ecc55451ee653e907b8c8f365
novik65 <Novik@users.noreply.github.com>
2010-08-12 09:13:46 +0000

    make tags

---
+++ edit/3.1/lang/fr.js
@@ -0,0 +1,19 @@
+/*
+ * PLUGIN EDIT
+ * 
+ * File Name: fr.js
+ * 	French language file.
+ * 
+ * File Author:
+ *    Nicobubulle (nicobubulle@gmail.com)
+ */
+
+ 
+ theUILang.EditTrackers 		= "Editer le torrent...";
+ theUILang.EditTorrentProperties 	= "Propri\u00e9t\u00e9s du torrent";
+ theUILang.errorAddTorrent	= "Erreur lors de l'ajout du .torrent";
+ theUILang.errorWriteTorrent	= "Erreur lors de l'\u00e9criture du .torrent";
+ theUILang.errorReadTorrent	= "Erreur lors de la lecture du .torrent";
+ theUILang.cantFindTorrent      = "Le fichier source pour ce torrent n'a pas \u00e9t\u00e9 trouv\u00e9."
+
+thePlugins.get("edit").langLoaded();
