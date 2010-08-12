e6094c46be66b5a26c23c838c0a089057d074fc0
novik65 <Novik@users.noreply.github.com>
2010-08-12 09:16:33 +0000

    make tags

---
+++ source/3.1/lang/fr.js
@@ -0,0 +1,14 @@
+/*
+ * PLUGIN SOURCE
+ *
+ * File Name: fr.js
+ * 	French language file.
+ *
+ * File Author:
+ *    Nicobubulle (nicobubulle@gmail.com)
+ */
+
+ theUILang.getSource            = "T\u00e9l\u00e9charger le .torrent";
+ theUILang.cantFindTorrent      = "Le fichier source du torrent n'a pas \u00e9t\u00e9 trouv\u00e9.";
+
+thePlugins.get("source").langLoaded();
