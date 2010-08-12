5c1dfbefbd609a330dc1e11ff8dc6dabd3e4baaf
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-08-12 09:13:16 +0000

    make tags

---
+++ tags/plugins/datadir/3.1/lang/fr.js
@@ -0,0 +1,17 @@
+/*
+ * PLUGIN DATADIR
+ * 
+ * File Name: fr.js
+ * 	French language file.
+ * 
+ * File Author:
+ *    Nicobubulle (nicobubulle@gmail.com)
+ */
+
+ theUILang.DataDir		= "Enregistrer sous";
+ theUILang.DataDirMove		= "D\u00e9placer les fichiers";
+ theUILang.datadirDlgCaption	= "R\u00e9pertoire du torrent";
+ theUILang.datadirDirNotFound	= "Plugin 'DataDir': R\u00e9pertoire invalide";
+ theUILang.datadirSetDirFail	= "Plugin 'DataDir': L'op\u00e9ration a \u00e9chou\u00e9e";
+
+thePlugins.get("datadir").langLoaded();
