aba32eb726c2f2a4be2b9a5846ae1336434ba835
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-08-12 09:16:07 +0000

    make tags

---
+++ tags/plugins/seedingtime/3.1/lang/fr.js
@@ -0,0 +1,15 @@
+/*
+ * PLUGIN SeedingTime
+ *
+ * File Name: fr.js
+ * 	French language file.
+ *
+ * File Author:
+ *    Nicobubulle (nicobubulle@gmail.com)
+ */
+
+ theUILang.seedingTimeBadVersion        = "Plugin 'SeedingTime': Cette version de rTorrent ne supporte pas ce plugin. Le plugin ne fonctionnera pas.";
+ theUILang.seedingTime          = "Termin\u00e9";
+ theUILang.addTime              = "Ajout\u00e9";
+
+thePlugins.get("seedingtime").langLoaded();
---
+++ tags/plugins/seedingtime/3.1/lang/sr.js
@@ -0,0 +1,7 @@
+﻿
+ 
+ theUILang.seedingTimeBadVersion	= "Додатак SeedingTime: Верзија rTorrent-а коју ви користите не подржава овај додатак.";
+ theUILang.seedingTime		= "Завршено";
+ theUILang.addTime		= "Added";
+
+thePlugins.get("seedingtime").langLoaded();
