6b267c25c6af371360cadb477a35ce1923b0fd52
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-08-12 09:10:59 +0000

    make tags

---
+++ tags/plugins/chunks/3.1/lang/fr.js
@@ -0,0 +1,19 @@
+/*
+ * PLUGIN CHUNKS
+ * 
+ * File Name: fr.js
+ * 	French language file.
+ * 
+ * File Author:
+ *    Nicobubulle (nicobubulle@gmail.com)
+ */
+
+theUILang.Chunks = "Chunks";
+theUILang.chunksNoChunksFound = "Pas de chunks trouv\u00e9s!";
+theUILang.chunksRows = "ligne(s)";
+theUILang.chunksColumns = "colonne(s)";
+theUILang.chunksChunks = "chunk(s)";
+theUILang.chunksSize = "taille d'un chunk";
+theUILang.chunksNotSupported = "Votre version de rTorrent doit \u00eatre >= 0.8.5 pour voir la progression des chunks. Votre version : ";
+
+thePlugins.get("chunks").langLoaded();
