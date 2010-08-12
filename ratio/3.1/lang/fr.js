99ab58794ecf7095f932d4fb1bd590e8cead41d8
novik65 <Novik@users.noreply.github.com>
2010-08-12 09:14:40 +0000

    make tags

---
+++ ratio/3.1/lang/fr.js
@@ -0,0 +1,26 @@
+/*
+ * PLUGIN RATIO
+ * 
+ * File Name: fr.js
+ * 	French language file.
+ * 
+ * File Author:
+ *    Nicobubulle (nicobubulle@gmail.com)
+ */
+
+ 
+ theUILang.ratioUnsupported     = "Plugin 'Ratio': cette version de rTorrent ne support pas 'ratio'. Le plugin ne fonctionnera pas.";
+ theUILang.ratios			= "Groupes de ratio";
+ theUILang.ratio			= "Groupe de ratio";
+ theUILang.mnuRatio		= "Affecter a un groupe de ratio";
+ theUILang.mnuRatioUnlimited	= "Pas de ratio";
+ theUILang.ratioName 		= "Nom";
+ theUILang.minRatio 		= "Min";
+ theUILang.maxRatio 		= "Max";
+ theUILang.ratioUpload 		= "Envoi";
+ theUILang.ratioAction 		= "Action";
+ theUILang.ratioStop 		= "Arr\u00eater";
+ theUILang.ratioStopAndRemove 	= "Arr\u00eater et retirer du groupe";
+ theUILang.ratioErase 		= "Effacer";
+
+thePlugins.get("ratio").langLoaded();
