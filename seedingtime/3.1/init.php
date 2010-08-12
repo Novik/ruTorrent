0c4bed76d47eb7658ba4979d035b63ccd4cda6c2
novik65 <Novik@users.noreply.github.com>
2010-08-12 09:16:07 +0000

    make tags

---
+++ seedingtime/3.1/init.php
@@ -0,0 +1,18 @@
+<?php
+
+if( $theSettings->iVersion >= 0x806 )
+{
+	$req = new rXMLRPCRequest( array(
+		$theSettings->getOnFinishedCommand(array("seedingtime".getUser(),
+			getCmd('d.set_custom').'=seedingtime,"$'.getCmd('execute_capture').'={date,+%s}"')),
+		$theSettings->getOnInsertCommand(array("addtime".getUser(),
+			getCmd('d.set_custom').'=addtime,"$'.getCmd('execute_capture').'={date,+%s}"'))
+		));
+	if($req->success())
+	        $theSettings->registerPlugin("seedingtime");
+	else
+	        $jResult .= "plugin.disable(); log('seedingtime: '+theUILang.pluginCantStart);";
+}
+else
+	$jResult .= "plugin.disable(); plugin.showError('theUILang.seedingTimeBadVersion');";
+?>
