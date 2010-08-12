2579bae556d239bd85d7cfbfe17d9c8fa90c4cc9
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-08-12 09:15:39 +0000

    make tags

---
+++ tags/plugins/scheduler/3.1/lang/fr.js
@@ -0,0 +1,35 @@
+/*
+ * PLUGIN SCHEDULER
+ *
+ * File Name: fr.js
+ * 	French language file.
+ *
+ * File Author:
+ *    Nicobubulle (nicobubulle@gmail.com)
+ */
+ 
+ theUILang.scheduler                    = "Planificateur";
+ theUILang.schedulerOn                  = "Activer le planificateur";
+ theUILang.schedulerGraph                       = "Scheduler Table";
+ theUILang.schShortWeek                 = [ "Lun", "Mar", "Mer", "Jeu", "Vre", "Sam", "Dim" ];
+ theUILang.schFullWeek                  = [ "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" ];
+
+ theUILang.schUnlimited                 = "Illimit\u00e9";
+ theUILang.schLimited                   = "Limit\u00e9";
+ theUILang.schTurnOff                   = "Eteins";
+ theUILang.schSeedingOnly                       = "Seulement en Seed";
+
+ theUILang.schUnlimitedDesc             = "Illimit\u00e9 - Sans limite globale";
+ theUILang.schLimitedDesc                       = "Limit\u00e9 - Utilise les limites sp\u00e9cifiques du planificateur";
+ theUILang.schTurnOffDesc                       = "Eteins - Arrete tous les torrents";
+ theUILang.schSeedingOnlyDesc           = "Seulement en Seed - Partage uniquement les donn\u00e9es";
+
+ theUILang.schLimitedUL                 = "Envoi limit\u00e9";
+ theUILang.schLimitedDL                 = "R\u00e9ception limit\u00e9e";
+
+ theUILang.schedPHPNotFound             = "Plugin 'Scheduler': rTorrent ne peut pas acc\u00e9der \u00e0 l'interpr\u00e9teur php. Le plugin ne fonctionnera pas.";
+ theUILang.schedUpdaterNotAvailable     = "Plugin 'Scheduler': rTorrent ne peut pas acc\u00e9der au fichier plugins/scheduler/update.php en Lecture. Le plugin ne fonctionnera pas.";
+
+ theUILang.shcIgnore			= "Ignore scheduler";
+
+thePlugins.get("scheduler").langLoaded();
