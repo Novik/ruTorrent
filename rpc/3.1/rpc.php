22e3e892c20f7df98669dff68aeac0a53187b29b
novik65 <Novik@users.noreply.github.com>
2010-08-12 09:15:08 +0000

    make tags

---
+++ rpc/3.1/rpc.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once( '../../php/xmlrpc.php' );
+
+if (!isset($HTTP_RAW_POST_DATA))
+	$HTTP_RAW_POST_DATA = file_get_contents("php://input");
+
+if(isset($HTTP_RAW_POST_DATA) && (strpos($HTTP_RAW_POST_DATA,"execute")===false))
+{
+	$result = rXMLRPCRequest::send($HTTP_RAW_POST_DATA);
+	if(!empty($result))
+	{
+		$pos = strpos($result, "\r\n\r\n");
+		if($pos !== false)
+			$result = substr($result,$pos+4);
+		cachedEcho($result, "text/xml");
+	}
+}
+
+header("HTTP/1.0 500 Server Error");
+cachedEcho("Link to XMLRPC failed. May be, rTorrent is down?","text/html");
+
+?>
