745b3401a31cc65d38e801cbcd8e15523dd4d003
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-08-17 14:00:34 +0000

    minor fix

--- trunk/feeds/lang/lv.js
+++
@@ -1,9 +0,0 @@
-﻿
-theUILang.feedAll		= "All torrents";
-theUILang.feedCompleted		= "Completed torrents";
-theUILang.feedDownloading	= "Downloading torrents";
-theUILang.feedActive		= "Active torrents";
-theUILang.feedInactive		= "Inactive torrents";
-theUILang.feedError		= "Error torrents";
-
-thePlugins.get("feeds").langLoaded();
