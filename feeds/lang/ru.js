930c0a4a452a99c24cb2e232fedb068a92760512
novik65 <Novik@users.noreply.github.com>
2010-08-17 14:00:34 +0000

    minor fix

--- feeds/lang/ru.js
+++
@@ -1,9 +0,0 @@
-﻿
-theUILang.feedAll		= "Все закачки";
-theUILang.feedCompleted		= "Завершенные закачки";
-theUILang.feedDownloading	= "Загружаемые закачки";
-theUILang.feedActive		= "Активные закачки";
-theUILang.feedInactive		= "Неактивные закачки";
-theUILang.feedError		= "Закачки с ошибками";
-
-thePlugins.get("feeds").langLoaded();
