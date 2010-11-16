c39a47bc915ddf4307f8284cfdb52691f6d37cf0
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-11-16 11:37:14 +0000

    make tags

---
+++ tags/plugins/loginmgr/3.2/accounts/PlayTheNet.php
@@ -0,0 +1,31 @@
+<?php
+
+class PlayTheNetAccount extends commonAccount
+{
+	protected function isOK($client)
+	{
+		return(strpos($client->results, '<input type="password" name="pass"')===false);
+	}
+	protected function login($client,$login,$password,&$url,&$method,&$content_type,&$body)
+	{                                                                   
+		if($client->fetch( "https://www.play-the.net/?section=LOGIN&Func=access_denied" ))
+		{
+                        $client->setcookies();
+			$client->referer = "https://www.play-the.net/?section=LOGIN&Func=access_denied";
+        		if($client->fetch( "https://www.play-the.net/?section=LOGIN&type=0","POST","application/x-www-form-urlencoded", 
+				"user=".rawurlencode($login)."&pass=".rawurlencode($password)."&Connexion=%C9tablir+la+Connexion" ))
+			{
+				$client->referer = "https://www.play-the.net/?section=LOGIN&type=0";
+				$client->setcookies();
+				return(true);
+			}
+		}
+		return(false);
+	}
+	public function test($url)
+	{
+		return(preg_match( "/^http(s)?:\/\/www\.play\-the\.net\//si", $url ));
+	}
+}
+
+?>
