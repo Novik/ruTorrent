735f9b8c023f9ed8c6b5aa886b6e2bb0c8597709
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-11-16 11:35:39 +0000

    make tags

---
+++ tags/plugins/extsearch/3.2/action.php
@@ -0,0 +1,89 @@
+<?php
+require_once( dirname(__FILE__)."/../../php/util.php" );
+require_once( dirname(__FILE__)."/../../php/rtorrent.php" );
+require_once( "engines.php" );
+
+set_time_limit(0);
+$em = engineManager::load();
+if($em===false)
+{
+	$em = new engineManager();
+	$em->obtain("./engines");
+}
+
+if(isset($_REQUEST['mode']))
+{
+	$cmd = $_REQUEST['mode'];
+	switch($cmd)
+	{
+		case "set":
+		{
+			$em->set();
+			cachedEcho($em->get(),"application/javascript");
+			break;
+		}
+		case "get":
+		{
+			cachedEcho($em->action( $_REQUEST['eng'], $_REQUEST['what'], $_REQUEST['cat'] ),"application/json");
+			break;
+		}
+		case "loadtorrents":
+		{
+			if(!isset($HTTP_RAW_POST_DATA))
+				$HTTP_RAW_POST_DATA = file_get_contents("php://input");
+			if(isset($HTTP_RAW_POST_DATA))
+			{
+				$vars = explode('&', $HTTP_RAW_POST_DATA);
+				$lbl = null;
+				$dir = null;
+				$isStart = true; 
+				$isAddPath = true;
+				$fast = false;
+				$urls = array();
+				$engs = array();
+				$ndx = array();
+				$teg = '';
+				foreach($vars as $var)
+				{
+					$parts = explode("=",$var);
+					if($parts[0]=="torrents_start_stopped")
+						$isStart = false;
+					else
+					if($parts[0]=="not_add_path")
+						$isAddPath = false;
+					else
+					if($parts[0]=="dir_edit")
+						$dir = rawurldecode($parts[1]);
+					else
+					if($parts[0]=="label")
+						$lbl = rawurldecode($parts[1]);
+					else
+					if($parts[0]=="fast_resume")
+						$fast = $parts[1];
+					else
+					if($parts[0]=="teg")
+						$teg = $parts[1];
+					else
+					if($parts[0]=="url")
+						$urls[] = rawurldecode($parts[1]);
+					else
+					if($parts[0]=="eng")
+						$engs[] = $parts[1];
+					else
+					if($parts[0]=="ndx")
+						$ndx[] = $parts[1];
+				}
+				$status = $em->getTorrents( $engs, $urls, $isStart, $isAddPath, $dir, $lbl, $fast );
+				$ret = '{ "teg": "'.$teg.'", "data": [';
+				for($i = 0; $i< count($status); $i++)
+					$ret .= '{ "hash" : "'.$status[$i].'", "ndx": '.$ndx[$i].' },';
+				$len = strlen($ret);
+				if($ret[$len-1]==',')
+					$ret = substr($ret,0,$len-1);
+				cachedEcho($ret.']}',"application/json");
+			}
+			break;
+		}
+	}
+}
+?>
---
+++ tags/plugins/extsearch/3.2/engines/KickAssTorrents.php
@@ -0,0 +1,60 @@
+<?php
+
+class KickAssTorrentsEngine extends commonEngine
+{
+	public $defaults = array( "public"=>true, "page_size"=>25 );
+	public $categories = array( 'all'=>'', 'Anime'=>'anime', 'Applications'=>'applications', 'Books'=>'books', 'Games'=>'games', 'Movies'=>'movies', 
+		'Music'=>'music', 'Other'=>'other', 'TV'=>'tv', 'XXX'=>'xxx' );
+
+	public function action($what,$cat,&$ret,$limit,$useGlobalCats)
+	{
+		$added = 0;
+		$url = 'http://www.kickasstorrents.com';
+		if($useGlobalCats)
+			$categories = array( 'all'=>'', 'movies'=>'movies', 'tv'=>'tv', 'music'=>'music', 'games'=>'games', 'anime'=>'anime', 'software'=>'applications', 'books'=>'books' );
+		else
+			$categories = &$this->categories;
+		if(!array_key_exists($cat,$categories))
+			$cat = $categories['all'];
+		else
+			$cat = $categories[$cat];
+
+		for($pg = 1; $pg<11; $pg++)
+		{
+			$cli = $this->fetch( $url.'/search/'.$what.'/'.$pg.'/?categories[]='.$cat.'&field=seeders&sorder=desc&json=1' );
+			if($cli==false || 
+				!preg_match('/"total_results": (?P<cnt>\d+)/siU',$cli->results, $matches) ||
+				(intval($matches["cnt"])<=0))
+				break;
+			$res = preg_match_all('/[\,\[]\{\n"title": "(?P<name>.*)",\n"category": "(?P<cat>.*)",\n"link": "(?P<desc>.*)",.*'.
+				'"pubDate": "(?P<date>.*)",\n"torrentLink": "(?P<link>.*)",.*'.
+				'"seeds": "(?P<seeds>.*)",\n"leechs": "(?P<leech>.*)",\n"size": "(?P<size>.*)",.*\}/siU', $cli->results, $matches);
+			if($res)
+			{
+				for($i=0; $i<$res; $i++)
+				{
+					$link = $matches["link"][$i];
+					if(!array_key_exists($link,$ret) && intval($matches["seeds"][$i]))
+					{
+						$item = $this->getNewEntry();
+						$item["desc"] = $matches["desc"][$i];
+						$item["name"] = self::removeTags($matches["name"][$i]);
+						$item["size"] = $matches["size"][$i];
+						$item["seeds"] = intval($matches["seeds"][$i]);
+						$item["peers"] = intval($matches["leech"][$i]);
+						$item["time"] = strtotime($matches["date"][$i]);
+						$item["cat"] = self::removeTags($matches["cat"][$i]);
+						$ret[$link] = $item;
+						$added++;
+						if($added>=$limit)
+							return;
+					}
+				}
+			}
+			else
+				break;
+		}
+	}
+}
+
+?>
