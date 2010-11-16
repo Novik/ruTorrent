94fae8fe8e9345d3758e31c7d08bce4e866fcc2d
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-11-16 11:38:33 +0000

    make tags

---
+++ tags/plugins/rssurlrewrite/3.2/lang/en.js
@@ -0,0 +1,21 @@
+﻿
+ theUILang.rssNewRule		= "New rule";
+ theUILang.mnu_rssurlrewrite	= "Rules of URL replacement in RSS";
+ theUILang.rssRulesManager	= "Rules manager";
+ theUILang.rssAddRule		= "Add";
+ theUILang.rssDelRule		= "Delete";
+ theUILang.rssCheckRule		= "?";
+ theUILang.rssRulesLegend	= "Rule settings";
+ theUILang.rssSrcHref		= "If URL of torrent's load match pattern";
+ theUILang.rssSrcGuid		= "If URL of torrent's description match pattern";
+ theUILang.rssDstHref		= "then replace URL of torrent's load to";
+ theUILang.rssDstGuid		= "then replace URL of torrent's description to";
+ theUILang.rssRulesDebug	= "Rule debug";
+ theUILang.rssTestString	= "Test";
+ theUILang.rssTestResult	= "Result";
+ theUILang.rssURLInfo		= "URL info";
+ theUILang.rssURLGUID		= "Description URL";
+ theUILang.rssURLHref           = "Load URL";
+ theUILang.rssPatternError	= "Error in pattern string.";
+ 
+thePlugins.get("rssurlrewrite").langLoaded();
