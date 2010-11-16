2c48c390d747f87002e70fa722ef0410c37bff8c
novik65 <Novik@users.noreply.github.com>
2010-11-16 11:38:33 +0000

    make tags

---
+++ rssurlrewrite/3.2/lang/zh-cn.js
@@ -0,0 +1,21 @@
+﻿
+ theUILang.rssNewRule		= "新建规则";
+ theUILang.mnu_rssurlrewrite	= "替换 RSS 中 URL 的规则";
+ theUILang.rssRulesManager	= "规则管理";
+ theUILang.rssAddRule		= "添加";
+ theUILang.rssDelRule		= "删除";
+ theUILang.rssCheckRule		= "?";
+ theUILang.rssRulesLegend	= "规则设置";
+ theUILang.rssSrcHref		= "If URL of torrent's load match pattern";
+ theUILang.rssSrcGuid		= "If URL of torrent's description match pattern";
+ theUILang.rssDstHref		= "then replace URL of torrent's load to";
+ theUILang.rssDstGuid		= "then replace URL of torrent's description to";
+ theUILang.rssRulesDebug	= "规则调试";
+ theUILang.rssTestString	= "测试";
+ theUILang.rssTestResult	= "结果";
+ theUILang.rssURLInfo		= "URL 信息";
+ theUILang.rssURLGUID		= "描述 URL";
+ theUILang.rssURLHref           = "载入 URL";
+ theUILang.rssPatternError	= "模式串中有错误.";
+ 
+thePlugins.get("rssurlrewrite").langLoaded();
