6236141a85a74ed4f5ba2facd8d9110f54e28989
novik65 <Novik@users.noreply.github.com>
2010-11-16 11:45:02 +0000

    make tags

---
+++ 3.2/rutorrent/php/doneplugins.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once( "xmlrpc.php" );
+require_once( 'settings.php' );
+
+$theSettings = rTorrentSettings::load();
+$jResult = "";
+
+if(!isset($HTTP_RAW_POST_DATA))
+	$HTTP_RAW_POST_DATA = file_get_contents("php://input");
+if(isset($HTTP_RAW_POST_DATA))
+{
+	$vars = explode('&', $HTTP_RAW_POST_DATA);
+	foreach($vars as $var)
+	{
+		$parts = explode("=",$var);
+		if($parts[0]=="plg")
+		{
+			$php = "../plugins/".$parts[1]."/done.php";
+			if(is_file($php) && is_readable($php))
+				require_once($php);
+			$theSettings->unregisterPlugin($parts[1]);
+			$jResult.="thePlugins.get('".$parts[1]."').remove();";
+		}
+	}
+	$theSettings->store();
+}
+
+echo $jResult;
+
+?>
