e338e79539414a58c98130c50d4f76b130eb3181
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2010-11-16 11:36:24 +0000

    make tags

---
+++ tags/plugins/geoip/3.2/lang/nl.js
@@ -0,0 +1,283 @@
+
+// Contributed by art.spb, December 2009.
+
+thePlugins.get("geoip").langLoaded();
+
+theUILang.countryName = "Land";
+
+theUILang.country = new Array ();
+
+theUILang.country ["ad"] = "Andorra";
+theUILang.country ["ae"] = "Verenigde Arabische Emiraten";
+theUILang.country ["AF"] = "Afghanistan";
+theUILang.country ["AG"] = "Antigua en Barbuda";
+theUILang.country ["ai"] = "Anguilla";
+theUILang.country ["al"] = "Albanië";
+theUILang.country ["am"] = "Armenië";
+theUILang.country ["een"] = "Nederlandse Antillen";
+theUILang.country ["oa"] = "Angola";
+theUILang.country ["aq"] = "Antarctica";
+theUILang.country ["ar"] = "Argentinië";
+theUILang.country ["als"] = "Amerikaans-Samoa";
+theUILang.country ["at"] = "Oostenrijk";
+theUILang.country ["au"] = "Australia";
+theUILang.country ["aw"] = "Aruba";
+theUILang.country ["ax"] = "Åland-eilanden";
+theUILang.country ["az"] = "Azerbeidzjan";
+
+theUILang.country ["BA"] = "Bosnië en Herzegovina";
+theUILang.country ["bb"] = "Barbados";
+theUILang.country ["bd"] = "Bangladesh";
+theUILang.country ["worden"] = "België";
+theUILang.country ["bf"] = "Burkina Faso";
+theUILang.country ["bg"] = "Bulgarije";
+theUILang.country ["bh"] = "Bahrein";
+theUILang.country ["bi"] = "Burundi";
+theUILang.country ["bj"] = "Benin";
+theUILang.country ["bm"] = "Bermuda";
+theUILang.country ["bn"] = "Brunei Darussalam";
+theUILang.country ["bo"] = "Bolivia, multinationale staat van";
+theUILang.country ["br"] = "Brazilië";
+theUILang.country ["bs"] = "Bahama's";
+theUILang.country ["BT"] = "Bhutan";
+theUILang.country ["bv"] = "Bouvet Island";
+theUILang.country ["bw"] = "Botswana";
+theUILang.country ["door"] = "Wit-Rusland";
+theUILang.country ["bz"] = "Belize";
+
+theUILang.country ["ca"] = "Canada";
+theUILang.country ["cc"] = "Cocos (Keeling) Islands";
+theUILang.country ["cd"] = "Congo, de Democratische Republiek van de";
+theUILang.country ["cf"] = "Centraal-Afrikaanse Republiek";
+theUILang.country ["CG"] = "Congo";
+theUILang.country ["ch"] = "Zwitserland";
+theUILang.country ["ci"] = "Ivoorkust";
+theUILang.country ["ck"] = "Cook Islands";
+theUILang.country ["cl"] = "Chili";
+theUILang.country ["cm"] = "Kameroen";
+theUILang.country ["cn"] = "China";
+theUILang.country ["co"] = "Colombia";
+theUILang.country ["CR"] = "Costa Rica";
+theUILang.country ["cu"] = "Cuba";
+theUILang.country ["cv"] = "Kaapverdië";
+theUILang.country ["cx"] = "Christmas Island";
+theUILang.country ["CY"] = "Cyprus";
+theUILang.country ["cz"] = "Tsjechië";
+
+theUILang.country ["de"] = "Duitsland";
+theUILang.country ["dj"] = "Djibouti";
+theUILang.country ["DK"] = "Denemarken";
+theUILang.country ["dm"] = "Dominica";
+theUILang.country ["doen"] = "Dominicaanse Republiek";
+theUILang.country ["dz"] = "Algerije";
+
+theUILang.country ["ec"] = "Ecuador";
+theUILang.country ["ee"] = "Estland";
+theUILang.country ["bv"] = "Egypte";
+theUILang.country ["eh"] = "Westelijke Sahara";
+theUILang.country ["ER"] = "Eritrea";
+theUILang.country ["es"] = "Spanje";
+theUILang.country ["et"] = "Ethiopië";
+theUILang.country ["eu"] = "Europese Unie";
+
+theUILang.country ["fi"] = "Finland";
+theUILang.country ["fj"] = "Fiji";
+theUILang.country ["fk"] = "Falkland Eilanden (Malvinas)";
+theUILang.country ["fm"] = "Micronesia, Federale Staten van";
+theUILang.country ["voor"] = "Faeröer";
+theUILang.country ["fr"] = "Frankrijk";
+
+theUILang.country ["GA"] = "Gabon";
+theUILang.country ["GB"] = "Verenigd Koninkrijk";
+theUILang.country ["gd"] = "Grenada";
+theUILang.country ["ge"] = "Georgia";
+theUILang.country ["GF"] = "Frans-Guyana";
+theUILang.country ["gg"] = "Guernsey";
+theUILang.country ["gh"] = "Ghana";
+theUILang.country ["gi"] = "Gibraltar";
+theUILang.country ["gl"] = "Groenland";
+theUILang.country ["gm"] = "Gambia";
+theUILang.country ["gn"] = "Guinea";
+theUILang.country ["gp"] = "Guadeloupe";
+theUILang.country ["gq"] = "Equatoriaal-Guinea";
+theUILang.country ["gr"] = "Griekenland";
+theUILang.country ["gs"] = "Zuid-Georgië en de Zuidelijke Sandwicheilanden";
+theUILang.country ["GT"] = "Guatemala";
+theUILang.country ["gu"] = "Guam";
+theUILang.country ["gw"] = "Guinee-Bissau";
+theUILang.country ["Gy"] = "Guyana";
+
+theUILang.country ["HK"] = "Hong Kong";
+theUILang.country ["hm"] = "Heard en McDonald eilanden";
+theUILang.country ["hn"] = "Honduras";
+theUILang.country ['hr'] = "Kroatië";
+theUILang.country ["ht"] = "Haïti";
+theUILang.country ["hu"] = "Hongarije";
+
+theUILang.country ["id"] = "Indonesië";
+theUILang.country ["ie"] = "Ierland";
+theUILang.country ["il"] = "Israël";
+theUILang.country ["im"] = "Isle of Man";
+theUILang.country ["in"] = "India";
+theUILang.country ["io"] = "British Indian Ocean Territory";
+theUILang.country ["iq"] = "Irak";
+theUILang.country ["ir"] = "Iran, Islamitische Republiek";
+theUILang.country ["is"] = "IJsland";
+theUILang.country ["het"] = "Italië";
+
+theUILang.country ["JE"] = "Jersey";
+theUILang.country ["jm"] = "Jamaica";
+theUILang.country ["jo"] = "Jordan"
+theUILang.country ["JP"] = "Japan";
+
+theUILang.country ["ke"] = "Kenia";
+theUILang.country ["kg"] = "Kirgizië";
+theUILang.country ["kh"] = "Cambodja";
+theUILang.country ["Ki"] = "Kiribati";
+theUILang.country ["km"] = "Comoren";
+theUILang.country ["kn"] = "Saint Kitts en Nevis";
+theUILang.country ["kp"] = "Korea, Democratische Volksrepubliek van";
+theUILang.country ["KR"] = "Korea, Republiek";
+theUILang.country ["kw"] = "Koeweit";
+theUILang.country ["ky"] = "Cayman Islands";
+theUILang.country ["KZ"] = "Kazachstan";
+
+theUILang.country ["la"] = "Lao Democratische Volksrepubliek";
+theUILang.country ["lb"] = "Libanon";
+theUILang.country ["LC"] = "Saint Lucia";
+theUILang.country ["li"] = "Liechtenstein";
+theUILang.country ["lk"] = "Sri Lanka";
+theUILang.country ["LR"] = "Liberia";
+theUILang.country ["ls"] = "Lesotho";
+theUILang.country ["LT"] = "Litouwen";
+theUILang.country ["lu"] = "Luxemburg";
+theUILang.country ["lv"] = "Letland";
+theUILang.country ["ly"] = "Libyan Arab Jamahiriya";
+
+theUILang.country ["ma"] = "Marokko";
+theUILang.country ["mc"] = "Monaco";
+theUILang.country ["md"] = "Moldavië";
+theUILang.country ["me"] = "Montenegro";
+theUILang.country ["mg"] = "Madagascar";
+theUILang.country ["mh"] = "Marshall Islands";
+theUILang.country ["mk"] = "Macedonië, de Voormalige Joegoslavische Republiek";
+theUILang.country ["ml"] = "Mali";
+theUILang.country ["mm"] = "Myanmar";
+theUILang.country ["mn"] = "Mongolië";
+theUILang.country ["mo"] = "Macau";
+theUILang.country ["mp"] = "Noordelijke Marianen";
+theUILang.country ["mq"] = "Martinique";
+theUILang.country ["de heer"] = "Mauritanië";
+theUILang.country ["ms"] = "Montserrat";
+theUILang.country ["mt"] = "Malta";
+theUILang.country ["mu"] = "Mauritius";
+theUILang.country ["mv"] = "Maldiven";
+theUILang.country ["mw"] = "Malawi";
+theUILang.country ["mx"] = "Mexico";
+theUILang.country ["mijn"] = "Maleisië";
+theUILang.country ["mz"] = "Mozambique";
+
+theUILang.country ["NA"] = "Namibië";
+theUILang.country ["nc"] = "Nieuw-Caledonië";
+theUILang.country ["ne"] = "Niger";
+theUILang.country ["nf"] = "Norfolk Island";
+theUILang.country ["ng"] = "Nigeria";
+theUILang.country ["Ni"] = "Nicaragua";
+theUILang.country ["nl"] = "Nederland";
+theUILang.country ["nee"] = "Noorwegen";
+theUILang.country ["NP"] = "Nepal";
+theUILang.country ["NR"] = "Nauru";
+theUILang.country ["nu"] = "Niue";
+theUILang.country ["nz"] = "Nieuw-Zeeland";
+
+theUILang.country ["OM"] = "Oman";
+
+theUILang.country ["pa"] = "Panama";
+theUILang.country ["ie"] = "Peru";
+theUILang.country ["pf"] = "Frans-Polynesië";
+theUILang.country ["pg"] = "Papoea-Nieuw-Guinea";
+theUILang.country ["nl"] = "Nederland";
+theUILang.country ["PK"] = "Pakistan";
+theUILang.country ["pl"] = "Polen";
+theUILang.country ["pm"] = "Saint-Pierre en Miquelon";
+theUILang.country ["pn"] = "Pitcairn";
+theUILang.country ["pr"] = "Puerto Rico";
+theUILang.country ["ps"] = "Palestijnse Bezette Gebieden";
+theUILang.country ["pt"] = "Portugal";
+theUILang.country ["pw"] = "Palau";
+theUILang.country ["py"] = "Paraguay";
+
+theUILang.country ["q"] = "Qatar";
+
+theUILang.country ["re"] = "Reunion";
+theUILang.country ["ro"] = "Roemenië";
+theUILang.country ["RS"] = "Servië";
+theUILang.country ["ru"] = "Russische Federatie";
+theUILang.country ["rw"] = "Rwanda";
+
+theUILang.country ["sa"] = "Saoedi-Arabië";
+theUILang.country ["SB"] = "Salomonseilanden";
+theUILang.country ["sc"] = "de Seychellen";
+theUILang.country ["sd"] = "Soedan";
+theUILang.country ["se"] = "Zweden"
+theUILang.country ["sg"] = "Singapore";
+theUILang.country ["sh"] = "Saint Helena";
+theUILang.country ["si"] = "Slovenië";
+theUILang.country ["sj"] = "Svalbard en Jan Mayen";
+theUILang.country ["sk"] = "Slowakije";
+theUILang.country ["sl"] = "Sierra Leone";
+theUILang.country ["sm"] = "San Marino";
+theUILang.country ["sn"] = "Senegal";
+theUILang.country ["dus"] = "Somalië";
+theUILang.country ["SR"] = "Suriname";
+theUILang.country ["st"] = "Sao Tome en Principe";
+theUILang.country ["sv"] = "El Salvador";
+theUILang.country ["sy"] = "Syrische Arabische Republiek";
+theUILang.country ["sz"] = "Swaziland";
+
+theUILang.country ["TC"] = "Turks-en Caicoseilanden";
+theUILang.country ["td"] = "Tsjaad";
+theUILang.country ["TF"] = "Franse Zuidelijke Gebieden";
+theUILang.country ["tg"] = "Togo";
+theUILang.country ["th"] = "Thailand";
+theUILang.country ["tj"] = "Tadzjikistan";
+theUILang.country ["tk"] = "Tokelau-eilanden";
+theUILang.country ["tl"] = "Oost-Timor";
+theUILang.country ["tm"] = "Turkmenistan";
+theUILang.country ["tn"] = "Tunesië";
+theUILang.country ['naar'] = "Tonga";
+theUILang.country [tr '] = "Turkije";
+theUILang.country ["TP"] = "Portugees Timor";
+theUILang.country ["tt"] = "Trinidad en Tobago";
+theUILang.country ["TV"] = "Tuvalu";
+theUILang.country ["tw"] = "Taiwan, Provincie van China";
+theUILang.country ['tz'] = "Tanzania, Verenigde Republiek van";
+
+theUILang.country ["ua"] = "Oekraïne";
+theUILang.country ["ug"] = "Uganda";
+theUILang.country ["uk"] = "Verenigd Koninkrijk";
+theUILang.country ["um"] = "Verenigde Staten Kleine afgelegen eilanden";
+theUILang.country ["un"] = "Onbekend";
+theUILang.country ["ons"] = "Verenigde Staten";
+theUILang.country ["uy"] = "Uruguay";
+theUILang.country ["uz"] = "Oezbekistan";
+
+theUILang.country ["VA"] = "Heilige Stoel (Vaticaanstad)";
+theUILang.country ["vc"] = "Saint Vincent en de Grenadines";
+theUILang.country ["ve"] = "Venezuela, Bolivariaanse Republiek van";
+theUILang.country ["VG"] = "Britse Maagdeneilanden";
+theUILang.country ["vi"] = "Amerikaanse Maagdeneilanden";
+theUILang.country ["vn"] = "Viet Nam";
+theUILang.country ["vu"] = "Vanuatu";
+
+theUILang.country ["wf"] = "Wallis en Futuna";
+theUILang.country ["was"] = "Samoa";
+
+theUILang.country ["gij"] = "Jemen";
+theUILang.country ["yt"] = "Mayotte";
+
+theUILang.country ["za"] = "Zuid-Afrika";
+theUILang.country ["zm"] = "Zambia";
+theUILang.country ["zw"] = "Zimbabwe";
+
+thePlugins.get ("geoip"). langLoaded ();
