d6baf7341f0b1a1465f4d79dad9c0d01ad016843
novik65 <Novik@users.noreply.github.com>
2010-11-23 09:46:47 +0000

    fix issue 510

--- rutorrent/js/webui.js
+++ rutorrent/js/webui.js
@@ -2098,7 +2098,7 @@ var theWebUI =
 	        if(w!==null)
 	        {
 			$("#CatList").width( w );
-			$("#VDivider").width( $(window).width()-w-5 );
+			$("#VDivider").width( $(window).width()-w-10 );
 		}
 		if(h!==null)
 		{
@@ -2151,7 +2151,7 @@ var theWebUI =
 		}
 		else
 		{
-			$("#VDivider").width( ww-w-5 );
+			$("#VDivider").width( ww-10 );
 			w = ww;
 		}
 		w-=11;
