ff39290f780755f24dd8d284225968242f6bde23
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2011-01-07 16:30:16 +0000

    minor fix

--- trunk/rutorrent/lang/da.js
+++ trunk/rutorrent/lang/da.js
@@ -385,6 +385,7 @@
  peerSnub			: "Snub",
  peerDetails			: "Details",
  peerUnsnub			: "Unsnub",
- peerAddLabel			: "Enter IP/Hostname[:port]"
+ peerAddLabel			: "Enter IP/Hostname[:port]",
+ noTorrentList			: "Torrent list not yet available, connection to rtorrent not established."
  };
 
