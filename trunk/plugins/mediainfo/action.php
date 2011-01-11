f0005a5d901049f08659d9f351e2c30eac8d1de6
mporter360 <mporter360@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2011-01-11 00:49:05 +0000

    minor fix (mediainfo plug-in)

--- trunk/plugins/mediainfo/action.php
+++ trunk/plugins/mediainfo/action.php
@@ -3,25 +3,24 @@ require_once( '../../php/xmlrpc.php' );
 eval( getPluginConf( 'mediainfo' ) );
 
 function mediaInfo() {
-        if(isset($_REQUEST['hash']) && isset($_REQUEST['no']))
-        {
-                                $req = new rXMLRPCRequest( array(
-                                        new rXMLRPCCommand( "d.get_base_path", $_REQUEST['hash'] ),
-                                        new rXMLRPCCommand( "f.get_path", array($_REQUEST['hash'],intval($_REQUEST['no'])))));
-                                if($req->success()) {
-                                        $dir = $req->val[0];
-                                        $filename = $req->val[1];
-                                                if($dir=='') {
-                                                        $req = new rXMLRPCRequest( array(
-                                                                new rXMLRPCCommand( "d.open", $_REQUEST['hash'] ),
-                                                                new rXMLRPCCommand( "d.get_base_path", $_REQUEST['hash'] ),
-                                                                new rXMLRPCCommand( "d.close", $_REQUEST['hash'] )));
-                                                        if($req->success())
-                                                                $dir = $req->val[1];
-                                                }
-                                        return(shell_exec(getExternal("mediainfo").' --Output=HTML "'.$dir."/".$filename.'"'));
-                                }
-        }
+	if(isset($_REQUEST['hash']) && isset($_REQUEST['no'])) {
+    	$req = new rXMLRPCRequest( 
+			new rXMLRPCCommand( "f.get_frozen_path", array($_REQUEST['hash'],intval($_REQUEST['no']))) );
+	if($req->success())
+	{
+		$filename = $req->val[0];
+		if($filename=='')
+		{
+			$req = new rXMLRPCRequest( array(
+				new rXMLRPCCommand( "d.open", $_REQUEST['hash'] ),
+				new rXMLRPCCommand( "f.get_frozen_path", array($_REQUEST['hash'],intval($_REQUEST['no'])) ),
+				new rXMLRPCCommand( "d.close", $_REQUEST['hash'] ) ) );
+			if($req->success())
+				$filename = $req->val[1];
+		}
+        return(shell_exec(getExternal("mediainfo").' --Output=HTML "'.$filename.'"'));
+      }
+	}
 return false;
 }
 echo mediaInfo();
