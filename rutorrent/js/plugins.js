095a307d0eecc22a20aa366564eb12ea14fc4fa4
novik65 <Novik@users.noreply.github.com>
2011-01-17 16:20:01 +0000

    minor changes

--- rutorrent/js/plugins.js
+++ rutorrent/js/plugins.js
@@ -237,20 +237,11 @@ rPlugin.prototype.attachPageToTabs = function(dlg,name,idBefore)
 		var newLbl = document.createElement("li");
 		newLbl.id = "tab_"+dlg.id;
 		newLbl.innerHTML = "<a href=\"javascript://void();\" onmousedown=\"theTabs.show('"+dlg.id+"');\" onfocus=\"this.blur();\">" + name + "</a>";
- 		var beforeTab = idBefore ? $$(idBefore) : null;
-	 	if(beforeTab)
- 		{
-			beforeTab.parentNode.insertBefore(dlg,beforeTab);
-			var beforeLbl = $$("tab_"+idBefore);
-			beforeLbl.parentNode.insertBefore(newLbl,beforeLbl);
-		}
-		else
-		{
-			beforeTab = $$("lcont");
-			beforeTab.parentNode.appendChild(dlg);
-			var beforeLbl = $$("tab_lcont");
-			beforeLbl.parentNode.appendChild(newLbl);
-		}
+		if(!idBefore)
+			idBefore = "lcont";
+		$$(idBefore).parentNode.insertBefore(dlg,$$(idBefore));
+		var beforeLbl = $$("tab_"+idBefore);
+		beforeLbl.parentNode.insertBefore(newLbl,beforeLbl);
 		theTabs.show("lcont");
 	}
 	return(this);
