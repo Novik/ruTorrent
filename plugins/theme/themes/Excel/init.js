1f179196120cd32bece4e98775953db675e21d80
novik65 <Novik@users.noreply.github.com>
2011-03-25 13:07:09 +0000

    category state now saved between UI loading

--- plugins/theme/themes/Excel/init.js
+++ plugins/theme/themes/Excel/init.js
@@ -2,10 +2,14 @@ plugin.oldAllDone = plugin.allDone;
 plugin.allDone = function()
 {
 	plugin.oldAllDone.call(this);
-
 	$(".catpanel").prepend(
 		$("<img></img>").attr( { src: "plugins/theme/themes/Excel/images/pnl_open.png", hspace: 10 } ).
 			css( { "margin-left": -31, "margin-top": 2, "vertical-align": -3 } ) );
+	$(".catpanel img").each( function()
+	{
+		var owner = $(this).parent()[0];
+		theWebUI.showPanel(owner,!theWebUI.settings["webui.closed_panels"][owner.id]);
+	});
 	$(".tabbar li:last-child a").after( $("<img></img>").attr( { src: $(".tabbar li:last-child").hasClass("selected") ? 
 		"plugins/theme/themes/Excel/images/tabbghfin.png" : 
 		"plugins/theme/themes/Excel/images/tabbgfin.png" } ).
@@ -22,15 +26,13 @@ theTabs.show = function(id)
 		"plugins/theme/themes/Excel/images/tabbgfin.png" } );
 }
 
-theWebUI.togglePanel = function(pnl)
+theWebUI.showPanel = function(pnl,enable)
 {
 	var cont = $('#'+pnl.id+"_cont");
-	cont.toggle();
-	if(cont.is(":visible"))
-		$('#'+pnl.id+" img").attr("src","plugins/theme/themes/Excel/images/pnl_open.png");
-	else
-		$('#'+pnl.id+" img").attr("src","plugins/theme/themes/Excel/images/pnl_close.png");
-}
+	cont.toggle(enable);
+	theWebUI.settings["webui.closed_panels"][pnl.id] = !enable;
+	$('#'+pnl.id+" img").attr("src",enable ? "plugins/theme/themes/Excel/images/pnl_open.png" : "plugins/theme/themes/Excel/images/pnl_close.png");
+},
 
 plugin.speedCreate = rSpeedGraph.prototype.create;
 rSpeedGraph.prototype.create = function( aOwner )
