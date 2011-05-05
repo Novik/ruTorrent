89072334e1b8b507d56c18e53b856091ef3ed81e
novik65 <Novik@users.noreply.github.com>
2011-05-05 08:13:26 +0000

    localization fixes

--- plugins/rssurlrewrite/lang/vi.js
+++ plugins/rssurlrewrite/lang/vi.js
@@ -1,15 +1,15 @@
-﻿
+ 
  theUILang.rssNewRule			= "Quy luật mới";
  theUILang.mnu_rssurlrewrite	= "Quy luật thay thế đường dẫn RSS";
  theUILang.rssRulesManager		= "Quản lý Quy luật";
  theUILang.rssAddRule			= "Thêm";
  theUILang.rssDelRule			= "Xóa";
- theUILang.rssCheckRule			= "?";
+ theUILang.rssCheckRule			= "Kiểm tra";
  theUILang.rssRulesLegend		= "Cài đặt quy luật";
- theUILang.rssSrcHref			= "If URL of torrent's load match pattern";
- theUILang.rssSrcGuid			= "If URL of torrent's description match pattern";
- theUILang.rssDstHref			= "then replace URL of torrent's load to";
- theUILang.rssDstGuid			= "then replace URL of torrent's description to";
+ theUILang.rssSrcHref			= "Nếu URL của torrent trùng với";
+ theUILang.rssSrcGuid			= "Nếu mô tả của của torrent trùng với";
+ theUILang.rssDstHref			= "thay thế URL của torrent thành";
+ theUILang.rssDstGuid			= "thay thế URL của torrent có mô tả đó thành";
  theUILang.rssRulesDebug		= "Bắt lỗi quy luật";
  theUILang.rssTestString		= "Kiểm tra";
  theUILang.rssTestResult		= "Kết quá";
