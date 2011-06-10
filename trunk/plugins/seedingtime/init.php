81fcdac5d0d6a9286422484cd975cb0a2ec5efd9
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2011-06-10 12:06:30 +0000

    minor fix

--- trunk/plugins/seedingtime/init.php
+++ trunk/plugins/seedingtime/init.php
@@ -6,7 +6,7 @@ $req = new rXMLRPCRequest( array(
 	$theSettings->getOnInsertCommand(array("addtime".getUser(),
 		getCmd('d.set_custom').'=addtime,"$'.getCmd('execute_capture').'={date,+%s}"')),
 	$theSettings->getOnHashdoneCommand(array("seedingtimecheck".getUser(),
-		getCmd('branch=').'$'.getCmd('d.get_complete=').'$'.getCmd('and').'=$'.
+		getCmd('branch=').'$'.getCmd('d.get_complete=').'$'.getCmd('and').'=$'.getCmd('not').'=$'.
 		getCmd('d.get_custom').'=seedingtime,"'.getCmd('d.set_custom').'=seedingtime,$'.getCmd('d.get_custom').'=addtime'.'"')),
 	));
 if($req->success())
