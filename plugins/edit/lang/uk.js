0cdb096b4b09d5d70225e94a52c9b62986957e6c
pfactum@gmail.com <pfactum@gmail.com>
2011-07-26 20:03:21 +0000

    l10n: update edit plugin Ukrainian localization

--- plugins/edit/lang/uk.js
+++ plugins/edit/lang/uk.js
@@ -1,10 +1,10 @@
 ﻿
  
- theUILang.EditTrackers 		= "Edit torrent...";
- theUILang.EditTorrentProperties 	= "Torrent Properties";
- theUILang.errorAddTorrent	= "Error adding torrent file";
- theUILang.errorWriteTorrent	= "Error writing torrent file";
- theUILang.errorReadTorrent	= "Error reading torrent file";
- theUILang.cantFindTorrent      = "Source torrent file for this download not found."
+ theUILang.EditTrackers 		= "Редагувати…";
+ theUILang.EditTorrentProperties 	= "Властивості торента";
+ theUILang.errorAddTorrent	= "Сталася помилка додавання торента";
+ theUILang.errorWriteTorrent	= "Сталася помилка записування у файл";
+ theUILang.errorReadTorrent	= "Сталася помилка читання з файлу";
+ theUILang.cantFindTorrent	= "Вихідний файл торента для цього завантаження не знайдено.";
 
 thePlugins.get("edit").langLoaded();
