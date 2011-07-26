b4e19a2ffc72dcb64e42f4265e22a6008a9924f9
pfactum@gmail.com <pfactum@gmail.com>
2011-07-26 18:21:48 +0000

    l10n: update autotools plugin Ukrainian localization

--- plugins/autotools/lang/uk.js
+++ plugins/autotools/lang/uk.js
@@ -1,15 +1,15 @@
 ﻿
 
- var s_PluginFail	= "Plugin will not work.";
+ var s_PluginFail	= "Плагін не працюватиме.";
 
- theUILang.autotools			= "Autotools";
- theUILang.autotoolsEnableLabel		= "Enable \"AutoLabel\" feature, Template:";
- theUILang.autotoolsEnableMove		= "Enable \"AutoMove\" feature";
- theUILang.autotoolsPathToFinished	= "Path to finished downloads";
- theUILang.autotoolsEnableWatch		= "Enable \"AutoWatch\" feature";
- theUILang.autotoolsPathToWatch		= "Path to base watch directory";
- theUILang.autotoolsWatchStart		= "Start download automatically";
- theUILang.autotoolsNoPathToFinished	= "Autotools plugin: path to finished downloads is not set. " + s_PluginFail;
- theUILang.autotoolsNoPathToWatch		= "Autotools plugin: path to base watch directory is not set. " + s_PluginFail;
+ theUILang.autotools			= "Автоматизація";
+ theUILang.autotoolsEnableLabel		= "Увімкнути функцію «АвтоМітки», Шаблон:";
+ theUILang.autotoolsEnableMove		= "Увімкнути функцію «АвтоПереміщення»";
+ theUILang.autotoolsPathToFinished	= "Каталог для завершених завантажень";
+ theUILang.autotoolsEnableWatch		= "Увімкнути функцію «АвтоДодавання»";
+ theUILang.autotoolsPathToWatch		= "Каталог для пошуку нових торентів";
+ theUILang.autotoolsWatchStart		= "Автоматично починати завантаження";
+ theUILang.autotoolsNoPathToFinished	= "Плагін autotools: каталог для завершених завантажень не задано. " + s_PluginFail;
+ theUILang.autotoolsNoPathToWatch		= "Плагін autotools: базовий каталог для нових торентів не задано. " + s_PluginFail;
 
 thePlugins.get("autotools").langLoaded();
