14b4eb615fc459a614d752d8010ee9e503137653
pfactum@gmail.com <pfactum@gmail.com>
2011-07-26 18:30:36 +0000

    l10n: update create plugin Ukrainian localization

--- plugins/create/lang/uk.js
+++ plugins/create/lang/uk.js
@@ -1,22 +1,22 @@
 ﻿
  
- theUILang.mnu_create 			= "Create torrent...";
- theUILang.CreateNewTorrent 		= "Create New Torrent";
- theUILang.SelectSource 		= "Select Source";
- theUILang.TorrentProperties 		= "Torrent Properties";
- theUILang.PieceSize 			= "Piece size";
- theUILang.Other 			= "Other";
- theUILang.StartSeeding 		= "Start seeding";
- theUILang.PrivateTorrent		= "Private torrent";
- theUILang.torrentCreate		= "Create...";
- theUILang.BadTorrentData		= "You must fill all required fields!";
- theUILang.createExternalNotFound 	= "Create plugin: Plugin will not work. Web-server can't access external program";
- theUILang.incorrectDirectory 		= "Incorrect directory";
- theUILang.cantExecExternal 		= "Can't execute external program";
- theUILang.createConsole		= "Console";
- theUILang.createErrors		 	= "Errors";
- theUILang.torrentSave		 	= "Save";
- theUILang.torrentKill		 	= "Stop";
- theUILang.torrentKilled	 	= "Process was stopped.";
+ theUILang.mnu_create 			= "Створити торент…";
+ theUILang.CreateNewTorrent	 	= "Створення нового торента";
+ theUILang.SelectSource 		= "Вибір джерела";
+ theUILang.TorrentProperties 		= "Властивості торента";
+ theUILang.PieceSize 			= "Розмір частини";
+ theUILang.Other 			= "Інше";
+ theUILang.StartSeeding 		= "Почати роздачу";
+ theUILang.PrivateTorrent		= "Приватний торент";
+ theUILang.torrentCreate		= "Створити…";
+ theUILang.BadTorrentData		= "Необхідно ввести дані в обов’язкові поля.";
+ theUILang.createExternalNotFound 	= "Плагін create: зовнішня програма не доступна для веб-сервера";
+ theUILang.incorrectDirectory 		= "Неправильний каталог";
+ theUILang.cantExecExternal 		= "Сталася помилка виконання зовнішньої програми";
+ theUILang.createConsole		= "Консоль";
+ theUILang.createErrors		 	= "Помилки";
+ theUILang.torrentSave		 	= "Зберегти";
+ theUILang.torrentKill		 	= "Зупинити";
+ theUILang.torrentKilled	 	= "Процес зупинено.";
 
 thePlugins.get("create").langLoaded();
