747d247c0f781c2a9d15b29a2fc0a16a779f3102
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2011-07-29 12:30:54 +0000

    make tags

---
+++ tags/plugins/data/3.3/data/lang/uk.js
@@ -0,0 +1,6 @@
+﻿
+
+ theUILang.getData		= "Отримати файл";
+ theUILang.cantAccessData	= "Користувач веб-сервера не має доступу до даних цього торента."; 
+
+thePlugins.get("data").langLoaded();
---
+++ tags/plugins/data/3.3/data/lang/vi.js
@@ -0,0 +1,6 @@
+﻿
+ 
+ theUILang.getData		= "Lấy dữ liệu";
+ theUILang.cantAccessData	= "Máy chủ Web không thể truy cập dữ liệu của torrent này.";
+
+thePlugins.get("data").langLoaded();
