4f7dc5e3af9e75548fb9bce9392dc381c5aff9a2
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2011-07-29 12:25:23 +0000

    make tags

---
+++ tags/plugins/unpack/3.3/unpack/lang/ru.js
@@ -0,0 +1,17 @@
+﻿
+
+theUILang.unpack		= "Распаковать";
+theUILang.unpackPath		= "Путь (пусто - в директорию закачки)";
+theUILang.processAll		= "Обрабатывать все тома";
+theUILang.unpackTaskFailed      = "Распаковка завершена с ошибками";
+theUILang.unpackTaskOK		= "Распаковка завершена удачно";
+theUILang.unpackTaskStarted	= "Распаковка начата";
+theUILang.unzipNotFound		= "Плагин unpack: Программа unzip недоступна пользователю rtorrent.";
+theUILang.unrarNotFound		= "Плагин unpack: Программа unrar недоступна пользователю rtorrent.";
+theUILang.unpackEnabled		= "Разрешить автораспаковку";
+theUILang.unpackTorrents	= "При распаковке данных всего торрента добавлять к пути";
+theUILang.unpackAddLabel	= "Метку торрента";
+theUILang.unpackAddName		= "Имя торрента";
+theUILang.unpackNoFiles		= "Файлы архивов отсутствуют в данных торрента.";
+
+thePlugins.get("unpack").langLoaded();
