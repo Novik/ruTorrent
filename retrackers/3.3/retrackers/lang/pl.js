be8234654e0b7e2280c9e361d17e497d57dfd21c
novik65 <Novik@users.noreply.github.com>
2011-07-29 12:27:58 +0000

    make tags

---
+++ retrackers/3.3/retrackers/lang/pl.js
@@ -0,0 +1,7 @@
+﻿
+ 
+ theUILang.retrackers		= "Retrackers";
+ theUILang.dontAddToPrivate	= "Nie dodawaj Retrackers do prywatnych trackerów";
+ theUILang.addToBegin		= "Add retrackers to the beginning of the trackers list";
+
+thePlugins.get("retrackers").langLoaded();
