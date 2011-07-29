b5689e3e01b6f4861ccff87cd1722ff53fab3bf2
novik65 <Novik@users.noreply.github.com>
2011-07-29 12:30:40 +0000

    make tags

---
+++ datadir/3.3/datadir/lang/vi.js
@@ -0,0 +1,10 @@
+﻿
+
+ theUILang.DataDir		= "Lưu vào";
+ theUILang.DataDirMove		= "Di chuyển các tập tin dữ liệu";
+ theUILang.datadirDlgCaption	= "Thư mục chứa dữ liệu Torrent";
+ theUILang.datadirDirNotFound	= "DataDir plugin: Thư mục không hợp lệ";
+ theUILang.datadirSetDirFail	= "DataDir plugin: Quá trình thực hiện thất bại";
+ theUILang.datadirPHPNotFound	= "DataDir plugin: rTorrent không thể truy cập trình thông dịch PHP. Thành phần bổ sung sẽ không hoạt động";
+
+thePlugins.get("datadir").langLoaded();
