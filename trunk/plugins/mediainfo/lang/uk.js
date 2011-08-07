8dd30cfd3381f3697b2662be51f2b3fa587d4284
pfactum@gmail.com <pfactum@gmail.com@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2011-08-07 18:12:17 +0000

    l10n: unify Ukrainian localization

--- trunk/plugins/mediainfo/lang/uk.js
+++ trunk/plugins/mediainfo/lang/uk.js
@@ -1,6 +1,6 @@
 theUILang.mediainfo			= "MediaInfo…";
 theUILang.mediaError		= "Помилка";
 theUILang.badMediaData 		= "Не вдалося отримати відомості про цей файл";
-theUILang.MediaInfoNotFound = "Плагін MediaInfo: програма MediaInfo не доступна користувачу веб-сервера.";
+theUILang.MediaInfoNotFound = "Плагін MediaInfo: програма MediaInfo недоступна користувачу веб-сервера.";
 
 thePlugins.get("mediainfo").langLoaded();
