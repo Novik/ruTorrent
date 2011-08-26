bdf303ed3a84ae1d22892c8533a1980cd52d338b
novik65 <Novik@users.noreply.github.com>
2011-08-26 14:21:16 +0000

    add plugin 'screenshots'

---
+++ plugins/screenshots/init.php
@@ -0,0 +1,10 @@
+<?php
+
+if(!$theSettings->isPluginRegistered("explorer"))
+	require_once( "ffmpeg.php" );
+
+$st = ffmpegSettings::load();
+$jResult.=("plugin.ffmpegSettings = ".json_encode($st->get()).";");
+$theSettings->registerPlugin("screenshots");
+
+?>
---
+++ plugins/screenshots/lang/lv.js
@@ -0,0 +1,13 @@
+﻿
+theUILang.exFFMPEG	= "Screenshots";
+theUILang.exFrameWidth = "Frame width";
+theUILang.exFramesCount = "Frames count";
+theUILang.exStartOffset = "Start frame offset";
+theUILang.exBetween = "Time between frames";
+theUILang.exSave = "Save";
+theUILang.exSaveAll = "Save all";
+theUILang.exScreenshot = "Screenshot";
+theUILang.exPlayInterval = "Slideshow interval";
+theUILang.exImageFormat	= "Image format";
+
+thePlugins.get("screenshots").langLoaded();
