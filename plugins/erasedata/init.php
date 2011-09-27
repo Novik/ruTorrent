a0f8ce1e63fd86aaf0b7d038bbbfba8b01a8bd71
novik65 <Novik@users.noreply.github.com>
2011-09-27 08:03:37 +0000

    fix plugin 'erasedata'

--- plugins/erasedata/init.php
+++ plugins/erasedata/init.php
@@ -9,10 +9,9 @@ $thisDir = dirname(__FILE__);
 
 $req = new rXMLRPCRequest( array(
 	$theSettings->getOnEraseCommand(array('erasedata0'.getUser(),
-		getCmd('branch').'=$'.getCmd('not').'=$'.getCmd('d.get_custom5').'=,,$'.getCmd('cat').'=$'.getCmd('d.open').'=,'.
-			'"$'.getCmd('f.multicall').'=,\"'.getCmd('execute').'={'.$thisDir.'/cat.sh,'.$listPath.',$'.getCmd('system.pid').'=,$'.getCmd('f.get_frozen_path').'=}\"",'.
-			'"$'.getCmd('execute').'={'.$thisDir.'/fin.sh,'.$listPath.',$'.getCmd('system.pid').'=,$'.getCmd('d.get_hash').'=,$'.getCmd('d.get_base_path').'=,$'.getCmd('d.is_multi_file').'=}"'
-		)),
+		getCmd('d.open').'= ; '.getCmd('branch=').getCmd('d.get_custom5').'=,"'.getCmd('f.multicall').'=,\"'.getCmd('execute').'={'.$thisDir.'/cat.sh,'.$listPath.',$system.pid=,$'.getCmd('f.get_frozen_path').'=}\""')),
+	$theSettings->getOnEraseCommand(array('erasedata1'.getUser(),
+		getCmd('branch=').getCmd('d.get_custom5').'=,"'.getCmd('execute').'={'.$thisDir.'/fin.sh,'.$listPath.',$'.getCmd('system.pid').'=,$'.getCmd('d.get_hash').'=,$'.getCmd('d.get_base_path').'=,$'.getCmd('d.is_multi_file').'=}"')),
 	new rXMLRPCCommand('schedule', array( 'erasedata'.getUser(), '5', $garbageCheckInterval."", 
 		getCmd('execute').'={sh,-c,'.escapeshellarg(getPHP()).' '.escapeshellarg($thisDir.'/update.php').' '.escapeshellarg(getUser()).' &}' ))
 	));
