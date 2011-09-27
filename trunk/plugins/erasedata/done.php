0496e229e3c22028f9f9a0ee5eb952696ca4bceb
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2011-09-27 08:03:37 +0000

    fix plugin 'erasedata'

--- trunk/plugins/erasedata/done.php
+++ trunk/plugins/erasedata/done.php
@@ -2,6 +2,7 @@
 
 $req = new rXMLRPCRequest( array(
 	$theSettings->getOnEraseCommand(array('erasedata0'.getUser(), getCmd('cat='))),
+	$theSettings->getOnEraseCommand(array('erasedata1'.getUser(), getCmd('cat='))),
 	new rXMLRPCCommand('schedule_remove', 'erasedata'.getUser())
 	));
 $req->run();
