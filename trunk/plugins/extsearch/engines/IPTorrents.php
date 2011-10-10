325076e45f101060d4d26717b03aa510fe25896e
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2011-10-10 11:36:50 +0000

    fix iptorrents engine for plugin extsearch

--- trunk/plugins/extsearch/engines/IPTorrents.php
+++ trunk/plugins/extsearch/engines/IPTorrents.php
@@ -2,42 +2,38 @@
 
 class IPTorrentsEngine extends commonEngine
 {
-	public $defaults = array( "public"=>false, "page_size"=>35, "cookies"=>"www.iptorrents.com|pass=XXX;uid=XXX" );
-	public $categories = array( 'all'=>'', 'Anime'=>'&c60=1', 'Appz/misc'=>'&c1=1', 'AudioBook'=>'&c64=1',
-		'Books - Mags'=>'&c35=1', 'Games'=>'&c2=1&c47=1&c43=1&c45=1&c39=1&c40=1&c67=1&c50=1&c42=1&c44=1','HD/X264'=>'&c48=1',
-		'iPod'=>'&c62=1&c66=1', 'Kids'=>'&c54=1', 'MAC'=>'&c69=1', 'Mobile'=>'&c58=1', 'Movies'=>'&c6=1&c70=1&c38=1&c68=1&c7=1&c48=1',
-		'Music'=>'&c37=1&c3=1', 'Pics/Wallpapers'=>'&c36=1', 'Sports'=>'&c55=1', 'TV'=>'&c66=1&c4=1&c65=1', 'XXX'=>'&c8=1' );
+	public $defaults = array( "public"=>false, "page_size"=>35, "cookies"=>"on.iptorrents.com|pass=XXX;uid=XXX" );
+	public $categories = array( 'all'=>'l1;l2;l3;l4;l5;l6;l7;l8;l35;l36;l37;l38;l39;l40;l42;l43;l44;l45;l47;l48;l50;l54;l55;l58;l60;l62;l64;l65;l66;l67;l68;l69;l70;l71;',
+		'Anime'=>'l60;', 'Appz/misc'=>'l1;', 'AudioBook'=>'l64;', 'Books - Mags'=>'l35;', 'Games'=>'l2;l47;l43;l45;l39;l40;l67;l50;l42;l44;l71;',
+		'HD/X264'=>'l48;', 'iPod'=>'l62;l66;', 'Kids'=>'l54;', 'MAC'=>'l69;', 'Mobile'=>'l58;', 'Movies'=>'l6;l70;l38;l68;l7;l48;',
+		'Music'=>'l37;l3;', 'Pics/Wallpapers'=>'l36;', 'Sports'=>'l55;', 'TV'=>'l66;l4;l5;l65;', 'XXX'=>'l8;' );
+
 
 	public function action($what,$cat,&$ret,$limit,$useGlobalCats)
 	{
 		$added = 0;
-		$url = 'http://www.iptorrents.com';
+		$url = 'http://on.iptorrents.com';
 		if($useGlobalCats)
-			$categories = array( 'all'=>'', 'movies'=>'&c6=1&c70=1&c38=1&c68=1&c7=1&c48=1', 'tv'=>'&c66=1&c4=1&c65=1', 'music'=>'&c37=1&c3=1', 'games'=>'&c2=1&c47=1&c43=1&c45=1&c39=1&c40=1&c67=1&c50=1&c42=1&c44=1', 'anime'=>'&c60=1', 'software'=>'&c1=1', 'pictures'=>'&c36=1', 'books'=>'&c64=1&c35=1' );
+			$categories = array( 'all'=>'l1;l2;l3;l4;l5;l6;l7;l8;l35;l36;l37;l38;l39;l40;l42;l43;l44;l45;l47;l48;l50;l54;l55;l58;l60;l62;l64;l65;l66;l67;l68;l69;l70;l71;', 
+				'movies'=>'l6;l70;l38;l68;l7;l48;', 'tv'=>'l66;l4;l5;l65;', 'music'=>'l37;l3;', 'games'=>'l2;l47;l43;l45;l39;l40;l67;l50;l42;l44;l71;', 
+				'anime'=>'l60;', 'software'=>'l1;', 'pictures'=>'l36;', 'books'=>'l64;l35;' );
 		else
 			$categories = &$this->categories;
 		if(!array_key_exists($cat,$categories))
 			$cat = $categories['all'];
 		else
 			$cat = $categories[$cat];
-		for($pg = 0; $pg<10; $pg++)
+		for($pg = 1; $pg<11; $pg++)
 		{
-			$cli = $this->fetch( $url.'/browse.php?search='.$what.'&sort=seeders&d=DESC&incldead=0&page='.$pg.$cat );
-			if( ($cli==false) || (strpos($cli->results, "<h3>Nothing found!</h3>")!==false) ||
-				(strpos($cli->results, "<h1>Not logged in!</h1>")!==false))
+			$cli = $this->fetch( $url.'/torrents/?'.$cat.'o=seeders;q=@title '.$what.';p='.$pg );
+			if( ($cli==false) || (strpos($cli->results, ">Nothing found!<")!==false) ||
+				(strpos($cli->results, ">Password:<")!==false))
 				break;
-			$res = preg_match_all('/<img class=".*" border="0" width="50" src=.* alt="(?P<cat>.*)" \/><\/a>'.
-				'.*<a href="details.php\?id=(?P<id>\d+)">(?P<name>.*)<\/a>.*<td .*>.*<\/td>'.
-				'<td .*>.*href="download.php\/\d+\/(?P<tname>.*)".*<\/a><\/td><td .*>.*<\/td>.*<td .*>(?P<date>.*)<\/td>.*<td .*>(?P<size>.*)<\/td>'.
-				'.*<td .*>.*<\/td>.*<td .*>(?P<seeds>.*)<\/td>.*<td .*>(?P<leech>.*)<\/td>/siU', $cli->results, $matches);
-			if(($res!==false) && ($res>0) &&
-				count($matches["id"])==count($matches["cat"]) &&
-				count($matches["cat"])==count($matches["name"]) && 
-				count($matches["name"])==count($matches["size"]) &&
-				count($matches["size"])==count($matches["seeds"]) &&
-				count($matches["seeds"])==count($matches["date"]) &&
-				count($matches["seeds"])==count($matches["tname"]) &&
-				count($matches["date"])==count($matches["leech"]) )
+			$res = preg_match_all('`<img class=".*" border="0" width="50" src=.* alt="(?P<cat>.*)" /></a>'.
+				'.*<a href="/details\.php\?id=(?P<id>\d+)">(?P<name>.*)</a>.*<td .*>.*</td>'.
+				'<td .*>.*href="/download\.php/\d+\/(?P<tname>.*)".*</a></td><td .*>.*</td>.*<td .*>(?P<date>.*)</td>.*<td .*>(?P<size>.*)</td>'.
+				'.*<td .*>.*</td>.*<td .*>(?P<seeds>.*)</td>.*<td .*>(?P<leech>.*)</td>`siU', $cli->results, $matches);
+			if($res)
 			{
 				for($i=0; $i<count($matches["id"]); $i++)
 				{
@@ -48,7 +44,7 @@ class IPTorrentsEngine extends commonEngine
 						$item["cat"] = self::removeTags($matches["cat"][$i]);
 						$item["desc"] = $url."/details.php?id=".$matches["id"][$i];
 						$item["name"] = self::removeTags($matches["name"][$i]);
-						$item["size"] = self::formatSize(str_replace("<br>"," ",$matches["size"][$i]));
+						$item["size"] = self::formatSize($matches["size"][$i]);
 						$item["time"] = strtotime(self::removeTags($matches["date"][$i]));
 						$item["seeds"] = intval(self::removeTags($matches["seeds"][$i]));
 						$item["peers"] = intval(self::removeTags($matches["leech"][$i]));
