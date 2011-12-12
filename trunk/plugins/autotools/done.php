16baccd07dead5d44441a5191c54e05cb27e7b12
novik65@gmail.com <novik65@gmail.com@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2011-12-12 14:16:15 +0000

    minor fix

--- trunk/plugins/autotools/done.php
+++ trunk/plugins/autotools/done.php
@@ -1,7 +1,7 @@
 <?php
 
 $req = new rXMLRPCRequest( array( 
-	$theSettings->getOnInsertCommand(array('autolabel'.getUser(), getCmd('cat='))),
+	$theSettings->getOnInsertCommand(array('_autolabel'.getUser(), getCmd('cat='))),
 	$theSettings->getOnFinishedCommand(array('automove'.getUser(), getCmd('cat='))),
 	new rXMLRPCCommand('schedule_remove', 'autowatch'.getUser())
 	));
