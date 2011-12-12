80c71bb6841e7f279e452c10d0488fd73fd78594
serge.6401@gmail.com <serge.6401@gmail.com@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2011-12-12 18:10:25 +0000

    minor fixes

--- trunk/plugins/extratio/lang/fr.js
+++ trunk/plugins/extratio/lang/fr.js
@@ -1,18 +1,19 @@
 ﻿
+
  theUILang.ratioRulesManager	= "Rules Manager";
- theUILang.mnu_ratiorule	= "Ratio rules";
+ theUILang.mnu_ratiorule	= "Ratio Rules";
  theUILang.ratAddRule		= "Add";
  theUILang.ratDelRule		= "Delete";
  theUILang.ratUpRule		= "Up";
  theUILang.ratDownRule		= "Down";
  theUILang.ratioIfLegend	= "If";
- theUILang.ratLabelContain	= "Label of torrent contain";
- theUILang.ratTrackerContain	= "One of tracker's URL of the torrent contain";
- theUILang.ratTrackerPublic	= "All trackers of the torrent are public";
- theUILang.ratTrackerPrivate	= "One of tracker of the torrent is private";
+ theUILang.ratLabelContain	= "Torrent Label contains";
+ theUILang.ratTrackerContain	= "One of torrent's tracker URLs contains";
+ theUILang.ratTrackerPublic	= "All torrent's trackers are public";
+ theUILang.ratTrackerPrivate	= "One of torrent's trackers is private";
  theUILang.ratioThenLegend	= "Then";
  theUILang.setRatioTo		= "Set ratio to";
- theUILang.setChannelTo		= "Set throttle to";
- theUILang.ratioNewRule		= "New rule";
+ theUILang.setChannelTo 	= "Set throttle to";
+ theUILang.ratioNewRule 	= "New rule";
 
 thePlugins.get("extratio").langLoaded();
--- trunk/plugins/extratio/lang/ru.js
+++ trunk/plugins/extratio/lang/ru.js
@@ -1,4 +1,5 @@
 ﻿
+
  theUILang.ratioRulesManager	= "Менеджер правил";
  theUILang.mnu_ratiorule	= "Правила ratio";
  theUILang.ratAddRule		= "Добавить";
@@ -8,11 +9,11 @@
  theUILang.ratioIfLegend	= "Если";
  theUILang.ratLabelContain	= "Метка закачки содержит";
  theUILang.ratTrackerContain	= "Один из URL трекеров закачки содержит";
- theUILang.ratTrackerPublic	= "Один из трекеров закачки частный";
- theUILang.ratTrackerPrivate	= "Все трекера закачки публичные";
+ theUILang.ratTrackerPublic	= "Все трекера закачки публичные";
+ theUILang.ratTrackerPrivate	= "Один из трекеров закачки частный";
  theUILang.ratioThenLegend	= "То";
  theUILang.setRatioTo		= "Установить ratio";
- theUILang.setChannelTo		= "Установить канал";
- theUILang.ratioNewRule		= "Новое правило";
+ theUILang.setChannelTo 	= "Установить канал";
+ theUILang.ratioNewRule 	= "Новое правило";
 
 thePlugins.get("extratio").langLoaded();
