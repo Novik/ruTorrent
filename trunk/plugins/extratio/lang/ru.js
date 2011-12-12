d10601af9be176b2f5b0688d44a14b2300dac481
novik65@gmail.com <novik65@gmail.com@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2011-12-12 14:20:11 +0000

    add plugin extratio

---
+++ trunk/plugins/extratio/lang/ru.js
@@ -0,0 +1,18 @@
+﻿
+ theUILang.ratioRulesManager	= "Менеджер правил";
+ theUILang.mnu_ratiorule	= "Правила ratio";
+ theUILang.ratAddRule		= "Добавить";
+ theUILang.ratDelRule		= "Удалить";
+ theUILang.ratUpRule		= "Вверх";
+ theUILang.ratDownRule		= "Вниз";
+ theUILang.ratioIfLegend	= "Если";
+ theUILang.ratLabelContain	= "Метка закачки содержит";
+ theUILang.ratTrackerContain	= "Один из URL трекеров закачки содержит";
+ theUILang.ratTrackerPublic	= "Один из трекеров закачки частный";
+ theUILang.ratTrackerPrivate	= "Все трекера закачки публичные";
+ theUILang.ratioThenLegend	= "То";
+ theUILang.setRatioTo		= "Установить ratio";
+ theUILang.setChannelTo		= "Установить канал";
+ theUILang.ratioNewRule		= "Новое правило";
+
+thePlugins.get("extratio").langLoaded();
