dd635936eee2af60c182177ed4656eeabe515617
Novik65 <Novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2012-01-04 14:28:31 +0000

    add plugin history

---
+++ plugins/history/lang/uk.js
@@ -0,0 +1,18 @@
+﻿
+ theUILang.historyAddition	= "Addition";
+ theUILang.historyDeletion	= "Deletion";
+ theUILang.historyFinish	= "Finish";
+ theUILang.hitoryLog		= "Log";
+ theUILang.history		= "History";
+ theUILang.historyLimit		= "Max items count";
+ theUILang.seedingTime		= "Finished";
+ theUILang.addTime		= "Added";
+ theUILang.hstDeletePrompt	= "Do you really want to delete the selected item(s)?";
+ theUILang.hstDelete		= "Delete item(s)";
+ theUILang.Added		= "Added";
+ theUILang.Finished		= "Finished";
+ theUILang.Deleted		= "Deleted";
+ theUILang.Time			= "Time";
+ theUILang.Tracker		= "Tracker";
+
+thePlugins.get("history").langLoaded();
---
+++ plugins/history/update.php
@@ -0,0 +1,19 @@
+<?php
+	$path = dirname(realpath($argv[0]));
+	if(chdir($path))
+	{
+		if( count( $argv ) > 12 )
+			$_SERVER['REMOTE_USER'] = $argv[12];
+		require_once( './history.php' );
+		$hst = rHistoryData::load();
+		$mgr = rHistory::load();
+		$tracker = strstr( $argv[10], '#', true );
+		$hst->add( array(
+			"action"=>intval($argv[1]),
+			"name"=>$argv[2], "size"=>floatval($argv[3]), "downloaded"=>floatval($argv[4]),
+			"uploaded"=>floatval($argv[5]), "ratio"=>floatval($argv[6]), "creation"=>intval($argv[7]),
+			"added"=>intval($argv[8]), "finished"=>intval($argv[9]), "tracker"=>$tracker,
+			"label"=>rawurldecode($argv[11]),
+			), $mgr->log["limit"] );
+	}
+?>
