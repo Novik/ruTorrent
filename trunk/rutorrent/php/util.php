87900cca728b23e348554cd8d9db668a1d98e8ea
novik65@gmail.com <novik65@gmail.com@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2012-01-23 13:14:48 +0000

    add magnet links compability to plugin rss

--- trunk/rutorrent/php/util.php
+++ trunk/rutorrent/php/util.php
@@ -522,4 +522,36 @@ function sendFile( $filename, $contentType = null, $nameToSent = null, $mustExit
 	return(false);
 }
 
+function base32decode($input)
+{
+	$keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567=";
+        $buffer = 0;
+        $bitsLeft = 0;    
+        $output = '';
+        $i = 0;
+        $input = strtoupper($input);
+        $len = strlen($input);
+        while($i < $len)
+        {
+		$val = strpos( $keyStr, $input[$i++]);
+		if($val >= 0 && $val < 32) 
+		{
+			$buffer <<= 5;
+			$buffer |= $val;
+			$bitsLeft += 5;
+			if($bitsLeft >= 8) 
+			{
+				$output .= chr(($buffer >> ($bitsLeft - 8)) & 0xFF);
+				$bitsLeft -= 8;
+			}
+		} 
+        }
+        if($bitsLeft > 0) 
+        {
+		$buffer <<= 5;    
+		$output .= chr(($buffer >> ($bitsLeft - 3)) & 0xFF);
+        }         
+	return( strtoupper(bin2hex($output)) );
+}
+
 ?>
