fd8d7aa010aecea9eca317f41353274ec1c65bb6
novik65@gmail.com <novik65@gmail.com@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2012-02-29 12:18:53 +0000

    make tags

---
+++ tags/plugins/rutracker_check/3.4/lang/fr.js
@@ -0,0 +1,25 @@
+﻿/*
+ * PLUGIN RUTRACKER_CHECK
+ *
+ * File Name: fr.js
+ *      French language file.
+ *
+ * File Author:
+ *    Nicobubulle (nicobubulle@gmail.com)
+ */
+
+ theUILang.checkTorrent 	= "Vérifier les MAJ";
+ theUILang.chkHdr		= "Vérification de la MAJ du torrent";
+ theUILang.checkedAt		= "Dernière vérification";
+ theUILang.checkedResult	= "Resultat";
+ theUILang.chkResults		= [
+				  "En cours",
+				  "Mis à jour",
+				  "A jour",
+				  "Certainement supprimé",
+				  "Erreur d'accès au tracker",
+				  "Problème d'accès à rTorrent",
+				  "Pas besoin"
+				  ];
+
+thePlugins.get("rutracker_check").langLoaded();
