e8a111b362b2198ab508de8271dfb27142a9ffad
novik65@gmail.com <novik65@gmail.com@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2012-02-29 12:24:56 +0000

    make tags

---
+++ tags/plugins/_task/3.4/action.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once( "task.php" );
+
+$ret = array();
+
+switch($_REQUEST['cmd'])
+{
+	case "kill":
+	{
+	        $ret = rTask::kill($_REQUEST['no']);
+		break;
+	}
+	case "check":
+	{
+	        $ret = rTask::check($_REQUEST['no']);
+		break;
+	}
+}
+
+cachedEcho(json_encode($ret),"application/json");
+
+?>
---
+++ tags/plugins/_task/3.4/lang/sr.js
@@ -0,0 +1,8 @@
+﻿
+
+ theUILang.tskCommand		= "Running...";
+ theUILang.tskCommandDone	= "Done.";
+ theUILang.tskConsole		= "Console";
+ theUILang.tskErrors		= "Diagnostics";
+
+thePlugins.get("_task").langLoaded();
