eea5c255376157aeea3f39756e1d9382ba7bea39
novik65@gmail.com <novik65@gmail.com@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2012-02-29 12:15:40 +0000

    make tags

---
+++ tags/plugins/unpack/3.4/lang/vi.js
@@ -0,0 +1,17 @@
+﻿
+
+ theUILang.unpack		= "Giải nén";
+ theUILang.unpackPath		= "Giải nén vào (bỏ trống nếu muốn giải nén vào thư mục torrent hiện tại):";
+ theUILang.processAll		= "Thực hiện tất cả các khối";
+ theUILang.unpackTaskFailed	= "Giải nén xong, có một vài lỗi";
+ theUILang.unpackTaskOK 	= "Giải nén xong, không phát sinh lỗi";
+ theUILang.unpackTaskStarted	= "Bắt đầu giải nén";
+ theUILang.unzipNotFound	= "Unpack plugin: rTorrent không thể truy cập chương trình unzip.";
+ theUILang.unrarNotFound	= "Unpack plugin: rTorrent không thể truy cập chương trình unrar.";
+ theUILang.unpackEnabled	= "Enable autounpacking if torrent's label matches filter";
+ theUILang.unpackTorrents	= "Thêm vào đường dẫn khi giải nén dữ liệu torrent:";
+ theUILang.unpackAddLabel	= "Nhãn Torrent";
+ theUILang.unpackAddName	= "Tên Torrent";
+ theUILang.unpackNoFiles	= "Không tìm thấy dữ liệu nén trong torrent.";
+
+thePlugins.get("unpack").langLoaded();
