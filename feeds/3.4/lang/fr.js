c1d6333d3d3a3983f797d627fb17494cc114a8f7
novik65@gmail.com <Novik@users.noreply.github.com>
2012-02-29 12:21:29 +0000

    make tags

---
+++ feeds/3.4/lang/fr.js
@@ -0,0 +1,18 @@
+﻿/*
+ * PLUGIN FEEDS
+ *
+ * File Name: fr.js
+ * 	French language file.
+ *
+ * File Author:
+ *    Nicobubulle (nicobubulle@gmail.com)
+ */
+
+ theUILang.feedAll		= "Tous les torrents";
+ theUILang.feedCompleted	= "Torrents terminés";
+ theUILang.feedDownloading	= "Torrents en cours de téléchargement";
+ theUILang.feedActive		= "Torrents actifs";
+ theUILang.feedInactive 	= "Torrents inactifs";
+ theUILang.feedError		= "Torrents en erreur";
+
+thePlugins.get("feeds").langLoaded();
---
+++ feeds/3.4/lang/nl.js
@@ -0,0 +1,10 @@
+﻿
+
+ theUILang.feedAll		= "Alle Torrents";
+ theUILang.feedCompleted	= "Voltooide Torrents";
+ theUILang.feedDownloading	= "Torrents aan het Downloaden";
+ theUILang.feedActive		= "Actieve Torrents";
+ theUILang.feedInactive 	= "Inactieve Torrents";
+ theUILang.feedError		= "Foutieve Torrents";
+
+thePlugins.get("feeds").langLoaded();
