686a6a7d2fe459b6e9ada16a4894fb3bd31e1303
novik65@gmail.com <Novik@users.noreply.github.com>
2012-02-29 12:24:02 +0000

    make tags

---
+++ check_port/3.4/lang/uk.js
@@ -0,0 +1,10 @@
+﻿
+
+ theUILang.checkPort		= "Перевірити стан порту";
+ theUILang.portStatus		= [
+ 				  "Стан порту невідомий",
+ 				  "Порт закрито",
+ 				  "Порт відкрито"
+ 				  ];
+
+thePlugins.get("check_port").langLoaded();
