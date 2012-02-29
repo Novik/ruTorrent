27edb7428eaf60776237a3f203abdea43f010914
novik65@gmail.com <Novik@users.noreply.github.com>
2012-02-29 12:16:49 +0000

    make tags

---
+++ source/3.4/lang/vi.js
@@ -0,0 +1,6 @@
+﻿
+
+ theUILang.getSource		= "Lấy tập tin .torrent";
+ theUILang.cantFindTorrent	= "Không tìm thấy tập tin torrent gốc đang tải.";
+
+thePlugins.get("source").langLoaded();
---
+++ source/3.4/lang/zh-tw.js
@@ -0,0 +1,6 @@
+﻿
+
+ theUILang.getSource		= "Get .torrent";
+ theUILang.cantFindTorrent	= "Source torrent file for this download not found.";
+
+thePlugins.get("source").langLoaded();
