71102e72adf14f6eec3e4bd867a9a9a8ee3a6df9
novik65@gmail.com <Novik@users.noreply.github.com>
2012-02-29 12:22:06 +0000

    make tags

---
+++ extratio/3.4/lang/ru.js
@@ -0,0 +1,19 @@
+﻿
+
+ theUILang.ratioRulesManager	= "Менеджер правил";
+ theUILang.mnu_ratiorule	= "Правила ratio";
+ theUILang.ratAddRule		= "Добавить";
+ theUILang.ratDelRule		= "Удалить";
+ theUILang.ratUpRule		= "Вверх";
+ theUILang.ratDownRule		= "Вниз";
+ theUILang.ratioIfLegend	= "Если";
+ theUILang.ratLabelContain	= "Метка закачки содержит";
+ theUILang.ratTrackerContain	= "Один из URL трекеров закачки содержит";
+ theUILang.ratTrackerPublic	= "Все трекера закачки публичные";
+ theUILang.ratTrackerPrivate	= "Один из трекеров закачки частный";
+ theUILang.ratioThenLegend	= "То";
+ theUILang.setRatioTo		= "Установить ratio";
+ theUILang.setChannelTo 	= "Установить канал";
+ theUILang.ratioNewRule 	= "Новое правило";
+
+thePlugins.get("extratio").langLoaded();
---
+++ extratio/3.4/update.php
@@ -0,0 +1,17 @@
+<?php
+	$path = dirname(realpath($argv[0]));
+	if(chdir($path))
+	{
+		if( count( $argv ) > 4 )
+			$_SERVER['REMOTE_USER'] = $argv[4];
+		require_once( './rules.php' );
+		$mngr = rRatioRulesList::load();
+		$rule = $mngr->getRule( rawurlencode($argv[2]), $argv[1] );
+		if($rule)
+		{
+			$val = $rule->{$argv[3]};
+			if($val!='')
+				echo $val;
+		}
+	}
+?>
