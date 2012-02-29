0dd26159ee0abd1cef97aed00d0ce782f2a08f4e
novik65@gmail.com <Novik@users.noreply.github.com>
2012-02-29 12:16:25 +0000

    make tags

---
+++ throttle/3.4/done.php
@@ -0,0 +1,6 @@
+<?php
+
+$req = new rXMLRPCRequest( rTorrentSettings::get()->getOnInsertCommand(array('_throttle'.getUser(), getCmd('cat='))) );
+$req->run();
+
+?>
---
+++ throttle/3.4/lang/en.js
@@ -0,0 +1,10 @@
+﻿
+
+ theUILang.throttles		= "Channels";
+ theUILang.throttle		= "Channel";
+ theUILang.mnuThrottle		= "Set Channel";
+ theUILang.mnuUnlimited 	= "No Channel";
+ theUILang.channelName		= "Name";
+ theUILang.channelDefault	= "Default channel";
+
+thePlugins.get("throttle").langLoaded();
