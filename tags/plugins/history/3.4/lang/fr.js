0f71f160e91e10ab423ac94082113a9ef04c6195
novik65@gmail.com <novik65@gmail.com@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2012-02-29 12:21:06 +0000

    make tags

---
+++ tags/plugins/history/3.4/lang/fr.js
@@ -0,0 +1,27 @@
+﻿/*
+ * PLUGIN HISTORY
+ *
+ * File Name: fr.js
+ *      French language file.
+ *
+ * File Author:
+ *    Nicobubulle (nicobubulle@gmail.com)
+ */
+ 
+ theUILang.historyAddition	= "Ajoutés";
+ theUILang.historyDeletion	= "Supprimés";
+ theUILang.historyFinish	= "Terminés";
+ theUILang.hitoryLog		= "Archiver les torrents";
+ theUILang.history		= "Historique";
+ theUILang.historyLimit		= "Nombre maximum d'enregistrements";
+ theUILang.seedingTime		= "Finished";
+ theUILang.addTime		= "Ajouté";
+ theUILang.hstDeletePrompt	= "Êtes-vous sûr de vouloir supprimer le(s) enregistrement(s) ?";
+ theUILang.hstDelete		= "Supprimer le(s) enregistrement(s)";
+ theUILang.Added		= "Ajouté";
+ theUILang.Finished		= "Terminé";
+ theUILang.Deleted		= "Supprimé";
+ theUILang.Time			= "Heure";
+ theUILang.Tracker		= "Tracker";
+
+thePlugins.get("history").langLoaded();
---
+++ tags/plugins/history/3.4/lang/pl.js
@@ -0,0 +1,18 @@
+﻿
+ theUILang.historyAddition	= "Addition";
+ theUILang.historyDeletion	= "Deletion";
+ theUILang.historyFinish	= "Finish";
+ theUILang.hitoryLog		= "Log";
+ theUILang.history		= "Historia";
+ theUILang.historyLimit		= "Maximum number of records";
+ theUILang.seedingTime		= "Ukończone";
+ theUILang.addTime		= "Dodane";
+ theUILang.hstDeletePrompt	= "Czy napewno chcesz usunąć wybrane/ą pozycje?";
+ theUILang.hstDelete		= "Usuń pozycję";
+ theUILang.Added		= "Dodane";
+ theUILang.Finished		= "Ukończone";
+ theUILang.Deleted		= "Usunięte";
+ theUILang.Time			= "Czas";
+ theUILang.Tracker		= "Tracker";
+
+thePlugins.get("history").langLoaded();
