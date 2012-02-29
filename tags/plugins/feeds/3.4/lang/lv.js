1f1078b2e74ac42bb29bed27d8dd9c26ad596bcc
novik65@gmail.com <novik65@gmail.com@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2012-02-29 12:21:29 +0000

    make tags

---
+++ tags/plugins/feeds/3.4/lang/lv.js
@@ -0,0 +1,10 @@
+﻿
+
+ theUILang.feedAll		= "All torrents";
+ theUILang.feedCompleted	= "Completed torrents";
+ theUILang.feedDownloading	= "Downloading torrents";
+ theUILang.feedActive		= "Active torrents";
+ theUILang.feedInactive 	= "Inactive torrents";
+ theUILang.feedError		= "Error torrents";
+
+thePlugins.get("feeds").langLoaded();
---
+++ tags/plugins/feeds/3.4/lang/pl.js
@@ -0,0 +1,10 @@
+﻿
+
+ theUILang.feedAll		= "Wszystkie torrenty";
+ theUILang.feedCompleted	= "Kompletne torrenty";
+ theUILang.feedDownloading	= "Pobierane torrenty";
+ theUILang.feedActive		= "Aktywne torrenty";
+ theUILang.feedInactive 	= "Nieaktywne torrenty";
+ theUILang.feedError		= "Torrenty z błędem";
+
+thePlugins.get("feeds").langLoaded();
