c89ee2da8906aebb3b0e8fcf3cf72a087d4f823a
novik65@gmail.com <Novik@users.noreply.github.com>
2012-02-29 12:22:27 +0000

    make tags

---
+++ edit/3.4/lang/fr.js
@@ -0,0 +1,19 @@
+﻿/*
+ * PLUGIN EDIT
+ *
+ * File Name: fr.js
+ * 	French language file.
+ *
+ * File Author:
+ *    Nicobubulle (nicobubulle@gmail.com)
+ */
+
+
+ theUILang.EditTrackers 		= "Éditer le torrent...";
+ theUILang.EditTorrentProperties	= "Propriétés du torrent";
+ theUILang.errorAddTorrent		= "Erreur lors de l'ajout du .torrent";
+ theUILang.errorWriteTorrent		= "Erreur lors de l'écriture du .torrent";
+ theUILang.errorReadTorrent		= "Erreur lors de la lecture du .torrent";
+ theUILang.cantFindTorrent		= "Le fichier source pour ce torrent n'a pas été trouvé."
+
+thePlugins.get("edit").langLoaded();
---
+++ edit/3.4/lang/nl.js
@@ -0,0 +1,10 @@
+﻿
+
+ theUILang.EditTrackers 		= "Bewerk torrent ...";
+ theUILang.EditTorrentProperties	= "Torrent Eigenschappen";
+ theUILang.errorAddTorrent		= "Fout bij het toevoegen van het torrent-bestand";
+ theUILang.errorWriteTorrent		= "Fout bij schrijven van het torrent bestand";
+ theUILang.errorReadTorrent		= "Fout bij het lezen van het torrent bestand";
+ theUILang.cantFindTorrent		= "Bron torrent-bestand voor deze download niet gevonden."
+
+thePlugins.get("edit").langLoaded();
