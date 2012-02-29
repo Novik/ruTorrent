4cb473f0cb121b29c443bd3eaa8b2e9c37435f80
novik65@gmail.com <novik65@gmail.com@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2012-02-29 12:23:38 +0000

    make tags

---
+++ tags/plugins/chunks/3.4/lang/uk.js
@@ -0,0 +1,10 @@
+﻿
+
+ theUILang.Chunks		= "Частини";
+ theUILang.chunksNoChunksFound	= "Відомостей про частини немає.";
+ theUILang.chunksRows		= "рядки";
+ theUILang.chunksColumns	= "стовпці";
+ theUILang.chunksChunks 	= "частини";
+ theUILang.chunksSize		= "розмір частини";
+
+thePlugins.get("chunks").langLoaded();
