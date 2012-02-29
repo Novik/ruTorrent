841538ce75a2c625c4f064da7620c80c25807e64
novik65@gmail.com <novik65@gmail.com@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2012-02-29 12:22:57 +0000

    make tags

---
+++ tags/plugins/data/3.4/lang/nl.js
@@ -0,0 +1,6 @@
+﻿
+
+ theUILang.getData		= "Verkrijg data";
+ theUILang.cantAccessData	= "Web-server user heeft geen toegang tot de data van deze torrent.";
+
+thePlugins.get("data").langLoaded();
---
+++ tags/plugins/data/3.4/lang/zh-cn.js
@@ -0,0 +1,6 @@
+﻿
+
+ theUILang.getData		= "获取数据";
+ theUILang.cantAccessData	= "Webserver user can't access the data of this torrent.";
+
+thePlugins.get("data").langLoaded();
