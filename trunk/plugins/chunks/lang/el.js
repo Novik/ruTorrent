e5dc184805352d8865e371446843843bfa820e3e
novik65@gmail.com <novik65@gmail.com@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2012-04-13 09:55:32 +0000

    add greek lang

---
+++ trunk/plugins/chunks/lang/el.js
@@ -0,0 +1,11 @@
+﻿
+ theUILang.Chunks		= "Chunks";
+ theUILang.cAvail		= "Availability";
+ theUILang.cDownloaded		= "Downloaded";
+ theUILang.cMode		= "Mode";
+ theUILang.chunksCount		= "Chunks count";
+ theUILang.chunkSize		= "Chunk size";
+ theUILang.cLegend		= "Legend";
+ theUILang.cLegendVal		= [ "4 chunks per cell", "1 chunk per cell" ];
+
+thePlugins.get("chunks").langLoaded();
