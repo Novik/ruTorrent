36203ebced4292e8951652a05d473d002b44ecd7
novik65 <Novik@users.noreply.github.com>
2012-08-02 09:18:57 +0000

    fix some extsearch engines

--- plugins/extsearch/engines/TorrentReactor.php
+++ plugins/extsearch/engines/TorrentReactor.php
@@ -21,7 +21,7 @@ class TorrentReactorEngine extends commonEngine
 
 		for($pg = 0; $pg<10; $pg++)
 		{
-			$cli = $this->fetch( $url.'/search.php?search=&words='.$what.'&cid='.$cat.'&type=1&orderby=a.seeds&asc=0&skip='.($pg*35) );
+			$cli = $this->fetch( $url.'/ts.php?search=&words='.$what.'&cid='.$cat.'&type=1&orderby=a.seeds&asc=0&skip='.($pg*35) );
 			if($cli==false || (strpos($cli->results, '<td colspan="8" bgcolor="#ffffff">No results.')!==false))
 				break;
 			$res = preg_match_all('/<tr class="torrent_.*">.*<a rel="nofollow" href="(?P<link>[^"]*torrentreactor.net\/download.php\?[^"]*)">.*'.
