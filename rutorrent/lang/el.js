ead3a058277e28d07f1a7730031ea3a69e366141
ckanatas@gmail.com <ckanatas@gmail.com>
2012-09-07 13:33:52 +0000

    Add license

--- rutorrent/lang/el.js
+++ rutorrent/lang/el.js
@@ -1,6 +1,32 @@
-﻿
-var theUILang =
-{
+﻿/*
+ * WebUI - The WEB interface for uTorrent - http://www.utorrent.com
+ * NO COPYCATS of language update
+ *
+ * == BEGIN LICENSE ==
+ *
+ * Licensed under the terms of any of the following licenses at your
+ * choice:
+ *
+ *  - GNU General Public License Version 2 or later (the "GPL")
+ *    http://www.gnu.org/licenses/gpl.html
+ *
+ *  - GNU Lesser General Public License Version 2.1 or later (the "LGPL")
+ *    http://www.gnu.org/licenses/lgpl.html
+ *
+ *  - Mozilla Public License Version 1.1 or later (the "MPL")
+ *    http://www.mozilla.org/MPL/MPL-1.1.html
+ *
+ * == END LICENSE ==
+ *
+ * File Name: el.js
+ * 	Greek language file.
+ *
+ * File Author:
+ * 		Chris Kanatas (ckanatas@gmail.com)
+ */
+
+ var theUILang =
+ {
  kbs				: "kB/s",
  General			: "Γενικά",
  Downloads			: "Λήψεις",
