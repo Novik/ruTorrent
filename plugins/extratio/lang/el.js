de465fcb7ae32028cf43c4b305f5c0ad90a46123
ckanatas@gmail.com <ckanatas@gmail.com>
2012-09-07 10:08:05 +0000

    Trying to find what's causing strange appearance in extratio plugin..

--- plugins/extratio/lang/el.js
+++ plugins/extratio/lang/el.js
@@ -11,9 +11,9 @@
  theUILang.ratTrackerContain	= "One of torrent's tracker URLs contains";
  theUILang.ratTrackerPublic	= "All torrent's trackers are public";
  theUILang.ratTrackerPrivate	= "One of torrent's trackers is private";
- theUILang.ratioThenLegend	= "Then";
- theUILang.setRatioTo		= "Set ratio to";
- theUILang.setChannelTo 	= "Set throttle to";
- theUILang.ratioNewRule 	= "New rule";
-
+ theUILang.ratioThenLegend	= "Τότε";
+ theUILang.setRatioTo		= "Όρισε την αναλογία σε";
+ theUILang.setChannelTo 	= "Όρισε τον περιοριστή σε";
+ theUILang.ratioNewRule 	= "Νέος κανόνας";
+ 
 thePlugins.get("extratio").langLoaded();
