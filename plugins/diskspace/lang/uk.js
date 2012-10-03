d4f89acebae4aefdddd743d80cb74ef9b4d83198
serge.6401 <serge.6401@gmail.com>
2012-10-03 18:52:07 +0000

    minor fix

--- plugins/diskspace/lang/uk.js
+++ plugins/diskspace/lang/uk.js
@@ -6,6 +6,6 @@
  * Author: Oleksandr Natalenko (pfactum@gmail.com)
  */
 
- theUILang.diskNotification = "Попередження. Диск заповнено. Програма rtorrent може працювати некоректно, а дані не завантажуватимуться, поки не з’явиться вільне місце на диску.";
+ theUILang.diskNotification = "Попередження. Диск заповнено. Програма rTorrent може працювати некоректно, а дані не завантажуватимуться, поки не з’явиться вільне місце на диску.";
 
 thePlugins.get("diskspace").langLoaded();
