ad0b76b429df4759f3f6a0eb7bf29656e1fcecd9
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2012-12-07 12:08:07 +0000

    mark version 3.5

---
+++ tags/plugins/rutracker_check/3.5/init.js
@@ -0,0 +1,105 @@
+plugin.loadLang();
+
+if(plugin.canChangeMenu())
+{
+	plugin.createMenu = theWebUI.createMenu;
+	theWebUI.createMenu = function( e, id )
+	{
+		plugin.createMenu.call(this, e, id);
+		if(plugin.enabled && plugin.canChangeMenu())
+		{
+			var el = theContextMenu.get( theUILang.Force_recheck );
+			if( el )
+			{
+				theContextMenu.add( el, [theUILang.checkTorrent, 
+					((this.getTable("trt").selCount>1) && this.getHashes('checktorrent')) ||
+					this.isTorrentCommandEnabled("checktorrent",id) ? "theWebUI.perform( 'checktorrent' )" : null] );
+			}
+		}
+	}
+		
+	rTorrentStub.prototype.checktorrent = function()
+	{
+		this.content = "cmd=check";
+		for( var i = 0; i < this.hashes.length; i++ )
+			this.content += ("&hash="+this.hashes[i]);
+		this.contentType = "application/x-www-form-urlencoded";
+		this.mountPoint = "plugins/rutracker_check/action.php";
+		this.dataType = "json";
+	}
+}
+
+plugin.config = theWebUI.config;
+theWebUI.config = function(data)
+{
+	plugin.reqId1 = theRequestManager.addRequest("trt", "d.get_custom=chk-state",function(hash,torrent,value)
+	{
+		torrent.chkstate = value;
+		if(torrent.chkstate==4)	// STE_DELETED
+			torrent.state |= dStatus.error;
+	});
+	plugin.reqId2 = theRequestManager.addRequest("trt", "d.get_custom=chk-time",function(hash,torrent,value)
+	{
+		torrent.chktime = value;
+	});
+	plugin.config.call(this,data);
+}
+
+plugin.isTorrentCommandEnabled = theWebUI.isTorrentCommandEnabled;
+theWebUI.isTorrentCommandEnabled = function(act,hash) 
+{
+	if(act=="checktorrent")
+	{
+		if(!plugin.isTorrentCommandEnabled.call(this,act,hash))
+			return(false);
+		else
+		{
+//			var torrent = this.torrents[hash];
+//			return(torrent.done == 1000);
+return(true);
+		}
+	}
+	return(plugin.isTorrentCommandEnabled.call(this,act,hash));
+}
+
+plugin.getStatusIcon = theWebUI.getStatusIcon;
+theWebUI.getStatusIcon = function(torrent) 
+{
+	if(plugin.allStuffLoaded && (torrent.chkstate==4))	// STE_DELETED
+		return(["Status_Error", theUILang.chkResults[torrent.chkstate-1]]);
+	return(	plugin.getStatusIcon.call(theWebUI,torrent) );
+}
+
+plugin.updateDetails = theWebUI.updateDetails;
+theWebUI.updateDetails = function()
+{
+	plugin.updateDetails.call(this);
+	if(plugin.enabled && (this.dID != "") && this.torrents[this.dID])
+	{
+		var torrent = this.torrents[this.dID];
+		$("#chktime").text( torrent.chktime ? theConverter.time(new Date().getTime()/1000-(iv(torrent.chktime)+theWebUI.deltaTime/1000),true) : '' );
+		$("#chkresult").text( torrent.chkstate ? theUILang.chkResults[iv(torrent.chkstate-1)] : '' );
+	}
+}
+
+plugin.onLangLoaded = function()
+{
+	$("#mainlayout").append( 
+		"<tr id='chkinfo1'><td colspan='6' class='Header'>"+theUILang.chkHdr+"</td></tr>"+
+		"<tr id='chkinfo2'><td nowrap>"+theUILang.checkedAt+":</td>"+
+			"<td><span id='chktime' class='det'></span></td>"+
+			"<td nowrap>"+theUILang.checkedResult+":</td>"+
+			"<td colspan=3><span id='chkresult' class='det'></span></td></tr>" );
+}
+
+plugin.onRemove = function()
+{
+	$("#chkinfo1,#chkinfo2").remove();
+	theRequestManager.removeRequest( "trt", plugin.reqId1 );
+	theRequestManager.removeRequest( "trt", plugin.reqId2 );
+	$.each(theWebUI.torrents,function(hash,torrent)
+	{
+		delete torrent.chkstate;
+		delete torrent.chktime;
+	});
+}
---
+++ tags/plugins/rutracker_check/3.5/lang/fr.js
@@ -0,0 +1,23 @@
+﻿/*
+ * PLUGIN RUTRACKER_CHECK
+ *
+ * French language file.
+ *
+ * Author: Nicobubulle (nicobubulle@gmail.com)
+ */
+
+ theUILang.checkTorrent 	= "Vérifier les MAJ";
+ theUILang.chkHdr		= "Vérification de la MAJ du torrent";
+ theUILang.checkedAt		= "Dernière vérification";
+ theUILang.checkedResult	= "Resultat";
+ theUILang.chkResults		= [
+ 				  "En cours",
+ 				  "Mis à jour",
+ 				  "A jour",
+ 				  "Certainement supprimé",
+ 				  "Erreur d'accès au tracker",
+ 				  "Problème d'accès à rTorrent",
+ 				  "Pas besoin"
+ 				  ];
+
+thePlugins.get("rutracker_check").langLoaded();
