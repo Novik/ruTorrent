ac04314ef9917ea8d497b90096d691cdc944ca50
novik65 <Novik@users.noreply.github.com>
2012-12-07 12:04:20 +0000

    mark version 3.5

---
+++ data/3.5/lang/es.js
@@ -0,0 +1,12 @@
+﻿/*
+ * PLUGIN DATA
+ *
+ * Spanish language file.
+ *
+ * Author: 
+ */
+
+ theUILang.getData		= "Obtener archivo";
+ theUILang.cantAccessData	= "El Webserver no puede acceder a los datos de este torrent.";
+
+thePlugins.get("data").langLoaded();
---
+++ data/3.5/lang/pl.js
@@ -0,0 +1,12 @@
+﻿/*
+ * PLUGIN DATA
+ *
+ * Polish language file.
+ *
+ * Author: Dare (piczok@gmail.com)
+ */
+
+ theUILang.getData		= "Pobierz plik";
+ theUILang.cantAccessData	= "User Webservera nie ma dostępu do plików tego torrenta.";
+
+thePlugins.get("data").langLoaded();
