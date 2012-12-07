acb37fb2411075c901af7f9fc1fafc270b0489ce
novik65 <Novik@users.noreply.github.com>
2012-12-07 12:03:00 +0000

    mark version 3.5

---
+++ _task/3.5/lang/lv.js
@@ -0,0 +1,14 @@
+﻿/*
+ * PLUGIN _TASK
+ *
+ * Latvian language file.
+ *
+ * Author: 
+ */
+
+ theUILang.tskCommand		= "Running...";
+ theUILang.tskCommandDone	= "Done.";
+ theUILang.tskConsole		= "Console";
+ theUILang.tskErrors		= "Diagnostics";
+
+thePlugins.get("_task").langLoaded();
