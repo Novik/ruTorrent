4ed57a7e5752ff90b2b141aebff0dbb8201f6fef
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2012-12-07 12:02:33 +0000

    mark version 3.5

---
+++ tags/plugins/_getdir/3.5/getfiles.php
@@ -0,0 +1,181 @@
+<?php
+require_once( '../../php/util.php' );
+require_once( '../../php/settings.php' );
+
+$theSettings = rTorrentSettings::get();
+$dh = false;
+$curFile = null;
+
+$btn_id = "'".$_REQUEST['btn']."'";
+$edit_id = "'".$_REQUEST['edit']."'";
+$frame_id = "'".$_REQUEST['frame']."'";
+
+if(isset($_REQUEST['dir']) && strlen($_REQUEST['dir']))
+{
+	$dir = rawurldecode($_REQUEST['dir']);
+	rTorrentSettings::get()->correctDirectory($dir);
+	if(LFS::is_file($dir) && 
+		(($theSettings->uid<0) || 
+		isUserHavePermission($theSettings->uid,$theSettings->gid,$dir,0x0004)))
+	{
+		$curFile = basename($dir);
+		$dir = dirname($dir);
+	}
+	if(is_dir($dir))
+	{
+		$dh = @opendir($dir);
+		$dir = addslash($dir);
+		if( $dh && 
+			((strpos($dir,$topDirectory)!==0) ||
+			(($theSettings->uid>=0) && 
+			!isUserHavePermission($theSettings->uid,$theSettings->gid,$dir,0x0005))))
+		{
+			closedir($dh);
+			$dh = false;
+		}
+	}
+}
+if(!$dh)
+{
+	$curFile = null;
+	$dir = isLocalMode() ? $theSettings->directory : $topDirectory;
+	if(strpos(addslash($dir),$topDirectory)!==0)
+		$dir = $topDirectory;
+	$dh = @opendir($dir);
+}
+$files = array();
+$dirs = array();
+if($dh)
+{
+	$dir = addslash($dir);
+	while(false !== ($file = readdir($dh)))
+        {
+	        $path = fullpath($dir . $file);
+		if(($file=="..") && ($dir==$topDirectory))
+			continue;
+		if(is_dir($path) &&
+			(strpos(addslash($path),$topDirectory)===0) &&
+			( $theSettings->uid<0 || isUserHavePermission($theSettings->uid,$theSettings->gid,$path,0x0005))
+			)
+			$dirs['/'.$file] = $path;
+		else
+		{
+			if(LFS::is_file($path)
+				&& ( $theSettings->uid<0 || isUserHavePermission($theSettings->uid,$theSettings->gid,$path,0x0004))
+				)
+				$files[$file." "] = $path;
+		}
+        }
+        closedir($dh);
+	ksort($files,SORT_STRING);
+	ksort($dirs,SORT_STRING);
+	$files = array_merge($dirs,$files);
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<head>
+<style>
+body { background-color: window; color: windowtext; border: 0px; margin: 0px; padding: 0px; -moz-user-select:none; }
+td { padding-top: 1px; padding-bottom: 1px; padding-left: 0px; padding-right: 0px; cursor:default; font-size: 11px; font-family: Tahoma, Arial, Helvetica, sans-serif; }
+.rmenuobj { border-width: 0; }
+.rmenuitem { color: windowtext; }
+.rmenuitemselected { color: highlighttext; background-color: highlight; }
+</style>
+<title></title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script language='JavaScript'>
+
+document.oncontextmenu = function(e) { return false; }
+document.ondragstart = function() { return false; };
+document.onselectstart = function() { return false; };
+
+function bin2hex(s)
+{
+	var v,i, f = 0, a = [];
+	s += '';
+	f = s.length;
+	for (i = 0; i<f; i++)
+        	a[i] = s.charCodeAt(i).toString(16).replace(/^([\da-f])$/,"0$1");
+	return a.join('');
+}
+
+var doc = null;
+
+function init()
+{
+	doc = window.frameElement.ownerDocument;
+<?php
+	if(!$curFile)
+		$curFile = "/.";
+	echo "var el=document.getElementById('".bin2hex($curFile)."');";
+?>
+	document.documentElement.scrollTop = el.offsetTop;
+	menuClick(el);
+	if(/WebKit/i.test(navigator.userAgent))
+	{
+		var _timer=setInterval(function(){ scrollBy(1,1); clearInterval(_timer); },10);
+	}
+}
+
+selected = null;
+
+function menuClick(obj)
+{
+	if(selected)
+		selected.className = 'rmenuitem';
+	obj.className = 'rmenuitemselected';
+	selected = obj;
+	var code = obj.getAttribute('code');
+	if(code && doc)
+	{
+		var el = doc.getElementById(<?php echo $edit_id;?>);
+		el.value = decodeURIComponent(code);
+	}
+}
+
+function menuDblClick(obj)
+{
+	menuClick(obj);
+	location.search = "?dir="+obj.getAttribute('code')+ 
+		"&btn=" + <?php echo $btn_id;?> +
+		"&edit=" + <?php echo $edit_id;?> +
+		"&frame=" + <?php echo $frame_id;?> +
+		"&time=" + (new Date()).getTime();
+}
+
+function hideFrame()
+{
+	window.frameElement.style.visibility = "hidden";
+	window.frameElement.style.display = "none";
+	var edit = doc.getElementById(<?php echo $edit_id;?>);
+	var btn = doc.getElementById(<?php echo $btn_id;?>);
+	btn.value = "...";
+	edit.readOnly = false;
+}
+
+function menuDblClickAndExit(obj)
+{
+	menuClick(obj);
+	hideFrame();
+}
+
+</script>
+</head>
+<body onLoad='init()'>
+
+<table class='rmenuobj' cellpadding=0 cellspacing=0 width=100%>
+<?php
+foreach($files as $key=>$data)
+{
+	if(($key=='/.') || ($key[0]!='/'))
+		echo "<tr><td code='".rawurlencode($data)."' id='".bin2hex($key)."' class='rmenuitem' nowrap onclick='menuClick(this); return false;' ondblclick='menuDblClickAndExit(this); return false;'>";
+	else
+		echo "<tr><td code='".rawurlencode($data)."' id='".bin2hex($key)."' class='rmenuitem' nowrap onclick='menuClick(this); return false;' ondblclick='menuDblClick(this); return false;'>";
+	echo "&nbsp;&nbsp;";
+	echo $key;
+	echo "</td></tr>";
+}
+?>
+</table>
+</body>
---
+++ tags/plugins/_getdir/3.5/info.php
@@ -0,0 +1,94 @@
+<?php
+require_once( '../../php/util.php' );
+require_once( '../../php/settings.php' );
+
+if(isset($_REQUEST['mode']))
+{
+	$output = array();
+	$modes = explode(';',$_REQUEST['mode']);
+	$theSettings = rTorrentSettings::get();
+	if(in_array("labels",$modes))
+	{
+		$req = new rXMLRPCRequest( new rXMLRPCCommand("d.multicall", array("",getCmd("d.get_custom1="))) );
+		$labels = array();
+		if($req->run() && !$req->fault)
+		{
+			for($i=0; $i<count($req->val); $i++)
+			{
+				$val = trim(rawurldecode($req->val[$i]));
+				if( $val!='' )
+					$labels[$val] = true;
+			}
+			$output["labels"] = array_keys($labels);
+		}
+	}
+	if(in_array("dirlist",$modes))
+	{
+		$dh = false;
+		if(isset($_REQUEST['basedir']))
+		{
+			$dir = rawurldecode($_REQUEST['basedir']);
+			$theSettings->correctDirectory($dir);
+			$dh = @opendir($dir);
+			$dir = addslash($dir);
+
+			if( $dh &&
+				((strpos($dir,$topDirectory)!==0) ||
+				(($theSettings->uid>=0) &&
+				!isUserHavePermission($theSettings->uid,$theSettings->gid,$dir,0x0007))))
+			{
+				closedir($dh);
+				$dh = false;
+			}
+		}
+		if(!$dh)
+		{
+			$dir = isLocalMode() ? $theSettings->directory : $topDirectory;
+			if(strpos(addslash($dir),$topDirectory)!==0)
+				$dir = $topDirectory;
+			$dh = @opendir($dir);
+		}
+		if($dh)
+		{
+			$files = array();
+			$dir = addslash($dir);
+			while(false !== ($file = readdir($dh)))
+		        {
+				$path = fullpath($dir . $file);
+				if(($file=="..") && ($dir==$topDirectory))
+					continue;
+				if(is_dir($path) && is_readable($path) &&
+					(strpos(addslash($path),$topDirectory)===0) &&
+					( $theSettings->uid<0 || isUserHavePermission($theSettings->uid,$theSettings->gid,$path,0x0007))
+					)
+				{
+					$files[] = $file;
+				}
+			}
+		        closedir($dh);
+			sort($files,SORT_STRING);
+			$output["basedir"] = fullpath($dir);
+			$output["dirlist"] = $files;
+		}
+        }
+	if(in_array("channels",$modes) && $theSettings->isPluginRegistered('throttle'))
+	{
+		require_once( '../throttle/throttle.php' );
+		$trt = rThrottle::load();
+		$tnames = array();
+		foreach( $trt->thr as $thr )
+			$tnames[] = $thr["name"];
+		$output["channels"]  = $tnames;
+	}
+	if(in_array("ratios",$modes) && $theSettings->isPluginRegistered('ratio'))
+	{
+		require_once( '../ratio/ratio.php' );
+		$rat = rRatio::load();
+		$rnames = array();
+		foreach( $rat->rat as $r )
+			$rnames[] = $r["name"];
+		$output["ratios"] = $rnames;
+	}
+}
+
+cachedEcho(json_encode($output),"application/json");
