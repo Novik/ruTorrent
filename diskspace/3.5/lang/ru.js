e6ac15abc05918bc410ed58152c647581ec41aa4
novik65 <Novik@users.noreply.github.com>
2012-12-07 12:04:39 +0000

    mark version 3.5

---
+++ diskspace/3.5/lang/ru.js
@@ -0,0 +1,11 @@
+﻿/*
+ * PLUGIN DISKSPACE
+ *
+ * Russian language file.
+ *
+ * Author: 
+ */
+
+ theUILang.diskNotification = "Внимание! Ваш диск переполнен. До тех пор, пока Вы не освободите дисковое пространство, данные загружаться не будут, и rTorrent может работать не корректно.";
+
+thePlugins.get("diskspace").langLoaded();
---
+++ diskspace/3.5/lang/uk.js
@@ -0,0 +1,11 @@
+﻿/*
+ * PLUGIN DISKSPACE
+ *
+ * Ukrainian language file.
+ *
+ * Author: Oleksandr Natalenko (pfactum@gmail.com)
+ */
+
+ theUILang.diskNotification = "Попередження. Диск заповнено. Програма rTorrent може працювати некоректно, а дані не завантажуватимуться, поки не з’явиться вільне місце на диску.";
+
+thePlugins.get("diskspace").langLoaded();
