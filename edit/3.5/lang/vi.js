6d0f2b5131c938bb95593107a24309a49c70519d
novik65 <Novik@users.noreply.github.com>
2012-12-07 12:04:51 +0000

    mark version 3.5

---
+++ edit/3.5/lang/vi.js
@@ -0,0 +1,16 @@
+﻿/*
+ * PLUGIN EDIT
+ *
+ * Vietnamese language file.
+ *
+ * Author: Ta Xuan Truong (truongtx8 AT gmail DOT com)
+ */
+
+ theUILang.EditTrackers 		= "Chỉnh sửa torrent...";
+ theUILang.EditTorrentProperties	= "Các thuộc tính của Torrent";
+ theUILang.errorAddTorrent		= "Lỗi khi thêm tập tin torrent";
+ theUILang.errorWriteTorrent		= "Lỗi khi ghi tập tin torrent";
+ theUILang.errorReadTorrent		= "Lỗi khi đọc tập tin torrent";
+ theUILang.cantFindTorrent		= "Không tìm thấy tập tin torrent gốc."
+
+thePlugins.get("edit").langLoaded();
