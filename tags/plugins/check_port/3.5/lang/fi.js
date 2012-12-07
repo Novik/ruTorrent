b0b5c3c1a6d35181251209bace03e0ba052a4a26
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2012-12-07 12:03:24 +0000

    mark version 3.5

---
+++ tags/plugins/check_port/3.5/lang/fi.js
@@ -0,0 +1,16 @@
+﻿/*
+ * PLUGIN CHECK_PORT
+ *
+ * Finnish language file.
+ *
+ * Author: 
+ */
+
+ theUILang.checkPort		= "Check Port Status";
+ theUILang.portStatus		= [
+ 				  "Port status is unknown",
+ 				  "Port is closed",
+ 				  "Port is open"
+ 				  ];
+
+thePlugins.get("check_port").langLoaded();
---
+++ tags/plugins/check_port/3.5/lang/sk.js
@@ -0,0 +1,16 @@
+﻿/*
+ * PLUGIN CHECK_PORT
+ *
+ * Slovak language file.
+ *
+ * Author: 
+ */
+
+ theUILang.checkPort		= "Check Port Status";
+ theUILang.portStatus		= [
+ 				  "Port status is unknown",
+ 				  "Port is closed",
+ 				  "Port is open"
+ 				  ];
+
+thePlugins.get("check_port").langLoaded();
