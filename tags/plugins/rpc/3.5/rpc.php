4a17f9b1584204a33e2a8be574d1561345d52e3c
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2012-12-07 12:07:37 +0000

    mark version 3.5

---
+++ tags/plugins/rpc/3.5/rpc.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once( '../../php/xmlrpc.php' );
+
+if (!isset($HTTP_RAW_POST_DATA))
+	$HTTP_RAW_POST_DATA = file_get_contents("php://input");
+
+if(isset($HTTP_RAW_POST_DATA) 
+	&& !preg_match("/(execute|import)\s*=/i",$HTTP_RAW_POST_DATA))
+{
+	$result = rXMLRPCRequest::send($HTTP_RAW_POST_DATA);
+	if(!empty($result))
+	{
+		$pos = strpos($result, "\r\n\r\n");
+		if($pos !== false)
+			$result = substr($result,$pos+4);
+		cachedEcho($result, "text/xml");
+	}
+}
+
+header("HTTP/1.0 500 Server Error");
+cachedEcho("Link to XMLRPC failed. May be, rTorrent is down?","text/html");
