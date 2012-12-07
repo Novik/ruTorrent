e116e44d439420358b2a58d00a5a3f5ac57ad65f
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2012-12-07 12:04:10 +0000

    mark version 3.5

---
+++ tags/plugins/create/3.5/lang/el.js
@@ -0,0 +1,28 @@
+﻿/*
+ * PLUGIN CREATE
+ *
+ * Greek language file.
+ *
+ * Author: Chris Kanatas (ckanatas@gmail.com)
+ */
+
+ theUILang.mnu_create			= "Δημιουργία Torrent...";
+ theUILang.CreateNewTorrent		= "Δημιουργία Νέου Torrent";
+ theUILang.SelectSource 		= "Επιλογή πηγής";
+ theUILang.TorrentProperties		= "Ιδιότητες Torrent";
+ theUILang.PieceSize			= "Μέγεθος κομματιού";
+ theUILang.Other			= "Άλλο";
+ theUILang.StartSeeding 		= "Έναρξη αποστολής";
+ theUILang.PrivateTorrent		= "Ιδιωτικό torrent";
+ theUILang.torrentCreate		= "Δημιουργία...";
+ theUILang.BadTorrentData		= "Πρέπει να συμπληρώσετε όλα τα απαιτούμενα πεδία!";
+ theUILang.createExternalNotFound	= "Πρόσθετο Δημιουργία: Το πρόσθετο δεν θα λειτουργήσει. Ο χρήστης του διακομιστή Web δεν έχει πρόσβαση σε εξωτερικό πρόγραμμα";
+ theUILang.incorrectDirectory		= "Εσφαλμένος φάκελος";
+ theUILang.cantExecExternal		= "Δεν μπορεί να εκτελεστεί ένα εξωτερικό πρόγραμμα";
+ theUILang.createConsole		= "Κονσόλα";
+ theUILang.createErrors 		= "Σφάλματα";
+ theUILang.torrentSave			= "Αποθήκευση";
+ theUILang.torrentKill			= "Τερματισμός";
+ theUILang.torrentKilled		= "Η λειτουργία τερματίστηκε.";
+
+thePlugins.get("create").langLoaded();
