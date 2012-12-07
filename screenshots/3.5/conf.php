3819662b8258fdcb671a539da2ff3192269472e8
novik65 <Novik@users.noreply.github.com>
2012-12-07 12:08:28 +0000

    mark version 3.5

---
+++ screenshots/3.5/conf.php
@@ -0,0 +1,15 @@
+<?php
+
+$pathToExternals['ffmpeg'] = '';		// Something like /usr/bin/ffmpeg. If empty, will be found in PATH.
+
+$extensions = array
+(
+	"3g2","3gp","4xm","iff","iss","mtv","roq","a64","ac3","anm","apc","asf","avi","avm2","avs","bethsoftvid",
+	"bink","c93","cavsvideo","cdg","dirac","dnxhd","dsicin","dts","dv","dv1394","dvd","ea","eac3","ffm","film_cpk",
+	"filmstrip","flic","flv","gxf","h261","h263","h264","idcin","image2","image2pipe",
+	"ingenient","ipmovie","ipod","iv8","ivf","m4v","matroska","mjpeg","mov","m4a","mj2",
+	"mp2","mp4","mpeg","mpeg1video","mpeg2video","mpegts","mpegtsraw","mpegvideo",
+	"msnwctcp","mvi","mxf","mxf_d10","nc","nsv","nuv","ogg","psp","psxstr","rawvideo","rm","rpl","rtsp",
+	"smk","svcd","swf","vcd","video4linux","video4linux2","vob","webm","wmv",
+	"mkv","ogm","mpg","mpv","m1v","m2v","mp2","qt","rmvb","dat","ts"
+);
---
+++ screenshots/3.5/lang/es.js
@@ -0,0 +1,20 @@
+﻿/*
+ * PLUGIN SCREENSHOTS
+ *
+ * Spanish language file.
+ *
+ * Author: 
+ */
+
+ theUILang.exFFMPEG		= "Capturas";
+ theUILang.exFrameWidth 	= "Ancho del Frame";
+ theUILang.exFramesCount	= "Cantidad de Frames";
+ theUILang.exStartOffset	= "Comenzar en frame offset";
+ theUILang.exBetween		= "Tiempo entre frames";
+ theUILang.exSave		= "Guardar";
+ theUILang.exSaveAll		= "Guardar todas";
+ theUILang.exScreenshot 	= "Captura";
+ theUILang.exPlayInterval	= "Intervalo de Slideshow";
+ theUILang.exImageFormat	= "Formato de imagen";
+
+thePlugins.get("screenshots").langLoaded();
