523c0b7c0d03bf2c205cb5d0796373d354706d93
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2012-12-07 12:07:14 +0000

    mark version 3.5

---
+++ tags/plugins/ratio/3.5/lang/fr.js
@@ -0,0 +1,26 @@
+﻿/*
+ * PLUGIN RATIO
+ *
+ * French language file.
+ *
+ * Author: Nicobubulle (nicobubulle@gmail.com)
+ */
+
+ theUILang.ratios		= "Ratios";
+ theUILang.ratio		= "Ratio";
+ theUILang.mnuRatio		= "Affecter un ratio";
+ theUILang.mnuRatioUnlimited	= "Pas de ratio";
+ theUILang.ratioName		= "Nom";
+ theUILang.minRatio		= "Min";
+ theUILang.maxRatio		= "Max";
+ theUILang.ratioUpload		= "Envoi";
+ theUILang.ratioAction		= "Action";
+ theUILang.ratioStop		= "Arrêt";
+ theUILang.ratioStopAndRemove	= "Arrêt/retrait du gpe";
+ theUILang.ratioErase		= "Efface";
+ theUILang.ratioEraseData	= "Supprime les données";
+ theUILang.maxTime		= "Temps";
+ theUILang.ratioDefault 	= "Groupe de ratio par défaut";
+ theUILang.setThrottleTo	= "Appliquer la vitesse";
+
+thePlugins.get("ratio").langLoaded();
---
+++ tags/plugins/ratio/3.5/lang/zh-cn.js
@@ -0,0 +1,26 @@
+﻿/*
+ * PLUGIN RATIO
+ *
+ * Chinese Simplified language file.
+ *
+ * Author: 
+ */
+
+ theUILang.ratios		= "分享率设置";
+ theUILang.ratio		= "分享率设置";
+ theUILang.mnuRatio		= "设置分享率";
+ theUILang.mnuRatioUnlimited	= "无限制";
+ theUILang.ratioName		= "名称";
+ theUILang.minRatio		= "最小";
+ theUILang.maxRatio		= "最大";
+ theUILang.ratioUpload		= "上传";
+ theUILang.ratioAction		= "动作";
+ theUILang.ratioStop		= "停止";
+ theUILang.ratioStopAndRemove	= "停止 & 清除设置";
+ theUILang.ratioErase		= "移除";
+ theUILang.ratioEraseData	= "Remove data";
+ theUILang.maxTime		= "Time";
+ theUILang.ratioDefault 	= "Default ratio group";
+ theUILang.setThrottleTo	= "Set channel to";
+
+thePlugins.get("ratio").langLoaded();
