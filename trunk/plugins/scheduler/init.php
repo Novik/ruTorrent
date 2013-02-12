3128a1ff87808b6a4f831a63b7c2eb8140baad93
Novik65 <Novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2013-02-12 06:47:26 +0000

    minor fix

--- trunk/plugins/scheduler/init.php
+++ trunk/plugins/scheduler/init.php
@@ -5,7 +5,7 @@ require_once( '../plugins/scheduler/scheduler.php' );
 $schd = rScheduler::load();
 $schd->apply();
 
-$req = new rXMLRPCRequest( $theSettings->getAbsScheduleCommand('scheduler',$updateInterval,
+$req = new rXMLRPCRequest( $theSettings->getAbsScheduleCommand('scheduler',$updateInterval*60,
 	getCmd('execute').'={sh,-c,'.escapeshellarg(getPHP()).' '.escapeshellarg($rootPath.'/plugins/scheduler/update.php').' '.escapeshellarg(getUser()).' & exit 0}' ) );
 if($req->run() && !$req->fault)
 {
