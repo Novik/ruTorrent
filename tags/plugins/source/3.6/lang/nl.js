0eb02b403eb43f4fbc72f56cde61143891bc5cbe
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2013-10-16 11:35:26 +0000

    mark version 3.6

---
+++ tags/plugins/source/3.6/lang/nl.js
@@ -0,0 +1,12 @@
+﻿/*
+ * PLUGIN SOURCE
+ *
+ * Dutch language file.
+ *
+ * Author: rascalli (rascallim@gmail.com)
+ */
+
+ theUILang.getSource		= "Download .torrent";
+ theUILang.cantFindTorrent	= "Torrent file voor deze download niet gevonden.";
+
+thePlugins.get("source").langLoaded();
---
+++ tags/plugins/source/3.6/lang/sv.js
@@ -0,0 +1,12 @@
+﻿/*
+ * PLUGIN SOURCE
+ *
+ * Swedish language file.
+ *
+ * Author: Magnus Holm (holmen@brasse.se) 
+ */
+
+ theUILang.getSource		= "Hämta torrentfil";
+ theUILang.cantFindTorrent	= "Torrentfilen för denna nedladdning kunde inte hittas.";
+
+thePlugins.get("source").langLoaded();
