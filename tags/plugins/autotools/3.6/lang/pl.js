275a5e2b143553684b15eb7e0790922aaca0e1c8
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2013-10-16 11:25:05 +0000

    mark version 3.6

---
+++ tags/plugins/autotools/3.6/lang/pl.js
@@ -0,0 +1,26 @@
+﻿/*
+ * PLUGIN AUTOTOOLS
+ *
+ * Polish language file.
+ *
+ * Author: Dare (piczok@gmail.com)
+ */
+
+ var s_PluginFail			= "Wtyczka nie będzie działać.";
+
+ theUILang.autotools			= "Autotools";
+ theUILang.autotoolsEnableLabel 	= "Włącz funkcję \"AutoEtykieta\", Template: ";
+ theUILang.autotoolsEnableMove		= "Włącz funkcję \"AutoPrzenoszenie\" ";
+ theUILang.autotoolsPathToFinished	= "Ścieżka dla ukończonych pobierań";
+ theUILang.autotoolsEnableWatch 	= "Włącz funkcję \"AutoWatch\" ";
+ theUILang.autotoolsPathToWatch 	= "Ścieżka do bazowego katalogu obserwowanego";
+ theUILang.autotoolsWatchStart		= "Uruchom pobieranie automatycznie";
+ theUILang.autotoolsNoPathToFinished	= "Wtyczka Autotools: ścieżka dla ukończonych pobierań nie jest skonfigurowana. " + s_PluginFail;
+ theUILang.autotoolsNoPathToWatch	= "Autotools plugin: ścieżka bazowa jest nieustawiona. " + s_PluginFail;
+ theUILang.autotoolsFileOpType		= "Rodzaj operacji";
+ theUILang.autotoolsFileOpMove		= "Przenieś";
+ theUILang.autotoolsFileOpHardLink 	= "Hard link";
+ theUILang.autotoolsFileOpCopy		= "Kopia";
+ theUILang.autotoolsFileOpSoftLink	= "Soft link";
+
+thePlugins.get("autotools").langLoaded();
