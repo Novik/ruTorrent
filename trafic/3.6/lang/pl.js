57958018be14ac727d745e6f77f10247f4056d09
novik65 <Novik@users.noreply.github.com>
2013-10-16 11:36:12 +0000

    mark version 3.6

---
+++ trafic/3.6/lang/pl.js
@@ -0,0 +1,19 @@
+﻿/*
+ * PLUGIN TRAFFIC
+ *
+ * Polish language file.
+ *
+ * Author: Dare (piczok@gmail.com)
+ */
+
+ theUILang.traf 		= "Ruch";
+ theUILang.perDay		= "Przez dzień";
+ theUILang.perMonth		= "Przez miesiąc";
+ theUILang.perYear		= "Przez rok";
+ theUILang.allTrackers		= "Wszystkie trackery";
+ theUILang.ClearButton		= "Wyczyść";
+ theUILang.ClearQuest		= "Czy na pewno chcesz wyczyścić statystyki dla tego trackera?";
+ theUILang.selectedTorrent	= "Wybrany torrent";
+ theUILang.ratioDay		= "Ratio/day";
+ theUILang.ratioWeek		= "Ratio/week";
+ theUILang.ratioMonth		= "Ratio/month";
---
+++ trafic/3.6/lang/ru.js
@@ -0,0 +1,19 @@
+﻿/*
+ * PLUGIN TRAFFIC
+ *
+ * Russian language file.
+ *
+ * Author: 
+ */
+
+ theUILang.traf 		= "Трафик";
+ theUILang.perDay		= "За сутки";
+ theUILang.perMonth		= "За месяц";
+ theUILang.perYear		= "За год";
+ theUILang.allTrackers		= "Все трекеры";
+ theUILang.ClearButton		= "Очистить";
+ theUILang.ClearQuest		= "Вы действительно хотите очистить статистику для выбранных трекеров?";
+ theUILang.selectedTorrent	= "Выбранный торрент";
+ theUILang.ratioDay		= "Ратио/день";
+ theUILang.ratioWeek		= "Ратио/неделя";
+ theUILang.ratioMonth		= "Ратио/месяц";
