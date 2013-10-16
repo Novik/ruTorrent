10855983b941ce8d281840574cc38f426d7ab243
novik65 <Novik@users.noreply.github.com>
2013-10-16 11:26:56 +0000

    mark version 3.6

---
+++ diskspace/3.6/lang/lv.js
@@ -0,0 +1,11 @@
+﻿/*
+ * PLUGIN DISKSPACE
+ *
+ * Latvian language file.
+ *
+ * Author: 
+ */
+
+ theUILang.diskNotification = "Warning! The disk is full. rTorrent may not run correctly, and no data will be downloaded until you free some disk space.";
+
+thePlugins.get("diskspace").langLoaded();
