6dd87bf7caada209a229c10e15dfcebc6be1248a
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2013-10-16 11:28:21 +0000

    mark version 3.6

---
+++ tags/plugins/filedrop/3.6/lang/sv.js
@@ -0,0 +1,13 @@
+﻿/*
+ * PLUGIN FILEDROP
+ *
+ * Swedish language file.
+ *
+ * Author: Magnus Holm (holmen@brasse.se) 
+ */
+
+ theUILang.doesntSupportHTML5	= "Insticksprogram, filedrop: Din webbläsare stöder inte filuppladdningar med HTML5. Insticksprogrammet inaktiverades.";
+ theUILang.tooManyFiles 	= "Insticksprogram, filedrop: För många filer. Skall vara <= ";
+ theUILang.fileTooLarge 	= "är för stor. Var god ladda upp filer under";
+
+thePlugins.get("filedrop").langLoaded();
