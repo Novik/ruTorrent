d52b231af30fb2a63e9ea74e1c18f72403885cb4
novik65 <Novik@users.noreply.github.com>
2013-10-16 11:34:26 +0000

    mark version 3.6

---
+++ screenshots/3.6/lang/pt.js
@@ -0,0 +1,20 @@
+﻿/*
+ * PLUGIN SCREENSHOTS
+ *
+ * Portuguese language file.
+ *
+ * Author: 
+ */
+
+ theUILang.exFFMPEG		= "Screenshots";
+ theUILang.exFrameWidth 	= "Frame width";
+ theUILang.exFramesCount	= "Frames count";
+ theUILang.exStartOffset	= "Start frame offset";
+ theUILang.exBetween		= "Time between frames";
+ theUILang.exSave		= "Save";
+ theUILang.exSaveAll		= "Save all";
+ theUILang.exScreenshot 	= "Screenshot";
+ theUILang.exPlayInterval	= "Slideshow interval";
+ theUILang.exImageFormat	= "Image format";
+
+thePlugins.get("screenshots").langLoaded();
---
+++ screenshots/3.6/lang/uk.js
@@ -0,0 +1,20 @@
+﻿/*
+ * PLUGIN SCREENSHOTS
+ *
+ * Ukrainian language file.
+ *
+ * Author: Oleksandr Natalenko (pfactum@gmail.com)
+ */
+
+ theUILang.exFFMPEG		= "Знімки екрана";
+ theUILang.exFrameWidth 	= "Ширина кадра";
+ theUILang.exFramesCount	= "Кількість кадрів";
+ theUILang.exStartOffset	= "Початкове зміщення";
+ theUILang.exBetween		= "Час між кадрами";
+ theUILang.exSave		= "Зберегти";
+ theUILang.exSaveAll		= "Зберегти все";
+ theUILang.exScreenshot 	= "Знімок екрана";
+ theUILang.exPlayInterval	= "Інтервал показу";
+ theUILang.exImageFormat	= "Формат";
+
+thePlugins.get("screenshots").langLoaded();
