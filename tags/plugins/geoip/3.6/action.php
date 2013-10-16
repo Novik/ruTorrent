5e911c3c1d20fb1aa3b65be807a691a8661353c3
novik65 <novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2013-10-16 11:28:35 +0000

    mark version 3.6

---
+++ tags/plugins/geoip/3.6/action.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once( '../../php/util.php' );
+eval( getPluginConf( 'geoip' ) );
+require_once( 'ip_db.php' );
+
+$db = new ipDB();
+$db->add($_REQUEST["ip"],$_REQUEST["comment"]);
+
+cachedEcho( json_encode( array( "ip"=>$_REQUEST["ip"], "comment"=>$_REQUEST["comment"] ) ), "application/json" );
---
+++ tags/plugins/geoip/3.6/conf.php
@@ -0,0 +1,11 @@
+<?php
+	// configuration parameters
+
+	$retrieveCountry = true;
+	$retrieveHost = true;
+	$retrieveComments = true;
+
+	// For retrieve hosts
+
+	$dnsResolver = '8.8.8.8';	// use gethostbyaddr, if null
+	$dnsResolverTimeout = 1;	// timeout in seconds
