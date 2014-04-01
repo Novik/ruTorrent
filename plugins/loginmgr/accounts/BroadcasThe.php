d0e6b371558ced7be63fb2c7baf1e21d5f7a5601
novik65 <Novik@users.noreply.github.com>
2014-04-01 05:50:40 +0000

    fix BTN engine for plugin LoginMgr

--- plugins/loginmgr/accounts/BroadcasThe.php
+++ plugins/loginmgr/accounts/BroadcasThe.php
@@ -2,7 +2,7 @@
 
 class BroadcasTheAccount extends commonAccount
 {
-	public $url = "http://broadcasthe.net";
+	public $url = "https://broadcasthe.net";
 
 	protected function isOK($client)
 	{
@@ -11,16 +11,26 @@ class BroadcasTheAccount extends commonAccount
 	protected function login($client,$login,$password,&$url,&$method,&$content_type,&$body,&$is_result_fetched)
 	{                                                                   
 	        $is_result_fetched = false;
-		if($client->fetch( $this->url."/login.php" ))
+		if($client->fetch( $this->url ) &&
+			($client->status==503) &&
+			preg_match( '`name="jschl_vc" value="(?P<vc>[^"]+)"`si', $client->results, $matches ) &&
+			preg_match( '`a\.value = (?P<n1>\d+)\+(?P<n2>\d+)\*(?P<n3>\d+);`si', $client->results, $digits ))
 		{
-                        $client->setcookies();
-			$client->referer = $this->url."/login.php";
-
-        		if($client->fetch( $this->url."/login.php","POST","application/x-www-form-urlencoded", 
-				"username=".rawurlencode($login)."&password=".rawurlencode($password)."&keeplogged=1&login=Log+In%21" ))
+			$ans = intval($digits['n1'])+intval($digits['n2'])*intval($digits['n3'])+15;
+			$client->setcookies();
+			$client->referer = $this->url;
+			if($client->fetch( $this->url."/cdn-cgi/l/chk_jschl?jschl_vc=".$matches["vc"]."&jschl_answer=".$ans ) &&
+				($client->status==200))
 			{
-				$client->setcookies();
-				return(true);
+	                        $client->setcookies();
+				$client->referer = $this->url."/login.php";
+	        		if($client->fetch( $this->url."/login.php","POST","application/x-www-form-urlencoded", 
+					"username=".rawurlencode($login)."&password=".rawurlencode($password)."&keeplogged=1&login=Log+In%21" ) &&
+					($client->status==200))
+				{
+					$client->setcookies();
+					return(true);
+				}
 			}
 		}
 		return(false);
--- plugins/rutracker_check/init.php
+++ plugins/rutracker_check/init.php
@@ -4,7 +4,7 @@ require_once( "xmlrpc.php" );
 eval( getPluginConf( $plugin["name"] ) );
 
 $session = rTorrentSettings::get()->session;
-if( !strlen($session) || !@file_exists(addslash(rTorrentSettings::get()->session).'.') )
+if( !strlen($session) || !@file_exists(addslash($session).'.') )
 {
 	$jResult .= "plugin.disable(); noty('".$plugin["name"].": '+theUILang.webBadSessionError+' (".$session.").','error');";
 }
