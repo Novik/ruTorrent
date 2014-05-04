29da28de52831f059334d446cd2ab30c9f4431c6
Novik65 <Novik65@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2014-05-04 09:04:19 +0000

    fixes in the search engines

--- plugins/extsearch/engines/KickAssTorrents.php
+++ plugins/extsearch/engines/KickAssTorrents.php
@@ -25,7 +25,7 @@ class KickAssTorrentsEngine extends commonEngine
 			if( ($cli==false) || (strpos($cli->results, "<h2>Nothing found!</h2>")!==false) )
 				break;
 			$res = preg_match_all('`href="magnet:(?P<link>.*)".*<div class="torrentname">.*'.
-				'<a href="(?P<desc>.*)" class=".*bold">(?P<name>.*)</a>.*'.
+				'<a href="(?P<desc>.*)" class="cellMainLink">(?P<name>.*)</a>.*'.
 				'<span id="cat_\d+">(?P<cat>.*)</span>.*'.
 				'<td class="nobr.*">(?P<size>.*)</td>.*'.
 				'<td.*>.*</td>.*'.
