00ac835124aaf3dac4f210ee2a42931a7a1811da
novik65@gmail.com <Novik@users.noreply.github.com>
2014-06-22 10:22:41 +0000

    add torrenttech engine to loginmgr

---
+++ plugins/loginmgr/accounts/TorrentTech.php
@@ -0,0 +1,27 @@
+<?php
+
+class TorrentTechAccount extends commonAccount
+{
+	public $url = "http://www.torrentech.org";
+
+	protected function isOK($client)
+	{
+		return(strpos($client->results, 'type="password"')===false);
+	}
+	protected function login($client,$login,$password,&$url,&$method,&$content_type,&$body,&$is_result_fetched)
+	{                                                                   
+	        $is_result_fetched = false;
+		if($client->fetch( $this->url ))
+		{
+			$client->setcookies();
+			if(preg_match('`<form action="'.$this->url.'/index.php\?s=(?P<s>.+)&amp;act=Login&amp;CODE=01&amp;CookieDate=1`',$client->results, $matches) &&
+				$client->fetch( $this->url."/index.php?s=".$matches['s']."&act=Login&CODE=01&CookieDate=1","POST","application/x-www-form-urlencoded", 
+				"UserName=".rawurlencode($login)."&PassWord=".rawurlencode($password) ))
+			{
+				$client->setcookies();
+				return(true);
+			}
+		}			
+		return(false);
+	}
+}
