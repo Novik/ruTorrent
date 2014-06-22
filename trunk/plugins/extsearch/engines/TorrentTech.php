7ecf1c4d0b4482fd6eec801ea7243b434d27b362
novik65@gmail.com <novik65@gmail.com@f4b6cfda-67a8-11de-ba0c-230037762ff9>
2014-06-22 10:23:39 +0000

    add torrenttech engine to extsearch

---
+++ trunk/plugins/extsearch/engines/TorrentTech.php
@@ -0,0 +1,45 @@
+<?php
+
+class TorrentTechEngine extends commonEngine
+{
+	public $defaults = array( "public"=>false, "page_size"=>25, "auth"=>1 );
+
+	public function action($what,$cat,&$ret,$limit,$useGlobalCats)
+	{
+		$added = 0;
+		$url = 'http://www.torrentech.org';
+		for($pg = 0; $pg<10; $pg++)
+		{
+			$cli = $this->fetch( $url.'/index.php?forums=all&act=search&CODE=01&search_in=titles&result_type=topics&torrents-only=1&keywords='.$what.'&st='.($pg*25) );
+			if($cli==false || (strpos($cli->results, ' type="password"')!==false)) 
+				break;
+			$res = preg_match_all("`preview_it\((?P<id>\d+), event\)' onmouseout='preview_hide\(\)'>(?P<name>.*)</a>.*".
+				'<span class="desc">(?P<subname>.*)<.*'.
+				'<td align="center" class="row1" nowrap="nowrap" style="border-left: 1px solid #000;"><span .*>(?P<seeds>.*)</span> &middot; (?P<leech>.*) &middot.*'.
+				'<td class="row1"><span class="desc">(?P<date>.*)<'.
+				'`siU', $cli->results, $matches);
+			if($res)
+			{
+				for($i=0; $i<$res; $i++)
+				{
+					$link = $url."/index.php?act=attach&code=showtopic&tid=".$matches["id"][$i];
+					if(!array_key_exists($link,$ret))
+					{
+						$item = $this->getNewEntry();
+						$item["desc"] = $url."/index.php?showtopic=".$matches["id"][$i];
+						$item["name"] = self::removeTags($matches["name"][$i]).' / '.self::removeTags($matches["subname"][$i]);
+						$item["time"] = strtotime( str_replace(' - ',' ',str_replace('.','-',$matches["date"][$i])) );
+						$item["seeds"] = intval(self::removeTags($matches["seeds"][$i]));
+						$item["peers"] = intval(self::removeTags($matches["leech"][$i]));
+						$ret[$link] = $item;
+						$added++;
+						if($added>=$limit)
+							return;
+					}
+				}
+			}
+			else
+				break;
+		}
+	}
+}
