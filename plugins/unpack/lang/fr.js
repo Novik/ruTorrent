f6a82bcfb77882ad08a5c7840e7b2c7d88233f88
novik65@gmail.com <Novik@users.noreply.github.com>
2014-06-25 10:42:41 +0000

    refactoring

--- plugins/unpack/lang/fr.js
+++ plugins/unpack/lang/fr.js
@@ -8,16 +8,11 @@
 
  theUILang.unpack		= "Décompression";
  theUILang.unpackPath		= "Décompresser vers (vide = dossier du torrent)";
- theUILang.processAll		= "Décompression en cours";
- theUILang.unpackTaskFailed	= "Décompression terminée avec erreurs";
- theUILang.unpackTaskOK 	= "Décompression terminée sans erreurs";
- theUILang.unpackTaskStarted	= "Décompression commencée";
  theUILang.unzipNotFound	= "Plug-in 'Unpack': rTorrent ne peut pas accéder au programme 'unzip'.";
  theUILang.unrarNotFound	= "Plug-in 'Unpack': rTorrent ne peut pas accéder au programme 'unrar'.";
  theUILang.unpackEnabled	= "Enable autounpacking if torrent's label matches filter";
  theUILang.unpackTorrents	= "Lors de la décompression d'un torrent, ajouter au nom du dossier";
  theUILang.unpackAddLabel	= "Étiquette du torrent";
  theUILang.unpackAddName	= "Nom du torrent";
- theUILang.unpackNoFiles	= "Aucune archive trouvée dans les données du torrent.";
 
 thePlugins.get("unpack").langLoaded();
--- plugins/unpack/lang/hu.js
+++ plugins/unpack/lang/hu.js
@@ -8,16 +8,11 @@
 
  theUILang.unpack		= "Kicsomagolás";
  theUILang.unpackPath		= "Csomagolja ki ide(Hagyja üresen ha a torrent könyvtárába szeretné)";
- theUILang.processAll		= "Folyamat minden mennyiségben";
- theUILang.unpackTaskFailed	= "Kicsomagolás befejezési hiba";
- theUILang.unpackTaskOK 	= "Kicsomagolás sikeresen befejezve";
- theUILang.unpackTaskStarted	= "Kicsomagolás elindúlt";
  theUILang.unzipNotFound	= "Csomagolás bővitmény: rTorrent felhasználó nem fér hozzá az 'unzip' programhoz.";
  theUILang.unrarNotFound	= "Csomagolás bővitmény: rTorrent felhasználó nem fér hozzá az 'unrar' programhoz.";
  theUILang.unpackEnabled	= "Engedélyezi az auto kicsomagolást ha a címke";
  theUILang.unpackTorrents	= "Elérési út hozzáfűzéee a kicsomagolás után a torrent adatokhoz";
  theUILang.unpackAddLabel	= "Torrent címke";
  theUILang.unpackAddName	= "Torrent név";
- theUILang.unpackNoFiles	= "Nem található archívuma torrent adatban";
 
 thePlugins.get("unpack").langLoaded();
