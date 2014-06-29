8ddce634ed7380c76eba3522d4f7bd8db09deaeb
novik65@gmail.com <Novik@users.noreply.github.com>
2014-06-29 09:31:10 +0000

    minor fix

--- plugins/create/lang/fr.js
+++ plugins/create/lang/fr.js
@@ -24,6 +24,6 @@
  theUILang.torrentSave			= "Enregistrer";
  theUILang.torrentKill			= "Stop";
  theUILang.torrentKilled		= "Le processus a été interrompu.";
- theUILang.recentTrackers		= "Recent trackers"; 
+ theUILang.recentTrackers		= "Trackers récents"; 
 
 thePlugins.get("create").langLoaded();
