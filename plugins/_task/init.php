2a225a3449dd6d7a541f50ec32a7100e105c321d
novik65@gmail.com <Novik@users.noreply.github.com>
2014-07-09 13:02:22 +0000

    minor fix

--- plugins/_task/init.php
+++ plugins/_task/init.php
@@ -1,6 +1,8 @@
 <?php
 eval(getPluginConf($plugin["name"]));
+require_once( dirname(__FILE__)."/task.php" );
 
+rTaskManager::cleanup();
 $theSettings->registerPlugin($plugin["name"],$pInfo["perms"]);
 
 $jResult .= "plugin.maxConcurentTasks = ".$maxConcurentTasks.";";
