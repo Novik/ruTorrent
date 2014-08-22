c04d7112fa7c9a1e409d5645e97a4be48b50d263
root <novik65@gmail.com>
2014-08-22 15:08:09 +0400

    remove googlecode link

--- js/content.js
+++ js/content.js
@@ -186,7 +186,7 @@ function makeContent()
 			'&nbsp;&nbsp;&nbsp;rTorrent adaptation (ruTorrent):<br/>'+
 			'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Moskalets Alexander (<a href="mailto:novik65@gmail.com">Novik</a>)<br/>'+
 			'<br/>'+
-			'<strong>'+theUILang.Check_new_version+'&nbsp;<a href="http://rutorrent.googlecode.com" target=_blank>'+theUILang.here+'</a></strong><br/>'+
+			'<strong>'+theUILang.Check_new_version+'&nbsp;<a href="https://github.com/Novik/ruTorrent" target=_blank>'+theUILang.here+'</a></strong><br/>'+
 			'<br/>'+
 			'<center><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=2KEV2MSBTF99U" target=_blank><img src="images/btn_donate.gif" border=0/></a></center>'+
 		'</div>');
