9662be3c443a51766c41bac46800d39e78c9b7f8
Davor Babic <davor@davor.se>
2014-09-26 17:11:25 +0200

    _noty2 now only loads if the browser has support for desktop notifications. changed default icon to favicon.

--- plugins/_noty2/init.js
+++ plugins/_noty2/init.js
@@ -2,25 +2,19 @@
  * _noty2 -- (c) 2013 Davor Babic <davor@davor.se>
  * A drop-in replacement for the rutorrent plugin _noty.
  *
- * https://github.com/davorb/_noty2
- *
  * Licensed under the MIT licenses:
  * http://www.opensource.org/licenses/mit-license.php
  */
 (function($) {
-  $.noty = function(options) {
-    var message, options;
-    message = options.text ? options.text : options;
-    options = {
-      icon: 'plugins/_noty2/icon.png',
-      body: message
-    };
+  if ('Notification' in window) {
+    $.noty = function(options) {
+      options.icon = 'favicon.ico';
+      options.body = options.text ? options.text : options;
 
-    if ('Notification' in window) {
       if (Notification.permission === 'granted' ||
           (('webkitNotifications' in window &&
             window.webkitNotifications.checkPermission() === 0))) {
-        new Notification('rutorrent', options); // 'plugins/_noty2/icon.png',
+        new Notification('rutorrent', options);
       } else if (Notification.permission !== 'denied') {
         Notification.requestPermission(function(permission) {
           if (!('permission' in Notification)) { // for Chrome
@@ -32,6 +26,6 @@
           }
         });
       }
-    }
-  };
+    };
+  }
 })(jQuery);
