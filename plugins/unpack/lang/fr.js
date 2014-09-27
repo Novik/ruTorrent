369b3fe36e575548a239dd9e0df12268bf8a32c8
R1kk3r <gael.jobin@switzerlandmail.ch>
2014-09-28 00:01:04 +0200

    Push the file that was deleted by a previous "bad managed" conflict

---
+++ plugins/unpack/lang/fr.js
@@ -0,0 +1,19 @@
+/*
+ * PLUGIN UNPACK
+ *
+ * French language file.
+ *
+ * Author: Nicobubulle (nicobubulle@gmail.com)
+ */
+
+ theUILang.unpack		= "Décompression";
+ theUILang.unpackPath		= "Décompresser vers (vide = dossier du torrent)";
+ theUILang.unzipNotFound	= "Plug-in 'Unpack': rTorrent ne peut pas accéder au programme 'unzip'.";
+ theUILang.unrarNotFound	= "Plug-in 'Unpack': rTorrent ne peut pas accéder au programme 'unrar'.";
+ theUILang.unpackEnabled	= "Activer la décompression auto si l'étiquette correspond au filtre";
+ theUILang.unpackTorrents	= "Lors de la décompression d'un torrent, ajouter au nom du dossier";
+ theUILang.unpackAddLabel	= "Étiquette du torrent";
+ theUILang.unpackAddName	= "Nom du torrent";
+
+thePlugins.get("unpack").langLoaded();
+
