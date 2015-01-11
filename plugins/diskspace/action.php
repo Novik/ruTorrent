8dd6baa7fccde328c16bbb674297278733ac99b9
gmelikov <mail@gmelikov.ru>
2015-01-11 17:56:38 +0300

    Update action.php

--- plugins/diskspace/action.php
+++ plugins/diskspace/action.php
@@ -1,3 +1,3 @@
 <?php
 	require_once( '../../php/util.php' );
-	cachedEcho('{ "total": '.disk_total_space($topDirectory).', "free": '.disk_free_space($topDirectory).' }',"application/json");
+	cachedEcho('{ "total": '.disk_total_space($_GET["p"]).', "free": '.disk_free_space($_GET["p"]).' }',"application/json");
