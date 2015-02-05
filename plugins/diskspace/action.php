687a723d277ac599834bee09a8d787a00814585e
indvd00m <gotoindvdum@gmail.com>
2015-02-05 21:53:38 +0300

    plugins/diskspace: possibility to set mount point of a torrents partition

--- plugins/diskspace/action.php
+++ plugins/diskspace/action.php
@@ -1,3 +1,4 @@
 <?php
+	include 'conf.php';
 	require_once( '../../php/util.php' );
-	cachedEcho('{ "total": '.disk_total_space($topDirectory).', "free": '.disk_free_space($topDirectory).' }',"application/json");
+	cachedEcho('{ "total": '.disk_total_space($partitionDirectory).', "free": '.disk_free_space($partitionDirectory).' }',"application/json");
