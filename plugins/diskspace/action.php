25e72aefbcbc6b47346d9cd9ec613fcd702c4063
Novik <novik65@gmail.com>
2015-02-23 14:49:25 +0300

    minor fix

--- plugins/diskspace/action.php
+++ plugins/diskspace/action.php
@@ -1,4 +1,4 @@
 <?php
-	include 'conf.php';
 	require_once( '../../php/util.php' );
+	eval( getPluginConf( 'diskspace' ) );
 	cachedEcho('{ "total": '.disk_total_space($partitionDirectory).', "free": '.disk_free_space($partitionDirectory).' }',"application/json");
