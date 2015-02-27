06e77eea6a99532e5a14c42b09fbf3d413229f48
ChriZathens <c_kan1@hotmail.com>
2015-02-27 11:23:12 +0200

    minor fix

--- plugins/rssurlrewrite/lang/el.js
+++ plugins/rssurlrewrite/lang/el.js
@@ -13,7 +13,7 @@
  theUILang.rssDelRule		= "Διαγραφή";
  theUILang.rssCheckRule 	= "Δοκιμή";
  theUILang.rssRulesLegend	= "Ρυθμίσεις κανόνα";
- theUILang.rssSrcHref		= "Αν το URL του torrent που κατεβαίνει ταιριάζει με το πρότυπο ";
+ theUILang.rssSrcHref		= "Αν το URL του torrent που κατεβαίνει ταιριάζει με το πρότυπο";
  theUILang.rssSrcGuid		= "Αν το URL της περιγραφής του torrent ταιριάζει με το πρότυπο";
  theUILang.rssDstHref		= "τότε αντικατέστησε το URL του torrent που κατεβαίνει με";
  theUILang.rssDstGuid		= "τότε αντικατέστησε το URL της περιγραφής του torrent με";
