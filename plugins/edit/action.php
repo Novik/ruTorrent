1ee1e13308a3f25c241b129aeeba80fd5cb786ad
Novik <novik65@gmail.com>
2015-04-13 14:15:00 +0300

    add batch processing for plugin 'edit'

--- plugins/edit/action.php
+++ plugins/edit/action.php
@@ -6,7 +6,7 @@ require_once( '../../php/rtorrent.php' );
 ignore_user_abort(true);
 set_time_limit(0);
 $errors = array();
-$hash = null;
+$hashes = array();
 if(!isset($HTTP_RAW_POST_DATA))
 	$HTTP_RAW_POST_DATA = file_get_contents("php://input");
 if(isset($HTTP_RAW_POST_DATA))
@@ -16,14 +16,30 @@ if(isset($HTTP_RAW_POST_DATA))
 	$trackers = array();
 	$comment = '';
 	$trackersCount = 0;
+	$private = 0;
+	$setComment = false;
+	$setTrackers = false;	
+	$setPrivate = false;
 	foreach($vars as $var)
 	{
 		$parts = explode("=",$var);
 		if($parts[0]=="hash")
-			$hash = $parts[1];
+			$hashes[] = $parts[1];
 		else
 		if($parts[0]=="comment")
 			$comment = trim(rawurldecode($parts[1]));
+		else
+		if($parts[0]=="private")
+			$private = intval($parts[1]);
+		else
+		if($parts[0]=="set_comment")
+			$setComment = intval($parts[1]);
+		else
+		if($parts[0]=="set_trackers")
+			$setTrackers = intval($parts[1]);			
+		else
+		if($parts[0]=="set_private")
+			$setPrivate = intval($parts[1]);			
 		else
 		if($parts[0]=="tracker")
 		{
@@ -45,77 +61,89 @@ if(isset($HTTP_RAW_POST_DATA))
 	}
 	if(count($trackers)>0)
 		$announce_list[] = $trackers;
-	if($hash)	
+	if($setComment || $setTrackers || $setPrivate)
 	{
-		$req = new rXMLRPCRequest( array(		
-			new rXMLRPCCommand("get_session"),
-			new rXMLRPCCommand("d.is_open",$hash),
-			new rXMLRPCCommand("d.is_active",$hash),
-			new rXMLRPCCommand("d.get_state",$hash),
-			new rXMLRPCCommand("d.get_tied_to_file",$hash),
-			new rXMLRPCCommand("d.get_custom1",$hash),
-			new rXMLRPCCommand("d.get_directory_base",$hash),
-			new rXMLRPCCommand("d.get_connection_seed",$hash),
-			new rXMLRPCCommand("d.get_complete",$hash),
-			) );
-		$throttle = null;
-		if(rTorrentSettings::get()->isPluginRegistered("throttle"))
-			$req->addCommand(new rXMLRPCCommand("d.get_throttle_name",$hash));
-		if($req->run() && !$req->fault)
+		foreach($hashes as $hash)	
 		{
-			$isStart = (($req->val[1]!=0) && ($req->val[2]!=0) && ($req->val[3]!=0));
-			$fname = $req->val[0].$hash.".torrent";
-			if(empty($req->val[0]) || !is_readable($fname))
+			$req = new rXMLRPCRequest( array(		
+				new rXMLRPCCommand("get_session"),
+				new rXMLRPCCommand("d.is_open",$hash),
+				new rXMLRPCCommand("d.is_active",$hash),
+				new rXMLRPCCommand("d.get_state",$hash),
+				new rXMLRPCCommand("d.get_tied_to_file",$hash),
+				new rXMLRPCCommand("d.get_custom1",$hash),
+				new rXMLRPCCommand("d.get_directory_base",$hash),
+				new rXMLRPCCommand("d.get_connection_seed",$hash),
+				new rXMLRPCCommand("d.get_complete",$hash),
+				) );
+			$throttle = null;
+			if(rTorrentSettings::get()->isPluginRegistered("throttle"))
+				$req->addCommand(new rXMLRPCCommand("d.get_throttle_name",$hash));
+			if($req->run() && !$req->fault)
 			{
-				if(strlen($req->val[4]) && is_readable($req->val[4]))
-					$fname = $req->val[4];
-				else
-					$fname = null;
-			}
-			if($fname)
-			{
-				$torrent = new Torrent( $fname );		
-				if( !$torrent->errors() )
+				$isStart = (($req->val[1]!=0) && ($req->val[2]!=0) && ($req->val[3]!=0));
+				$fname = $req->val[0].$hash.".torrent";
+				if(empty($req->val[0]) || !is_readable($fname))
 				{
-					$torrent->clear_announce();
-					$torrent->clear_announce_list();
-					$torrent->clear_comment();
-					if(count($announce_list)>0)
+					if(strlen($req->val[4]) && is_readable($req->val[4]))
+						$fname = $req->val[4];
+					else
+						$fname = null;
+				}
+				if($fname)
+				{
+					$torrent = new Torrent( $fname );		
+					if( !$torrent->errors() )
 					{
-						$torrent->announce($announce_list[0][0]);
-						if($trackersCount>1)
-							$torrent->announce_list($announce_list);
-					}
-					$comment = trim($comment);
-					if(strlen($comment))
-						$torrent->comment($comment);
-					if(isset($torrent->{'rtorrent'}))
-						unset($torrent->{'rtorrent'});
-					if(count($req->val)>9)
-						$throttle = getCmd("d.set_throttle_name=").$req->val[9];
-					$eReq = new rXMLRPCRequest( new rXMLRPCCommand("d.erase", $hash ) );
-					if($eReq->run() && !$eReq->fault)
-					{
-						$label = rawurldecode($req->val[5]);
-						if(!rTorrent::sendTorrent($torrent, $isStart, false, $req->val[6], $label, false, ($req->val[8]==1), false,
-						        array(	getCmd("d.set_custom3")."=1",
-								getCmd("d.set_connection_seed=").$req->val[7],
-								$throttle)))
-							$errors[] = array('desc'=>"theUILang.errorAddTorrent", 'prm'=>$fname);
+						if($setPrivate)
+						{
+							$torrent->is_private($private);
+						}
+						if($setTrackers)
+						{
+							$torrent->clear_announce();
+							$torrent->clear_announce_list();
+							if(count($announce_list)>0)
+							{
+								$torrent->announce($announce_list[0][0]);
+								if($trackersCount>1)
+									$torrent->announce_list($announce_list);
+							}
+						}
+						if($setComment)
+						{
+							$torrent->clear_comment();
+							$comment = trim($comment);
+							if(strlen($comment))
+								$torrent->comment($comment);
+						}
+						if(isset($torrent->{'rtorrent'}))
+							unset($torrent->{'rtorrent'});
+						if(count($req->val)>9)
+							$throttle = getCmd("d.set_throttle_name=").$req->val[9];
+						$eReq = new rXMLRPCRequest( new rXMLRPCCommand("d.erase", $hash ) );
+						if($eReq->run() && !$eReq->fault)
+						{
+							$label = rawurldecode($req->val[5]);
+							if(!rTorrent::sendTorrent($torrent, $isStart, false, $req->val[6], $label, false, ($req->val[8]==1), false,
+							        array(	getCmd("d.set_custom3")."=1",
+									getCmd("d.set_connection_seed=").$req->val[7],
+									$throttle)))
+								$errors[] = array('desc'=>"theUILang.errorAddTorrent", 'prm'=>$fname);
+						}
+						else
+							$errors[] = array('desc'=>"theUILang.badLinkTorTorrent", 'prm'=>'');
 					}
 					else
-						$errors[] = array('desc'=>"theUILang.badLinkTorTorrent", 'prm'=>'');
+						$errors[] = array('desc'=>"theUILang.errorReadTorrent", 'prm'=>$fname);
 				}
 				else
-					$errors[] = array('desc'=>"theUILang.errorReadTorrent", 'prm'=>$fname);
-			}
-			else
-				$errors[] = array('desc'=>"theUILang.cantFindTorrent", 'prm'=>'');
-                }
-                else
-			$errors[] = array('desc'=>"theUILang.badLinkTorTorrent", 'prm'=>'');
-
+					$errors[] = array('desc'=>"theUILang.cantFindTorrent", 'prm'=>'');
+	                }
+        	        else
+				$errors[] = array('desc'=>"theUILang.badLinkTorTorrent", 'prm'=>'');
+		}
 	}
 }
 
-cachedEcho(json_encode(array( "errors"=>$errors, "hash"=>$hash )),"application/json");
+cachedEcho(json_encode(array( "errors"=>$errors, "hash"=>$hashes )),"application/json");
--- plugins/edit/init.js
+++ plugins/edit/init.js
@@ -3,33 +3,59 @@ plugin.loadLang();
 
 theWebUI.editTrackers = function(id)
 {
-	var trk = this.trackers[id];
-	var s = "";
-	if(trk.length)
-	{
-		var lastGroup = trk[0].group;
-		for(var i=0; i<trk.length; i++)
+	if(id)
+	{	
+		var trk = this.trackers[id];
+		var s = "";
+		if(trk.length)
 		{
-		        if(trk[i].name!="dht://")
-		        {
-				if(lastGroup != trk[i].group)
-				{
+			var lastGroup = trk[0].group;
+			for(var i=0; i<trk.length; i++)
+			{
+			        if(trk[i].name!="dht://")
+		        	{
+					if(lastGroup != trk[i].group)
+					{
+						s+='\r\n';
+						lastGroup = trk[i].group;
+					}
+					s+=trk[i].name;
 					s+='\r\n';
-					lastGroup = trk[i].group;
 				}
-				s+=trk[i].name;
-				s+='\r\n';
 			}
 		}
+		$('#etrackers').val($.trim(s));
+		$('#ecomment').val($.trim(this.torrents[id].comment));
+		$('#eset_trackers, #eset_comment').prop('checked',true);
+		$('#eset_private').prop('checked',false);
+		$('#eprivate').val(this.torrents[id].private);
 	}
-	$('#etrackers').val($.trim(s));
-	$('#ecomment').val($.trim(this.torrents[id].comment));
+	else
+	{
+		$('#etrackers').val('');
+		$('#ecomment').val('');
+		$('#eprivate').val('0');
+		$('input[type="checkbox"]').prop('checked',false);
+	}		
 	$('#editok').prop("disabled",false);
 	theDialogManager.show("tedit");
 }
 
 if(plugin.canChangeMenu())
 {
+	plugin.isTorrentCommandEnabled = theWebUI.isTorrentCommandEnabled;
+	theWebUI.isTorrentCommandEnabled = function(act,hash) 
+	{
+		if(act=="edittorrent")
+		{
+			if(!plugin.isTorrentCommandEnabled.call(this,act,hash))
+				return(false);
+			else
+				return(hash && (hash.length==40))
+		}
+		return(plugin.isTorrentCommandEnabled.call(this,act,hash));
+	}
+
 	plugin.createMenu = theWebUI.createMenu;
 	theWebUI.createMenu = function(e, id) 
 	{
@@ -38,7 +64,11 @@ if(plugin.canChangeMenu())
 		{
 			var el = theContextMenu.get(theUILang.Properties);
 			if(el)
-				theContextMenu.add(el,[theUILang.EditTrackers, (this.getTable("trt").selCount == 1) && (theWebUI.dID.length==40) ? "theWebUI.editTrackers('"+id+"')" : null]);
+			{
+				theContextMenu.add([theUILang.EditTrackers,  
+					((this.getTable("trt").selCount>1) && this.getHashes('edittorrent')) || this.isTorrentCommandEnabled("edittorrent",id) ? 
+					"theWebUI.editTrackers('"+theWebUI.dID+"')" : null]);				
+			}
 		}
 	}
 
@@ -48,7 +78,8 @@ if(plugin.canChangeMenu())
 		if(plugin.createTrackerMenu.call(theWebUI, e, id) && plugin.allStuffLoaded && plugin.enabled)
 		{
 			theContextMenu.add([CMENU_SEP]);
-			theContextMenu.add([theUILang.EditTrackers,  (theWebUI.dID.length==40) ? "theWebUI.editTrackers('"+theWebUI.dID+"')" : null]);
+			theContextMenu.add([theUILang.EditTrackers,  
+				this.isTorrentCommandEnabled("edittorrent",theWebUI.dID) ? "theWebUI.editTrackers('"+theWebUI.dID+"')" : null]);
 			return(true);
 		}
 		return(false);
@@ -58,22 +89,29 @@ if(plugin.canChangeMenu())
 theWebUI.sendEdit = function() 
 {
 	$('#editok').prop("disabled",true);
-	this.requestWithTimeout("?action=edittorrent",[this.receiveEdit, this], function()
+	this.requestWithTimeout("?action=edittorrent"+this.getHashes('edittorrent'),[this.receiveEdit, this], function()
 	{
 		theWebUI.timeout();
-		$('#editok').prop("disabled",true);
+		$('#editok').prop("disabled",false);
 	});
 }
 
 theWebUI.receiveEdit = function(d)
 {
 	$('#editok').prop("disabled",false);
-	if(!d.errors.length)
+	if(d.hash.length)
 	{
-		window.setTimeout( function() { theWebUI.getTrackers(d.hash) }, 1000 );
+		window.setTimeout( function() 
+		{ 
+			theWebUI.getTable("trt").clearSelection();
+			theWebUI.dID = "";
+			theWebUI.clearDetails();
+			theWebUI.getTorrents("list=1"); 
+		}, 1000 );
 		theDialogManager.hide("tedit");
 	}
-	else
+	if(d.errors.length)
+	{
 		for( var i=0; i<d.errors.length; i++)
 		{
 			var s = eval(d.errors[i].desc);
@@ -81,6 +119,7 @@ theWebUI.receiveEdit = function(d)
 				s = s + " ("+d.errors[i].prm+")";
 			noty(s,"error");
 		}
+	}			
 }
 
 plugin.onLangLoaded = function() 
@@ -88,10 +127,15 @@ plugin.onLangLoaded = function()
 	theDialogManager.make( "tedit", theUILang.EditTorrentProperties,
 		"<div class='cont fxcaret'>"+
 			"<fieldset>"+
-				"<label>"+theUILang.Trackers+": </label>"+
-				"<textarea id='etrackers'></textarea><br/>"+
-				"<label>"+theUILang.Comment+": </label>"+
-                               	"<input type='text' id='ecomment' name='ecomment' class='TextboxLarge'/><br/>"+
+				"<input type='checkbox' name='eset_trackers' id='eset_trackers'/><label for='eset_trackers'>"+theUILang.Trackers+": </label>"+
+				"<div class='text-wrapper'><textarea id='etrackers'></textarea></div>"+
+				"<input type='checkbox' name='eset_comment' id='eset_comment'/><label for='eset_comment'>"+theUILang.Comment+": </label>"+
+                               	"<input type='text' id='ecomment' name='ecomment' class='TextboxLarge'/>"+
+                               	"<input type='checkbox' name='eset_private' id='eset_private'/><label for='eset_private'>"+theUILang.trkPrivate+": </label>"+
+                               	"<select id='eprivate'>"+
+	                               	"<option value='0'>"+theUILang.no+"</option>"+
+	                               	"<option value='1'>"+theUILang.yes+"</option>"+	                               	
+                               	"</select>"+
 			"</fieldset>"+
 		"</div>"+
 		"<div class='aright buttons-list'><input type='button' value='"+theUILang.ok+"' class='OK Button' id='editok' onclick='theWebUI.sendEdit(); return(false);'/><input type='button' value='"+theUILang.Cancel+"' class='Cancel Button'/></div>",
@@ -100,14 +144,20 @@ plugin.onLangLoaded = function()
 
 rTorrentStub.prototype.edittorrent = function()
 {
-	this.content = "hash="+theWebUI.dID+"&comment="+encodeURIComponent($('#ecomment').val());
+	this.content = "&comment="+encodeURIComponent($('#ecomment').val())+
+		"&set_comment="+($('#eset_comment').is(":checked")+0)+
+		"&set_trackers="+($('#eset_trackers').is(":checked")+0)+
+		"&set_private="+($('#eset_private').is(":checked")+0)+
+		"&private="+$('#eprivate').val();
 	var arr = $('#etrackers').val().split("\n");
 	for(var i = 0; i<arr.length; i++)	
 	{
 		var s = arr[i].replace(/(^\s+)|(\s+$)/g, "");
 		if(s.toLowerCase()!='dht://')
-			this.content = 	this.content+"&tracker="+encodeURIComponent(s);
+			this.content += ("&tracker="+encodeURIComponent(s));
 	}
+	for( var i = 0; i < this.hashes.length; i++ )
+		this.content += ("&hash="+this.hashes[i]);	
 	this.contentType = "application/x-www-form-urlencoded";
 	this.mountPoint = "plugins/edit/action.php";
 	this.dataType = "json";
