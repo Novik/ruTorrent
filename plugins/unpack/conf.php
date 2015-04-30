3a573a3952fe7f3ea487bdfb304b35b07c1a693c
Andrew Parker <andrewmichaelparker@gmail.com>
2015-04-29 19:31:07 -0600

    Option to delete archive files after successful auto unpacking. Set $deleteAutoArchives variable to true in conf.php to enable. This will only delete the archives if AutoTools AutoMove is enabled with an operation of Copy, Soft Link or Hard Link and when auto unpacking, not manual unpacking.

--- plugins/unpack/conf.php
+++ plugins/unpack/conf.php
@@ -7,3 +7,6 @@ $pathToExternals['unzip'] = '';		// Something like /usr/bin/unzip. If empty, wil
 $pathToExternals['unrar'] = '';		// Something like /usr/bin/unrar. If empty, will be found in PATH.
 
 $cleanupAutoTasks = false;		// Remove autounpack tasks parameters after finish, otherwise will be shown in the 'Tasks' tab
+$deleteAutoArchives = false;	// Delete archive files after successful auto unpack. Will not remove archives unless AutoMove is enabled and the operation type is not Move.
+
+$unpack_debug_enabled = false;		// set "true" to enable debug output
--- plugins/unpack/unrar_file.sh
+++ plugins/unpack/unrar_file.sh
@@ -3,6 +3,11 @@
 # $1 - unrar
 # $2 - archive
 # $3 - output directory with tail slash
+# $5 - archive files to delete
 
 "$1" x -ai -c- -kb -o+ -p- -y -v -- "$2" "$3"
 
+ret=$?
+if [ $ret -le 1 ] && [ "$5" != '' ] ; then
+	rm "$5"
+fi
