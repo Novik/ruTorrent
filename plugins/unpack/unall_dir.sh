3d5008498a9f2e031d15b0a053071f62dc193df0
Andrew Parker <andrewmichaelparker@gmail.com>
2015-05-03 00:22:04 -0600

    Unpack: Adds an option in conf.php to that will unpack the contents of an archive to a temp directory and then move it from there to the actual destination folder. The temp folder is defined in $tempDirectory in the rutorrent config.php. This option only affects when archives are auto unpacked, not manual unpacks. This is useful if another program is monitoring your destination folder to further process the files. If the other program happens to scan the complete folder while an archive is in the process of unpacking, it will begin processing an incomplete file. With this option enable the unpacking process is done in a temp directory, and then the fully unpacked files are moved to the destination directory.

--- plugins/unpack/unall_dir.sh
+++ plugins/unpack/unall_dir.sh
@@ -5,12 +5,13 @@
 # $3 - output directory with tail slash
 # $4 - unzip
 # $5 - archive files to delete
+# $6 - unpack temp dir
 
 ret=0
-"$(dirname $0)"/unrar_dir.sh "$1" "$2" "$3" "$4" "$5"
+"$(dirname $0)"/unrar_dir.sh "$1" "$2" "$3" "$4" "$5" "$6"
 last=$?
 [ $last -gt 1 ] && ret=$last
-"$(dirname $0)"/unzip_dir.sh "$4" "$2" "$3" "$1" "$5"
+"$(dirname $0)"/unzip_dir.sh "$4" "$2" "$3" "$1" "$5" "$6"
 last=$?
 [ $last -gt 1 ] && ret=$last
 exit $ret
--- plugins/unpack/unrar_file.sh
+++ plugins/unpack/unrar_file.sh
@@ -4,10 +4,23 @@
 # $2 - archive
 # $3 - output directory with tail slash
 # $5 - archive files to delete
+# $6 - unpack temp dir
 
-"$1" x -ai -c- -kb -o+ -p- -y -v -- "$2" "$3"
+if [ "$6" != '' ] ; then
+	"$1" x -ai -c- -kb -o+ -p- -y -v -- "$2" "$6"
+	ret=$?
+else
+	"$1" x -ai -c- -kb -o+ -p- -y -v -- "$2" "$3"
+	ret=$?
+fi
 
 ret=$?
 if [ $ret -le 1 ] && [ "$5" != '' ] ; then
 	rm "$5"
+fi
+
+if [ "$6" != '' ] ; then
+	mkdir -p "$3"
+	mv "$6"* "$3"
+	rm -r "$6"
 fi
