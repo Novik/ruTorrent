62f149b3b139b4f0228983de5aee2a94fc87bdc3
Novik <novik65@gmail.com>
2015-07-31 15:28:19 +0300

    issue #1074

---
+++ plugins/loginmgr/accounts/KinozalTV.php
@@ -0,0 +1,31 @@
+<?php
+
+class KinozalTVAccount extends commonAccount
+{
+	public $url = "http://kinozal.tv";
+
+	protected function isOK($client)
+	{
+		return(strpos($client->results, 'type="password" name="password"')===false);
+	}
+	protected function login($client,$login,$password,&$url,&$method,&$content_type,&$body,&$is_result_fetched)
+	{
+	        $is_result_fetched = false;
+		if($client->fetch( $this->url ))
+		{
+                        $client->setcookies();
+			$client->referer = $this->url;
+        		if($client->fetch( $this->url."/takelogin.php","POST","application/x-www-form-urlencoded", 
+				"username=".rawurlencode($login)."&password=".rawurlencode($password) ))
+			{
+				$client->setcookies();
+				return(true);
+			}
+		}
+		return(false);
+	}
+	public function test($url)
+	{
+		return(preg_match( "/(\.|\/)kinozal.tv\/download.php/si", $url ));
+	}	
+}
