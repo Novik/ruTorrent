ac2f61ae2ad6b5a2ddaa94d3f7d3a58044bd57d3
Alexandre L. <alex131089@msn.com>
2015-08-14 03:00:22 +0200

    Allow call to rTorrentSettings

--- plugins/diskspace/action.php
+++ plugins/diskspace/action.php
@@ -1,4 +1,5 @@
 <?php
 	require_once( '../../php/util.php' );
+	require_once( '../../php/settings.php' );
 	eval( getPluginConf( 'diskspace' ) );
 	cachedEcho('{ "total": '.disk_total_space($partitionDirectory).', "free": '.disk_free_space($partitionDirectory).' }',"application/json");
