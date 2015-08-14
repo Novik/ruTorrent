09e8265f4a98144d70a0407f169b416d5879e936
Alexandre L. <alex131089@msn.com>
2015-08-14 03:01:27 +0200

    Use download directory by default to calculate free space

--- plugins/diskspace/conf.php
+++ plugins/diskspace/conf.php
@@ -2,4 +2,4 @@
 
 $diskUpdateInterval = 10;	// in seconds
 $notifySpaceLimit = 512;	// in Mb
-$partitionDirectory = &$topDirectory;	// mount point of a torrents partition, $topDirectory by default
+$partitionDirectory = rTorrentSettings::get()->directory; // mount point of a torrents partition, download directory by default
