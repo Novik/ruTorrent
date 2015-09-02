b62264eb84720a03b329a74695ee7a3ab017c93f
Alexandre L. <alex131089@msn.com>
2015-09-02 23:12:35 +0200

    Check if we are running locally
    To display the real space of the download directory, else fallback to the $topDirectory"

--- plugins/diskspace/conf.php
+++ plugins/diskspace/conf.php
@@ -2,4 +2,13 @@
 
 $diskUpdateInterval = 10;	// in seconds
 $notifySpaceLimit = 512;	// in Mb
-$partitionDirectory = rTorrentSettings::get()->directory; // mount point of a torrents partition, download directory by default
+
+// If we run locally && we the download directory seems to exists
+if ((stripos($scgi_host, 'unix:///') === 0 ||
+     stripos($scgi_host, '127.') === 0 ||
+     stripos($scgi_host, '::1') !== FALSE ||
+     stripos($scgi_host, 'localhost') === 0) && file_exists(rTorrentSettings::get()->directory)) {
+  $partitionDirectory = rTorrentSettings::get()->directory; // Then we can show the disk space of the download directory
+} else {
+  $partitionDirectory = &$topDirectory; // Else, we show $topDirectory by default as fallback
+}
