88ee82348f92eb5964514e0a0162c94a807f22bd
Alexandre L. <alex131089@msn.com>
2015-09-02 23:21:01 +0200

    Check if we have access to rTorrentSettings

--- plugins/diskspace/conf.php
+++ plugins/diskspace/conf.php
@@ -4,10 +4,12 @@ $diskUpdateInterval = 10;	// in seconds
 $notifySpaceLimit = 512;	// in Mb
 
 // If we run locally && we the download directory seems to exists
-if ((stripos($scgi_host, 'unix:///') === 0 ||
-     stripos($scgi_host, '127.') === 0 ||
-     stripos($scgi_host, '::1') !== FALSE ||
-     stripos($scgi_host, 'localhost') === 0) && file_exists(rTorrentSettings::get()->directory)) {
+if ( !empty(rTorrentSettings::get()->directory) && (
+      stripos($scgi_host, 'unix:///') === 0 ||
+      stripos($scgi_host, '127.') === 0 ||
+      stripos($scgi_host, '::1') !== FALSE ||
+      stripos($scgi_host, 'localhost') === 0
+     ) &&file_exists(rTorrentSettings::get()->directory) ) {
   $partitionDirectory = rTorrentSettings::get()->directory; // Then we can show the disk space of the download directory
 } else {
   $partitionDirectory = &$topDirectory; // Else, we show $topDirectory by default as fallback
