2bca6566cd7fab618eb497fd1df0ca8d27d0db4e
Alexandre L. <alex131089@msn.com>
2015-09-03 12:57:55 +0200

    Use ruTorrent internals

--- plugins/diskspace/conf.php
+++ plugins/diskspace/conf.php
@@ -4,12 +4,7 @@ $diskUpdateInterval = 10;	// in seconds
 $notifySpaceLimit = 512;	// in Mb
 
 // If we run locally && we the download directory seems to exists
-if ( !empty(rTorrentSettings::get()->directory) && (
-      stripos($scgi_host, 'unix:///') === 0 ||
-      stripos($scgi_host, '127.') === 0 ||
-      stripos($scgi_host, '::1') !== FALSE ||
-      stripos($scgi_host, 'localhost') === 0
-     ) &&file_exists(rTorrentSettings::get()->directory) ) {
+if ( isLocalMode() && rTorrentSettings::get()->linkExist && file_exists(rTorrentSettings::get()->directory) ) {
   $partitionDirectory = rTorrentSettings::get()->directory; // Then we can show the disk space of the download directory
 } else {
   $partitionDirectory = &$topDirectory; // Else, we show $topDirectory by default as fallback
