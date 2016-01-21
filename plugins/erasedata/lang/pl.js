6af97e9560cd428a88b51422c9f0336ca964dc2b
Micdu70 <Micdu70@users.noreply.github.com>
2016-01-21 09:07:14 +0100

    Modify prompt's "warning" of erasedata plugin

--- plugins/erasedata/lang/pl.js
+++ plugins/erasedata/lang/pl.js
@@ -1,4 +1,5 @@
+theUILang.Rem_torrents_content_prompt = "Do you really want to remove the selected torrent(s)? WARNING: This will delete torrent's content.";
 ﻿theUILang.Delete_data_with_path = "Delete Path";
-theUILang.Rem_torrents_with_path_prompt = "Do you really want to remove the selected torrent(s)? WARNING: This will delete all files in this torrent's current directory.",
+theUILang.Rem_torrents_with_path_prompt = "Do you really want to remove the selected torrent(s)? WARNING: This will delete all files in this torrent's current directory.";
 
 thePlugins.get("erasedata").langLoaded();
--- plugins/erasedata/lang/ru.js
+++ plugins/erasedata/lang/ru.js
@@ -1,4 +1,5 @@
+theUILang.Rem_torrents_content_prompt = "Do you really want to remove the selected torrent(s)? WARNING: This will delete torrent's content.";
 ﻿theUILang.Delete_data_with_path = "Удалить директорию";
-theUILang.Rem_torrents_with_path_prompt = "Вы действительно хотите удалить выбранные торрент(ы)? ВНИМАНИЕ: Все данные в корневой директории торрента будут удалены.",
+theUILang.Rem_torrents_with_path_prompt = "Вы действительно хотите удалить выбранные торрент(ы)? ВНИМАНИЕ: Все данные в корневой директории торрента будут удалены.";
 
 thePlugins.get("erasedata").langLoaded();
