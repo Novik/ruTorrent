ac064a47cef343ed639d29d44a20cbab9616dae8
Novik <novik65@gmail.com>
2016-01-27 12:29:26 +0300

    fix MyAnonamouse login

--- plugins/loginmgr/accounts/MyAnonamouse.php
+++ plugins/loginmgr/accounts/MyAnonamouse.php
@@ -10,7 +10,7 @@ class MyAnonamouseAccount extends commonAccount
 
 	protected function isOK($client)
 	{
-		return(strpos($client->results, '<form method="post" action="takelogin.php">')===false);
+		return(strpos($client->results, '<input type="password"')===false);
 	}
 	protected function login($client,$login,$password,&$url,&$method,&$content_type,&$body,&$is_result_fetched)
         {
@@ -19,9 +19,8 @@ class MyAnonamouseAccount extends commonAccount
                 {
                         $client->setcookies();
                         $client->referer = $this->url;
-
                         if($client->fetch( $this->url."/takelogin.php","POST","application/x-www-form-urlencoded",
-                                "username=".rawurlencode($login)."&password=".rawurlencode($password) ))
+                                "email=".rawurlencode($login)."&password=".rawurlencode($password) ))
                         {
                                 $client->setcookies();
                                 return(true);
