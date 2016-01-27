b5cda16f32ffd470c323cef6e2dbcbab567f8958
Limerainne <limerainne@gmail.com>
2016-01-28 03:21:23 +0900

    add korean translation for main lang, some plugins

---
+++ plugins/theme/lang/ko.js
@@ -0,0 +1,12 @@
+﻿/*
+ * PLUGIN THEME
+ *
+ * Korean language file.
+ *
+ * Author: Limerainne (limerainne@gmail.com)
+ */
+
+ theUILang.themeStandard	= "표준";
+ theUILang.theme		= "테마";
+
+thePlugins.get("theme").langLoaded();
