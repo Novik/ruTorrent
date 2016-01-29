986fd404823fdb99b91b2901d8576ed3db530396
Limerainne <limerainne@gmail.com>
2016-01-30 02:12:36 +0900

    partial translations for multiple plugins

---
+++ plugins/cookies/lang/ko.js
@@ -0,0 +1,12 @@
+﻿/*
+ * PLUGIN COOKIES
+ *
+ * Korean language file.
+ *
+ * Author: Limerainne (limerainne@gmail.com)
+ */
+
+ theUILang.cookiesDesc = "쿠키 (형식: 호스트|쿠키1;쿠키2...)";
+ theUILang.cookiesName = "쿠키";
+
+thePlugins.get("cookies").langLoaded();
---
+++ plugins/extratio/lang/ko.js
@@ -0,0 +1,25 @@
+﻿/*
+ * PLUGIN EXTRATIO
+ *
+ * Korean language file.
+ *
+ * Author: Limerainne (limerainne@gmail.com)
+ */
+
+ theUILang.ratioRulesManager	= "규칙 관리자";
+ theUILang.mnu_ratiorule	= "비율 규칙";
+ theUILang.ratAddRule		= "추가";
+ theUILang.ratDelRule		= "삭제";
+ theUILang.ratUpRule		= "위";
+ theUILang.ratDownRule		= "아래";
+ theUILang.ratioIfLegend	= "If";
+ theUILang.ratLabelContain	= "Torrent Label contains";
+ theUILang.ratTrackerContain	= "One of torrent's tracker URLs contains";
+ theUILang.ratTrackerPublic	= "All torrent's trackers are public";
+ theUILang.ratTrackerPrivate	= "One of torrent's trackers is private";
+ theUILang.ratioThenLegend	= "Then";
+ theUILang.setRatioTo		= "Set ratio to";
+ theUILang.setChannelTo 	= "Set throttle to";
+ theUILang.ratioNewRule 	= "New rule";
+
+thePlugins.get("extratio").langLoaded();
