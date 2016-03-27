f14757abe27c96abb9ceeafe9b7114312ee72946
rtorrentuser <serge.6401@gmail.com>
2016-03-27 15:10:12 +0300

    Show correct 'Result:' when tracker is unavailable

--- plugins/rutracker_check/trackers/rutracker.php
+++ plugins/rutracker_check/trackers/rutracker.php
@@ -8,13 +8,12 @@ class RuTrackerCheckImpl
             $topic_id = $matches["id"];
             $req_url = "http://api.rutracker.org/v1/get_tor_hash?by=topic_id&val=" . $topic_id;
             $client = ruTrackerChecker::makeClient($req_url);
-            if ($client->status == 200) {
-                $ret = json_decode($client->results, true);
-                if (array_key_exists("result", $ret)) $ret = $ret["result"];
-                if ($ret && array_key_exists($topic_id, $ret) && (strtoupper($ret[$topic_id]) == $hash)) {
-                    return ruTrackerChecker::STE_UPTODATE;
+            if ($client->status != 200) return ruTrackerChecker::STE_CANT_REACH_TRACKER;
+            $ret = json_decode($client->results, true);
+            if (array_key_exists("result", $ret)) $ret = $ret["result"];
+            if ($ret && array_key_exists($topic_id, $ret) && (strtoupper($ret[$topic_id]) == $hash)) {
+                return ruTrackerChecker::STE_UPTODATE;
                 }
-            }
             $client->setcookies();
             $client->fetchComplex("http://dl.rutracker.org/forum/dl.php?t=" . $topic_id);
             if ($client->status != 200) return (($client->status < 0) ? ruTrackerChecker::STE_CANT_REACH_TRACKER : ruTrackerChecker::STE_DELETED);
