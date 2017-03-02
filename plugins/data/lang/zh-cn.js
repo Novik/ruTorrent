7cbeadbecfc1a5157a5cbff2e9782075dbd9a615
mewsf <w1sh2f3@gmail.com>
2017-03-02 07:16:36 -0600

    Improve

--- plugins/data/lang/zh-cn.js
+++ plugins/data/lang/zh-cn.js
@@ -7,6 +7,6 @@
  */
 
  theUILang.getData		= "获取数据";
- theUILang.cantAccessData	= "Webserver user can't access the data of this torrent.";
+ theUILang.cantAccessData	= "网页服务器执行用户不能访问这个 torrent 的数据.";
 
 thePlugins.get("data").langLoaded();
