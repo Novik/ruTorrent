d19adadc93ac1e5f6800047463e36da5111796ba
mewsf <w1sh2f3@gmail.com>
2017-03-02 07:51:36 -0600

    Update zh-cn.js

--- plugins/extratio/lang/zh-cn.js
+++ plugins/extratio/lang/zh-cn.js
@@ -7,7 +7,7 @@
  */
 
  theUILang.ratioRulesManager	= "规则管理器";
- theUILang.mnu_ratiorule	= "比率规则";
+ theUILang.mnu_ratiorule	= "分享率规则";
  theUILang.ratAddRule		= "添加";
  theUILang.ratDelRule		= "删除";
  theUILang.ratUpRule		= "上升";
@@ -18,7 +18,7 @@
  theUILang.ratTrackerPublic	= "所有 torrent 的  trackers 是公开的";
  theUILang.ratTrackerPrivate	= "Torrent的某个  trackers 是私有的";
  theUILang.ratioThenLegend	= "然后";
- theUILang.setRatioTo		= "把比率设为";
+ theUILang.setRatioTo		= "把分享率设为";
  theUILang.setChannelTo 	= "把限制设为";
  theUILang.ratioNewRule 	= "新规则";
 
