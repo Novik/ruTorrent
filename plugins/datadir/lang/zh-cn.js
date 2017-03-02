1c521939513cc622d79b6130180f5bfa9b2d0f1a
mewsf <w1sh2f3@gmail.com>
2017-03-02 07:17:48 -0600

    Improve

--- plugins/datadir/lang/zh-cn.js
+++ plugins/datadir/lang/zh-cn.js
@@ -9,7 +9,7 @@
  theUILang.DataDir		= "保存到";
  theUILang.DataDirMove		= "移动数据文件";
  theUILang.datadirDlgCaption	= "Torrent 数据目录";
- theUILang.datadirDirNotFound	= "DataDir plugin: Invalid directory";
- theUILang.datadirSetDirFail	= "DataDir plugin: Operation fail";
+ theUILang.datadirDirNotFound	= "DataDir 插件: 无效的目录";
+ theUILang.datadirSetDirFail	= "DataDir 插件: 操作失败";
 
 thePlugins.get("datadir").langLoaded();
