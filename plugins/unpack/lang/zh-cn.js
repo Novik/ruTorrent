01bd0b75fa7b96dabc8a04ab5c912645123492b0
mewsf <w1sh2f3@gmail.com>
2017-03-03 05:47:24 -0600

    Improve

--- plugins/unpack/lang/zh-cn.js
+++ plugins/unpack/lang/zh-cn.js
@@ -8,9 +8,9 @@
 
  theUILang.unpack		= "解压缩";
  theUILang.unpackPath		= "解压到 (这里留空为 Torrents 目录)";
- theUILang.unzipNotFound	= "Unpack plugin: rTorrent user can't access 'unzip' program.";
- theUILang.unrarNotFound	= "Unpack plugin: rTorrent user can't access 'unrar' program.";
- theUILang.unpackEnabled	= "Enable autounpacking if torrent's label matches filter";
+ theUILang.unzipNotFound	= "Unpack 插件: rTorrent 执行用户不能访问 'unzip' 程序.";
+ theUILang.unrarNotFound	= "Unpack 插件: rTorrent 执行用户不能访问 'unrar' 程序.";
+ theUILang.unpackEnabled	= "启用自动解压缩如果 torrent 的标签符合过滤器";
  theUILang.unpackTorrents	= "当解压缩 Torrent 数据时添加路径:";
  theUILang.unpackAddLabel	= "Torrent 的标签";
  theUILang.unpackAddName	= "Torrent 的名称";
