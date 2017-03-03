d85bfa164b781b9743d11a19f7195c75e255de77
mewsf <w1sh2f3@gmail.com>
2017-03-03 05:27:12 -0600

    Update zh-cn.js

--- plugins/rssurlrewrite/lang/zh-cn.js
+++ plugins/rssurlrewrite/lang/zh-cn.js
@@ -13,17 +13,17 @@
  theUILang.rssDelRule		= "删除";
  theUILang.rssCheckRule 	= "?";
  theUILang.rssRulesLegend	= "规则设置";
- theUILang.rssSrcHref		= "If URL of torrent download matches pattern";
- theUILang.rssSrcGuid		= "If URL of torrent description matches pattern";
- theUILang.rssDstHref		= "then replace URL of torrent download with";
- theUILang.rssDstGuid		= "then replace URL of torrent description with";
+ theUILang.rssSrcHref		= "如果 torrent 的下载 URL 符合表达式";
+ theUILang.rssSrcGuid		= "如果 torrent 的描述 URL 符合表达式";
+ theUILang.rssDstHref		= "那么将 torrent 的下载 URL 替换为";
+ theUILang.rssDstGuid		= "那么将 torrent 的描述 URL 替换为";
  theUILang.rssRulesDebug	= "规则调试";
  theUILang.rssTestString	= "测试";
  theUILang.rssTestResult	= "结果";
  theUILang.rssURLInfo		= "URL 信息";
  theUILang.rssURLGUID		= "描述 URL";
- theUILang.rssURLHref		= "载入 URL";
- theUILang.rssPatternError	= "模式串中有错误.";
+ theUILang.rssURLHref		= "下载 URL";
+ theUILang.rssPatternError	= "表达式中有错误.";
  theUILang.rssStatus		= "RSS";
 
 thePlugins.get("rssurlrewrite").langLoaded();
