f017f8a7e993c1eee702b6a924bd2cf278ed45b9
Micdu70 <Micdu70@users.noreply.github.com>
2017-07-15 17:00:52 +0200

    Update fr.js

--- plugins/uploadeta/lang/fr.js
+++ plugins/uploadeta/lang/fr.js
@@ -6,11 +6,11 @@
  * Author: 
  */
 
- theUILang.uploadeta		= "Upload ETA";
- theUILang.uploadtarget		= "Upload Target";
- theUILang.ULtarget		= "UL Target";
- theUILang.ULremaining		= "UL Remaining";
- theUILang.ULETA		= "UL ETA";
- theUILang.ULdescription	= "This plugin only will show the amount of data and time that is left to an upload ratio target. It will not automatically remove a torrent for you. Look at 'ratio.min.set' in your rTorrent configuration file to remove a torrent when a upload target has been reached.";
+ theUILang.uploadeta		= "Estimation de partage";
+ theUILang.uploadtarget		= "Ratio de partage";
+ theUILang.ULtarget		= "Envoyé - Cible";
+ theUILang.ULremaining		= "Envoyé - Restant";
+ theUILang.ULETA		= "Envoyé - Temps estimé";
+ theUILang.ULdescription	= "Ce plugin montre seulement la quantité de données et le temps estimé pour atteindre un ratio de partage voulu. Il ne supprimera pas automatiquement un torrent pour vous. Regardez 'ratio.min.set' dans votre fichier de configuration rTorrent pour supprimer un torrent quand un ratio de partage est atteint.";
 
 thePlugins.get("uploadeta").langLoaded();
