975f1f13d52ce302d7688ff6d8b190596cbd1e50
Novik <novik65@gmail.com>
2017-09-26 12:33:46 +0300

    issue #1560

--- plugins/scheduler/conf.php
+++ plugins/scheduler/conf.php
@@ -1,8 +1,8 @@
 <?php
 	// configuration parameters
 
-	@define('SCH_DEF_UL', 327625*1024);	// default UL (KB/s). 
-	@define('SCH_DEF_DL', 327625*1024);	// default DL (KB/s)
-	// Can't be greater then 327625*1024 due to limitation in libtorrent ResourceManager::set_max_upload_unchoked function.
+	@define('SCH_DEF_UL', 327625);	// default UL (KB/s). 
+	@define('SCH_DEF_DL', 327625);	// default DL (KB/s)
+	// Can't be greater then 327625 due to limitation in libtorrent ResourceManager::set_max_upload_unchoked function.
 
 	$updateInterval = 60;	// in minutes, 1-6,10,12,15,20,30 or 60
