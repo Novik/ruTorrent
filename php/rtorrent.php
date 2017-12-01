40fce337edff44afa8ce0f97248ed0530ab6d44f
Novik <novik65@gmail.com>
2017-12-01 16:53:21 +0300

    add possibility to apply 'not add path' option without setting directory on torrent addition

--- php/rtorrent.php
+++ php/rtorrent.php
@@ -43,7 +43,8 @@ class rTorrent
 			}
 			if(!is_null($filename) && (rTorrentSettings::get()->iVersion>=0x805))
 				$cmd->addParameter(getCmd("d.set_custom")."=x-filename,".rawurlencode(getFileName($filename)));
-			$req = new rXMLRPCRequest();				
+			$req = new rXMLRPCRequest();
+			$directory = self::parseDirectory($directory, $isAddPath);
 			if($directory && (strlen($directory)>0))
 			{
 				if(!rTorrentSettings::get()->correctDirectory($directory))
@@ -96,6 +97,7 @@ class rTorrent
 				$req = new rXMLRPCRequest();
 				$cmd = new rXMLRPCCommand( $isStart ? 'load_start' : 'load' );
 				$cmd->addParameter($magnet);
+				$directory = self::parseDirectory($directory, $isAddPath);
 				if($directory && (strlen($directory)>0))
 				{
 					if(!rTorrentSettings::get()->correctDirectory($directory))
@@ -159,10 +161,8 @@ class rTorrent
 		$base = trim($base);
 	        if($base=='')
 	        {
-	        	$req = new rXMLRPCRequest( new rXMLRPCCommand('get_directory') );
-	        	if($req->success())
-	        		$base=$req->val[0];
-	        }
+	        	$base = rTorrentSettings::get()->directory;
+		}
 	        if($psize && rTorrentSettings::get()->correctDirectory($base))
 	        {
 		        $base = addslash($base);
@@ -197,5 +197,14 @@ class rTorrent
 			return($torrent);
 		}
 		return(false);
+	}
+
+	static protected function parseDirectory($directory, $isAddPath)
+	{
+		if(!$isAddPath && (!$directory || (strlen($directory)==0)))
+		{
+			$directory = rTorrentSettings::get()->directory;
+		}
+		return($directory);
 	}
 }
