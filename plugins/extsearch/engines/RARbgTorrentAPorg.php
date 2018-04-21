0bbea7ac0b8653fdbb3b3303c9a5048798514d00
emuhendis <bektasaykut@gmail.com>
2018-04-21 22:41:41 +0300

    Update RARbgTorrentAPorg.php

--- plugins/extsearch/engines/RARbgTorrentAPorg.php
+++ plugins/extsearch/engines/RARbgTorrentAPorg.php
@@ -7,7 +7,7 @@ class RARbgTorrentAPIEngine extends commonEngine
 		"all"=>"0", "XXX (18+)"=>"4","Movies/XVID"=>"14","Movies/XVID/720"=>"48","Movies/x264"=>"17","Movies/x264/1080"=>"44","Movies/x264/720"=>"45","Movies/x264/3D"=>"47","Movies/x264/4k"=>"50","Movies/x265/4k"=>"51","Movs/x265/4k/HDR"=>"52","Movies/Full BD"=>"42","Movies/BD Remux"=>"46","TV Episodes"=>"18","TV HD Episodes"=>"41","TV UHD Episodes"=>"49","Music/MP3"=>"23","Music/FLAC"=>"25","Games/PC ISO"=>"27","Games/PC RIP"=>"28","Games/PS3"=>"40","Games/XBOX-360"=>"32","Software/PC ISO"=>"33");
     
     public function get_token(){
-        $tokenurl='https://torrentapi.org/pubapi_v2.php?get_token=get_token';
+        $tokenurl='https://torrentapi.org/pubapi_v2.php?app_id=ruTorrent_extsearch&get_token=get_token';
         $json = file_get_contents($tokenurl);
         $obj = json_decode($json);
         return $obj->token;
@@ -17,7 +17,7 @@ class RARbgTorrentAPIEngine extends commonEngine
 	    $token = self::get_token();
 
         $added = 0;
-        $url="https://torrentapi.org/pubapi_v2.php?token=$token&format=json_extended&mode=search&search_string=$what&category=$cat";
+        $url="https://torrentapi.org/pubapi_v2.php?app_id=ruTorrent_extsearch&token=$token&format=json_extended&mode=search&search_string=$what&category=$cat";
 
         $json = file_get_contents($url);
         $obj = json_decode($json);
