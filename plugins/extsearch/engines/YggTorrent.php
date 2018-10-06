60e03ce64698f4587818c7a3ebc51613de08c988
mickkael <19755421+mickkael@users.noreply.github.com>
2018-10-06 17:23:44 +0800

    Update YggTorrent.php

--- plugins/extsearch/engines/YggTorrent.php
+++ plugins/extsearch/engines/YggTorrent.php
@@ -2,7 +2,7 @@
 
 class YggTorrentEngine extends commonEngine
 {
-    const URL = 'https://www.yggtorrent.to';
+    const URL = 'https://yggtorrent.to';
     const MAX_PAGE = 10;
     const PAGE_SIZE = 50;
 
