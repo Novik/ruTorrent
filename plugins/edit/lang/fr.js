b19349b1131b3b0d444acc473cbb16cc11d7575f
Micdu70 <Micdu70@users.noreply.github.com>
2018-12-21 20:41:28 +0100

    Update fr.js

--- plugins/edit/lang/fr.js
+++ plugins/edit/lang/fr.js
@@ -6,8 +6,8 @@
  * Author: Nicobubulle (nicobubulle@gmail.com)
  */
 
- theUILang.EditTrackers 		= "Éditer le torrent...";
- theUILang.EditTorrentProperties	= "Propriétés du torrent";
+ theUILang.EditTrackers 		= "Éditer le(s) torrent(s)...";
+ theUILang.EditTorrentProperties	= "Propriétés du ou des torrents";
  theUILang.errorAddTorrent		= "Erreur lors de l'ajout du .torrent";
  theUILang.errorWriteTorrent		= "Erreur lors de l'écriture du .torrent";
  theUILang.errorReadTorrent		= "Erreur lors de la lecture du .torrent";
