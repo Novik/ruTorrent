600ef44228e09f8bf39ed8908c6a69fe8da9c83a
mickkael <19755421+mickkael@users.noreply.github.com>
2019-04-06 19:13:45 +0800

    Create en.js

---
+++ plugins/cloudflare/lang/en.js
@@ -0,0 +1,12 @@
+/* PLUGIN CLOUDFLARE
+ *
+ * English language file.
+ *
+ * Author: 
+ */
+
+ theUILang.cloudflare           = "Cloudflare Scrape";
+ theUILang.pythonNotFound               = "Python Not Found";
+ theUILang.cannotLoadCfscrape           = "CfScrape Module cant be loaded in Python";
+
+thePlugins.get("cloudflare").langLoaded();
