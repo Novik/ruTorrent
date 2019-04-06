bd357c076ce9074718c4af60f316aec01bfccc64
mickkael <19755421+mickkael@users.noreply.github.com>
2019-04-06 18:59:29 +0800

    Update init.php

--- plugins/cloudflare/init.php
+++ plugins/cloudflare/init.php
@@ -7,7 +7,7 @@ if ($do_diagnostic) {
   findRemoteEXE('python',"thePlugins.get('cloudflare').showError('theUILang.pythonNotFound');",$remoteRequests);
   $error_code=shell_exec("$pathToPython -c \"import cfscrape\";echo \$?");
   if ($error_code != 0) {
-    $jResult .= "plugin.disable(); noty('cloudflare: '+theUILang.pluginCantStart,'Loading python module cfscrape failed');";
+    $jResult .= "plugin.disable(); noty('cloudflare: '+theUILang.cannotLoadCfscrape,'error');";
   } else {
     $theSettings->registerPlugin($plugin["name"],$pInfo["perms"]);
   }
