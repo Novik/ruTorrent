dcb55ace996ffe880d0b1c323d7e8514c14da295
mickkael <19755421+mickkael@users.noreply.github.com>
2019-04-06 15:47:58 +0800

    Create init.php

---
+++ plugins/cloudflare/init.php
@@ -0,0 +1,11 @@
+<?php
+eval(getPluginConf($plugin["name"]));
+
+findRemoteEXE('python',"thePlugins.get('cloudflare').showError('theUILang.pythonNotFound');",$remoteRequests);
+exec('python -c "import cfscrape"',"",$error_code);
+if () {
+  $jResult .= "plugin.disable(); noty('cloudflare: '+theUILang.pluginCantStart,'error');";
+} else {
+  $theSettings->registerPlugin($plugin["name"],$pInfo["perms"]);
+}
+  
