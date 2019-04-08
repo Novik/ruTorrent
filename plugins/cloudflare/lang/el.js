7e0d893f7323042f9784f65e8b2d43159dd2ac17
ChriZathens <c_kan1@hotmail.com>
2019-04-08 19:04:34 +0300

    Update el.js

--- plugins/cloudflare/lang/el.js
+++ plugins/cloudflare/lang/el.js
@@ -7,6 +7,6 @@
 
  theUILang.cloudflare		= "Cloudflare Scrape";
  theUILang.pythonNotFound	= "Δε βρέθηκε Python";
- theUILang.cannotLoadCfscrape	= "Το αρθρώματος CfScrape δεν μπορεί να φορτωθεί στην Python";
+ theUILang.cannotLoadCfscrape	= "Το πρόσθετο CfScrape δεν μπορεί να φορτωθεί στην Python";
 
 thePlugins.get("cloudflare").langLoaded();
