689a1b79d21509d3e20383b89c0b7fc6cc491763
Novik <novik65@gmail.com>
2019-04-09 12:13:34 +0300

    change design of setting externals in the conf files

--- plugins/_cloudflare/conf.php
+++ plugins/_cloudflare/conf.php
@@ -1,3 +1,6 @@
 <?php
 
-$pathToExternals['python'] = '';
+if(empty($pathToExternals['python']))	// May be path already defined?
+{
+	$pathToExternals['python'] = '';// Something like /usr/bin/python. If empty, will be found in PATH.
+}
--- plugins/unpack/conf.php
+++ plugins/unpack/conf.php
@@ -3,8 +3,14 @@
 @define('USE_UNZIP', true, true);
 @define('USE_UNRAR', true, true);
 
-$pathToExternals['unzip'] = '';		// Something like /usr/bin/unzip. If empty, will be found in PATH.
-$pathToExternals['unrar'] = '';		// Something like /usr/bin/unrar. If empty, will be found in PATH.
+if(empty($pathToExternals['unzip']))	// May be path already defined?
+{
+	$pathToExternals['unzip'] = '';	// Something like /usr/bin/unzip. If empty, will be found in PATH.
+}
+if(empty($pathToExternals['unrar']))	// May be path already defined?
+{
+	$pathToExternals['unrar'] = '';	// Something like /usr/bin/unrar. If empty, will be found in PATH.
+}
 
 $cleanupAutoTasks = false;		// Remove autounpack tasks parameters after finish, otherwise will be shown in the 'Tasks' tab
 $deleteAutoArchives = false;	// Delete archive files after successful auto unpack. Will not remove archives unless AutoMove is enabled and the operation type is not Move.
