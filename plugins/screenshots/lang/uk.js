a628509f8b706e17405620a2cb1cf2e1c43fba7a
koblack <koblack@users.noreply.github.com>
2019-04-18 19:00:10 +0200

    screenshots plugin: cleanup translations

--- plugins/screenshots/lang/uk.js
+++ plugins/screenshots/lang/uk.js
@@ -7,13 +7,13 @@
  */
 
  theUILang.exFFMPEG		= "Знімки екрана";
- theUILang.exFrameWidth 	= "Ширина кадра";
+ theUILang.exFrameWidth		= "Ширина кадра";
  theUILang.exFramesCount	= "Кількість кадрів";
  theUILang.exStartOffset	= "Початкове зміщення";
  theUILang.exBetween		= "Час між кадрами";
  theUILang.exSave		= "Зберегти";
  theUILang.exSaveAll		= "Зберегти все";
- theUILang.exScreenshot 	= "Знімок екрана";
+ theUILang.exScreenshot		= "Знімок екрана";
  theUILang.exPlayInterval	= "Інтервал показу";
  theUILang.exImageFormat	= "Формат";
 
--- plugins/screenshots/lang/vi.js
+++ plugins/screenshots/lang/vi.js
@@ -7,13 +7,13 @@
  */
 
  theUILang.exFFMPEG		= "Chụp màn hình";
- theUILang.exFrameWidth 	= "Độ rộng khung hình";
+ theUILang.exFrameWidth		= "Độ rộng khung hình";
  theUILang.exFramesCount	= "Số khung hình";
  theUILang.exStartOffset	= "Khung hình bắt đầu từ";
  theUILang.exBetween		= "Thời gian giữa các khung hình";
  theUILang.exSave		= "Lưu lại";
  theUILang.exSaveAll		= "Lưu tất cả";
- theUILang.exScreenshot 	= "Chụp màn hình";
+ theUILang.exScreenshot		= "Chụp màn hình";
  theUILang.exPlayInterval	= "Thời gian trình diễn";
  theUILang.exImageFormat	= "Định dạng ảnh";
 
