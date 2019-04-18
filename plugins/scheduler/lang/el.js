a66d6596353065fbc9020e25bb634f4c8eb2b22e
koblack <koblack@users.noreply.github.com>
2019-04-18 19:00:10 +0200

    scheduler plugin: cleanup translations

--- plugins/scheduler/lang/el.js
+++ plugins/scheduler/lang/el.js
@@ -9,9 +9,9 @@
  theUILang.scheduler		= "Χρονοπρογραμματισμός";
  theUILang.schedulerOn		= "Ενεργοποίηση χρονοπρογραμματισμού";
  theUILang.schedulerGraph	= "Πίνακας χρονοπρογραμματισμού";
- theUILang.schShortWeek 	= [ "Δε", "Τρ", "Τε", "Πε", "Πα", "Σα", "Κυ" ];
+ theUILang.schShortWeek		= [ "Δε", "Τρ", "Τε", "Πε", "Πα", "Σα", "Κυ" ];
  theUILang.schFullWeek		= [ "Δευτέρα", "Τρίτη", "Τετάρτη", "Πέμπτη", "Παρασκευή", "Σάββατο", "Κυριακή" ];
- theUILang.schUnlimited 	= "Απεριόριστο";
+ theUILang.schUnlimited		= "Απεριόριστο";
  theUILang.schLimited		= "Περιορισμένο";
  theUILang.schTurnOff		= "Απενεργοποίηση";
  theUILang.schSeedingOnly	= "Αποστολή μόνο";
@@ -19,8 +19,8 @@
  theUILang.schLimitedDesc	= "Περιορισμένο - Χρησιμοποιεί όριο εύρους ζώνης ορισμένο από τον χρονοπρογραμματισμό";
  theUILang.schTurnOffDesc	= "Απενεργοποίηση - Σταματά όλα τα torrents";
  theUILang.schSeedingOnlyDesc	= "Αποστολή μόνο - Να γίνεται μόνο αποστολή δεδομένων";
- theUILang.schLimitedUL 	= "Περιορισμένο UL";
- theUILang.schLimitedDL 	= "Περιορισμένο DL";
+ theUILang.schLimitedUL		= "Περιορισμένο UL";
+ theUILang.schLimitedDL		= "Περιορισμένο DL";
  theUILang.shcIgnore		= "Αγνόηση του χρονοπρογραμματισμού";
 
 thePlugins.get("scheduler").langLoaded();
--- plugins/scheduler/lang/sr.js
+++ plugins/scheduler/lang/sr.js
@@ -9,9 +9,9 @@
  theUILang.scheduler		= "Scheduler";
  theUILang.schedulerOn		= "Омогући Scheduler";
  theUILang.schedulerGraph	= "Scheduler-ова табела";
- theUILang.schShortWeek 	= [ "пон", "уто", "сре", "чет", "пет", "суб", "нед" ];
+ theUILang.schShortWeek		= [ "пон", "уто", "сре", "чет", "пет", "суб", "нед" ];
  theUILang.schFullWeek		= [ "понедељак", "уторак", "среда", "четвртак", "петак", "субота", "недеља" ];
- theUILang.schUnlimited 	= "Неограничено";
+ theUILang.schUnlimited		= "Неограничено";
  theUILang.schLimited		= "Ограничено";
  theUILang.schTurnOff		= "Искључено";
  theUILang.schSeedingOnly	= "Само сејање";
@@ -19,8 +19,8 @@
  theUILang.schLimitedDesc	= "Ограничено - Uses scheduler-specific bandwidth limit";
  theUILang.schTurnOffDesc	= "Искључено - Зауставља све торенте";
  theUILang.schSeedingOnlyDesc	= "Само сејање - Само узтовар података";
- theUILang.schLimitedUL 	= "Ограничен узтовар";
- theUILang.schLimitedDL 	= "Ограничен низтовар";
+ theUILang.schLimitedUL		= "Ограничен узтовар";
+ theUILang.schLimitedDL		= "Ограничен низтовар";
  theUILang.shcIgnore		= "Ignore scheduler";
 
 thePlugins.get("scheduler").langLoaded();
