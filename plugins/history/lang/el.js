7c65536c275a1dc136c78731610bcec49792770e
koblack <koblack@users.noreply.github.com>
2019-04-18 19:00:10 +0200

    history plugin: cleanup translations

--- plugins/history/lang/el.js
+++ plugins/history/lang/el.js
@@ -6,33 +6,33 @@
  * Author: Chris Kanatas (ckanatas@gmail.com)
  */
 
- theUILang.historyAddition	= "Προσθήκη";
- theUILang.historyDeletion	= "Διαγραφή";
- theUILang.historyFinish	= "Ολοκλήρωση";
- theUILang.historyLog		= "Καταγραφή";
- theUILang.history		= "Ιστορικό";
- theUILang.historyLimit 	= "Μέγιστος αριθμός καταχωρήσεων";
- theUILang.seedingTime		= "Ολοκληρώθηκε";
- theUILang.addTime		= "Προστέθηκε";
- theUILang.hstDeletePrompt	= "Θέλετε πραγματικά να διαγράψετε τις επιλεγμένες καταχωρήσεις;";
- theUILang.hstDelete		= "Διαγραφή καταχωρήσεων";
- theUILang.Added		= "Προστέθηκε";
- theUILang.Finished		= "Ολοκληρώθηκε";
- theUILang.Deleted		= "Διαγράφηκε";
- theUILang.Time 		= "Ημ/νία - Ώρα";
- theUILang.Tracker		= "Tracker";
- theUILang.enableNotifications	= "Ενεργοποίηση ειδοποιήσεων επιφάνειας εργασίας";
- theUILang.historyNotification	= "Ειδοποιήσεις επιφάνειας εργασίας";
- theUILang.notifAutoClose	= "Αυτόματο κλείσιμο ειδοποιήσεων μετά από";
- theUILang.notifTip		= {
- 				  false: "Το πρόγραμμα περιήγησης που χρησιμοποιείτε, δεν υποστηρίζει ειδοποιήσεις επιφάνειας εργασίας. Δοκιμάστε να χρησιμοποιήσετε άλλο πρόγραμμα περιήγησης, για παράδειγμα τον Google Chrome.",
- 				  "granted": "",
- 				  "default": "Το πρόγραμμα περιήγησης που χρησιμοποιείτε, υποστηρίζει ειδοποιήσεις επιφάνειας εργασίας. Πατήστε το παρακάτω κουμπί για να τις ενεργοποιήσετε.",
- 				  "denied": "Το πρόγραμμα περιήγησης που χρησιμοποιείτε υποστηρίζει ειδοποιήσεις επιφάνειας εργασίας, αλλά τις έχετε απενεργοποιήσει για αυτή την ιστοσελίδα. Κλείστε αυτό το παράθυρο διαλόγου και χρησιμοποιήστε τις ρυθμίσεις του περιηγητή σας για να ενεργοποιήσετε τις ειδοποιήσεις επιφάνειας εργασίας.",
- 				  };
- theUILang.pushbulletNotification = "Ειδοποιήσεις PushBullet";
- theUILang.pushbulletKey	= "Πιστοποίηση πρόσβασης PushBullet";
- theUILang.turnNotifyOn 	= "Ενεργοποίηση ειδοποιήσεων";
- theUILang.turnNotifyOff	= "Απενεργοποίηση ειδοποιήσεων";
+ theUILang.historyAddition		= "Προσθήκη";
+ theUILang.historyDeletion		= "Διαγραφή";
+ theUILang.historyFinish		= "Ολοκλήρωση";
+ theUILang.historyLog			= "Καταγραφή";
+ theUILang.history			= "Ιστορικό";
+ theUILang.historyLimit			= "Μέγιστος αριθμός καταχωρήσεων";
+ theUILang.seedingTime			= "Ολοκληρώθηκε";
+ theUILang.addTime			= "Προστέθηκε";
+ theUILang.hstDeletePrompt		= "Θέλετε πραγματικά να διαγράψετε τις επιλεγμένες καταχωρήσεις;";
+ theUILang.hstDelete			= "Διαγραφή καταχωρήσεων";
+ theUILang.Added			= "Προστέθηκε";
+ theUILang.Finished			= "Ολοκληρώθηκε";
+ theUILang.Deleted			= "Διαγράφηκε";
+ theUILang.Time				= "Ημ/νία - Ώρα";
+ theUILang.Tracker			= "Tracker";
+ theUILang.enableNotifications		= "Ενεργοποίηση ειδοποιήσεων επιφάνειας εργασίας";
+ theUILang.historyNotification		= "Ειδοποιήσεις επιφάνειας εργασίας";
+ theUILang.notifAutoClose		= "Αυτόματο κλείσιμο ειδοποιήσεων μετά από";
+ theUILang.notifTip			= {
+ 					  false: "Το πρόγραμμα περιήγησης που χρησιμοποιείτε, δεν υποστηρίζει ειδοποιήσεις επιφάνειας εργασίας. Δοκιμάστε να χρησιμοποιήσετε άλλο πρόγραμμα περιήγησης, για παράδειγμα τον Google Chrome.",
+ 					  "granted": "",
+ 					  "default": "Το πρόγραμμα περιήγησης που χρησιμοποιείτε, υποστηρίζει ειδοποιήσεις επιφάνειας εργασίας. Πατήστε το παρακάτω κουμπί για να τις ενεργοποιήσετε.",
+ 					  "denied": "Το πρόγραμμα περιήγησης που χρησιμοποιείτε υποστηρίζει ειδοποιήσεις επιφάνειας εργασίας, αλλά τις έχετε απενεργοποιήσει για αυτή την ιστοσελίδα. Κλείστε αυτό το παράθυρο διαλόγου και χρησιμοποιήστε τις ρυθμίσεις του περιηγητή σας για να ενεργοποιήσετε τις ειδοποιήσεις επιφάνειας εργασίας.",
+ 					  };
+ theUILang.pushbulletNotification	= "Ειδοποιήσεις PushBullet";
+ theUILang.pushbulletKey		= "Πιστοποίηση πρόσβασης PushBullet";
+ theUILang.turnNotifyOn			= "Ενεργοποίηση ειδοποιήσεων";
+ theUILang.turnNotifyOff		= "Απενεργοποίηση ειδοποιήσεων";
 
 thePlugins.get("history").langLoaded();
--- plugins/history/lang/no.js
+++ plugins/history/lang/no.js
@@ -6,33 +6,33 @@
  * Author: nirosa (nirosax@gmail.com)
  */
 
- theUILang.historyAddition	= "Tilsetting";
- theUILang.historyDeletion	= "Sletting";
- theUILang.historyFinish	= "Fullføring";
- theUILang.historyLog		= "Logg";
- theUILang.history		= "Historie";
- theUILang.historyLimit 	= "Maksimalt antall poster";
- theUILang.seedingTime		= "Fullført";
- theUILang.addTime		= "Lagt til";
- theUILang.hstDeletePrompt	= "Vil du virkelig slette den valgte post(en)?";
- theUILang.hstDelete		= "Slett post(er)";
- theUILang.Added		= "Lagt til";
- theUILang.Finished		= "Fullført";
- theUILang.Deleted		= "Slettet";
- theUILang.Time 		= "Tid";
- theUILang.Tracker		= "Tracker";
- theUILang.enableNotifications	= "Aktiver skrivebordsvarslinger";
- theUILang.historyNotification	= "Skrivebordsvarslinger";
- theUILang.notifAutoClose	= "Lukk notifikasjoner automatisk etter";
- theUILang.notifTip		= {
- 				  false: "Din nettleser støtter ikke skrivebordsvarslinger. Prøv å bruke en annen nettleser, for eksempel Google Chrome.",
- 				  "granted": "",
- 				  "default": "Din nettleser støtter skrivebordsvarslinger. Trykk på knappen nedenfor for å aktivere de.",
- 				  "denied": "Din nettleser støtter skrivebordsvarslinger, men du har deaktivert de for dette nettstedet. Lukk denne dialogen og aktiver skrivebordsvarslinger i dine nettleserinstillinger.",
- 				  };
- theUILang.pushbulletNotification = "PushBullet-notifikasjoner";
- theUILang.pushbulletKey	= "PushBullet-tilgangstoken";
- theUILang.turnNotifyOn 	= "Skru notifikasjoner på";
- theUILang.turnNotifyOff	= "Skru notifikasjoner av";
+ theUILang.historyAddition		= "Tilsetting";
+ theUILang.historyDeletion		= "Sletting";
+ theUILang.historyFinish		= "Fullføring";
+ theUILang.historyLog			= "Logg";
+ theUILang.history			= "Historie";
+ theUILang.historyLimit			= "Maksimalt antall poster";
+ theUILang.seedingTime			= "Fullført";
+ theUILang.addTime			= "Lagt til";
+ theUILang.hstDeletePrompt		= "Vil du virkelig slette den valgte post(en)?";
+ theUILang.hstDelete			= "Slett post(er)";
+ theUILang.Added			= "Lagt til";
+ theUILang.Finished			= "Fullført";
+ theUILang.Deleted			= "Slettet";
+ theUILang.Time				= "Tid";
+ theUILang.Tracker			= "Tracker";
+ theUILang.enableNotifications		= "Aktiver skrivebordsvarslinger";
+ theUILang.historyNotification		= "Skrivebordsvarslinger";
+ theUILang.notifAutoClose		= "Lukk notifikasjoner automatisk etter";
+ theUILang.notifTip			= {
+ 					  false: "Din nettleser støtter ikke skrivebordsvarslinger. Prøv å bruke en annen nettleser, for eksempel Google Chrome.",
+ 					  "granted": "",
+ 					  "default": "Din nettleser støtter skrivebordsvarslinger. Trykk på knappen nedenfor for å aktivere de.",
+ 					  "denied": "Din nettleser støtter skrivebordsvarslinger, men du har deaktivert de for dette nettstedet. Lukk denne dialogen og aktiver skrivebordsvarslinger i dine nettleserinstillinger.",
+ 					  };
+ theUILang.pushbulletNotification	= "PushBullet-notifikasjoner";
+ theUILang.pushbulletKey		= "PushBullet-tilgangstoken";
+ theUILang.turnNotifyOn			= "Skru notifikasjoner på";
+ theUILang.turnNotifyOff		= "Skru notifikasjoner av";
 
 thePlugins.get("history").langLoaded();
