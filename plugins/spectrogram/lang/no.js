e6db8a31ab6f4de7117f37abd4083e11d57b2d18
koblack <koblack@users.noreply.github.com>
2019-04-18 19:00:10 +0200

    spectrogram plugin: add missing translation files﻿

---
+++ plugins/spectrogram/lang/no.js
@@ -0,0 +1,12 @@
+﻿/*
+ * PLUGIN SPECTROGRAM
+ *
+ * Norwegian language file.
+ *
+ * Author: 
+ */
+
+ theUILang.exsox		= "Spectrogram";
+ theUILang.exSave		= "Save";
+
+thePlugins.get("spectrogram").langLoaded();
