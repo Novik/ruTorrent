1c5f822e0861f9da6ab43f5469802b2f2bef319a
koblack <koblack@users.noreply.github.com>
2019-04-18 19:00:11 +0200

    diskspace plugin: cleanup translations

--- plugins/diskspace/lang/sv.js
+++ plugins/diskspace/lang/sv.js
@@ -6,6 +6,6 @@
  * Author: Magnus Holm (holmen@brasse.se)
  */
 
- theUILang.diskNotification = "Varning! Disken är full. rTorrent kan inte köras korrekt och ingen data kommer att laddas ner förrän du frigör diskutrymme.";
+ theUILang.diskNotification	= "Varning! Disken är full. rTorrent kan inte köras korrekt och ingen data kommer att laddas ner förrän du frigör diskutrymme.";
 
 thePlugins.get("diskspace").langLoaded();
