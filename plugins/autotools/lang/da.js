923dbf79720994cd58a17703ba3cba84bca81357
koblack <koblack@users.noreply.github.com>
2019-04-18 19:00:11 +0200

    autotools plugin: cleanup translations

--- plugins/autotools/lang/da.js
+++ plugins/autotools/lang/da.js
@@ -9,16 +9,16 @@
  var s_PluginFail			= "Plugin will not work.";
 
  theUILang.autotools			= "Autotools";
- theUILang.autotoolsEnableLabel 	= "Enable \"AutoLabel\" feature, Template:";
+ theUILang.autotoolsEnableLabel		= "Enable \"AutoLabel\" feature, Template:";
  theUILang.autotoolsPathToFinished	= "Path to finished downloads";
- theUILang.autotoolsEnableWatch 	= "Enable \"AutoWatch\" feature";
- theUILang.autotoolsPathToWatch 	= "Path to base watch directory";
+ theUILang.autotoolsEnableWatch		= "Enable \"AutoWatch\" feature";
+ theUILang.autotoolsPathToWatch		= "Path to base watch directory";
  theUILang.autotoolsWatchStart		= "Start download automatically";
  theUILang.autotoolsNoPathToFinished	= "Autotools plugin: path to finished downloads is not set. " + s_PluginFail;
  theUILang.autotoolsNoPathToWatch	= "Autotools plugin: path to base watch directory is not set. " + s_PluginFail;
  theUILang.autotoolsFileOpType		= "Operation type";
  theUILang.autotoolsFileOpMove		= "Move";
- theUILang.autotoolsFileOpHardLink 	= "Hard link";
+ theUILang.autotoolsFileOpHardLink	= "Hard link";
  theUILang.autotoolsFileOpCopy		= "Copy";
  theUILang.autotoolsFileOpSoftLink	= "Soft link";
  theUILang.autotoolsAddLabel		= "Add torrent's label to path";
--- plugins/autotools/lang/hu.js
+++ plugins/autotools/lang/hu.js
@@ -9,16 +9,16 @@
  var s_PluginFail			= "A bővitmény nem működik.";
 
  theUILang.autotools			= "Auto Beállítás";
- theUILang.autotoolsEnableLabel 	= "Engedélyezi az \"Auto címkézés\" funkciót, sablon:";
+ theUILang.autotoolsEnableLabel		= "Engedélyezi az \"Auto címkézés\" funkciót, sablon:";
  theUILang.autotoolsPathToFinished	= "Befejezett letöltési elérési út";
- theUILang.autotoolsEnableWatch 	= "Engedélyezi az \"AutoWatch\" funkciót";
- theUILang.autotoolsPathToWatch 	= "Watch mappa elérési út";
+ theUILang.autotoolsEnableWatch		= "Engedélyezi az \"AutoWatch\" funkciót";
+ theUILang.autotoolsPathToWatch		= "Watch mappa elérési út";
  theUILang.autotoolsWatchStart		= "Letöltés indítása automatikusan";
- theUILang.autotoolsNoPathToFinished	= "Auto Beállítás bővítmény: a letöltési mappa nincs beállítva " + s_PluginFail;
- theUILang.autotoolsNoPathToWatch	= "Auto Beállítás bővítmény: a watch mappa nincs beállítva " + s_PluginFail;
+ theUILang.autotoolsNoPathToFinished	= "Auto Beállítás bővítmény: a letöltési mappa nincs beállítva. " + s_PluginFail;
+ theUILang.autotoolsNoPathToWatch	= "Auto Beállítás bővítmény: a watch mappa nincs beállítva. " + s_PluginFail;
  theUILang.autotoolsFileOpType		= "Művelet típusa";
  theUILang.autotoolsFileOpMove		= "Áthelyezés";
- theUILang.autotoolsFileOpHardLink 	= "Hard link";
+ theUILang.autotoolsFileOpHardLink	= "Hard link";
  theUILang.autotoolsFileOpCopy		= "Másolás";
  theUILang.autotoolsFileOpSoftLink	= "Soft link";
  theUILang.autotoolsAddLabel		= "Add torrent's label to path";
