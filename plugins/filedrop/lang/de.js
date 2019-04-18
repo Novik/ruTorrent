e4db25c653ed93342a7a30b746aafeb3ff3cc866
koblack <koblack@users.noreply.github.com>
2019-04-18 19:00:10 +0200

    filedrop plugin: cleanup translations

--- plugins/filedrop/lang/de.js
+++ plugins/filedrop/lang/de.js
@@ -7,7 +7,7 @@
  */
 
  theUILang.doesntSupportHTML5	= "Plugin filedrop: Ihr Browser unterstützt keine HTML5-Uploads. Plugin wurde deaktiviert.";
- theUILang.tooManyFiles 	= "Plugin filedrop: Zu viele Dateien. Maximal <= ";
- theUILang.fileTooLarge 	= "ist zu groß. Bitte lade Dateien bis zu";
+ theUILang.tooManyFiles		= "Plugin filedrop: Zu viele Dateien. Maximal <= ";
+ theUILang.fileTooLarge		= "ist zu groß. Bitte lade Dateien bis zu";
 
 thePlugins.get("filedrop").langLoaded();
