Novik/ruTorrent

commit c9f7016300ec3babe7e8bbde06d5e4518a5c7f88

add RUTracker and HDDream engines to plugin extsearch

plugins/loginmgr/init.js
@@ -9,10 +9,10 @@ if(plugin.enabled && plugin.canChangeOptions())
 		{
 			$.each( theWebUI.theAccounts, function(name,val)
 			{
-				$('#'+name+'_enabled').attr("checked", (val.enabled==1));
-				$('#'+name+'_login').val(val.login);
-				$('#'+name+'_password').val(val.password);
-				$('#'+name+'_enabled').change();
+				$('#'+name+'_lmenabled').attr("checked", (val.enabled==1));
+				$('#'+name+'_lmlogin').val(val.login);
+				$('#'+name+'_lmpassword').val(val.password);
+				$('#'+name+'_lmenabled').change();
 			});
 		}
 		plugin.accaddAndShowSettings.call(theWebUI,arg);
@@ -23,9 +23,9 @@ if(plugin.enabled && plugin.canChangeOptions())
 		var ret = false;
 		$.each( theWebUI.theAccounts, function(name,val)
 		{
-			if( ($('#'+name+'_enabled').attr("checked") ^ val.enabled) ||
-				($('#'+name+'_login').val()!=val.login) ||
-				($('#'+name+'_password').val()!=val.password))
+			if( ($('#'+name+'_lmenabled').attr("checked") ^ val.enabled) ||
+				($('#'+name+'_lmlogin').val()!=val.login) ||
+				($('#'+name+'_lmpassword').val()!=val.password))
 			{
 				ret = true;
 				return(false);
@@ -47,9 +47,9 @@ if(plugin.enabled && plugin.canChangeOptions())
 		var s = '';
 		$.each( theWebUI.theAccounts, function(name,val)
 		{
-			s+=("&"+name+"_enabled="+($('#'+name+'_enabled').attr("checked") ? 1 : 0)+
-				"&"+name+"_login="+encodeURIComponent($.trim($('#'+name+'_login').val()))+
-				"&"+name+"_password="+encodeURIComponent($.trim($('#'+name+'_password').val())));
+			s+=("&"+name+"_enabled="+($('#'+name+'_lmenabled').attr("checked") ? 1 : 0)+
+				"&"+name+"_login="+encodeURIComponent($.trim($('#'+name+'_lmlogin').val()))+
+				"&"+name+"_password="+encodeURIComponent($.trim($('#'+name+'_lmpassword').val())));
 		});
 		this.content = "mode=set"+s;
 	        this.contentType = "application/x-www-form-urlencoded";
@@ -67,15 +67,15 @@ plugin.onLangLoaded = function()
 			"<legend>"+name+"</legend>"+
 			"<table>"+
 				"<tr>"+
-					"<td><input type='checkbox' id='"+name+"_enabled' onchange=\"linked(this, 0, ['"+name+"_login','"+name+"_password']);\"/><label for='"+name+"_enabled' id='lbl_"+name+"_enabled'>"+theUILang.Enabled+"</label></td>"+
+					"<td><input type='checkbox' id='"+name+"_lmenabled' onchange=\"linked(this, 0, ['"+name+"_lmlogin','"+name+"_lmpassword']);\"/><label for='"+name+"_enabled' id='lbl_"+name+"_lmenabled'>"+theUILang.Enabled+"</label></td>"+
 				"</tr>"+
 				"<tr>"+
-					"<td><label id='lbl_"+name+"_login' for='"+name+"_login' class='disabled'>"+theUILang.accLogin+":</label></td>"+
-					"<td class=\"alr\"><input type='text' id='"+name+"_login' class='TextboxLarge' maxlength='32' disabled='true' /></td>"+
+					"<td><label id='lbl_"+name+"_lmlogin' for='"+name+"_lmlogin' class='disabled'>"+theUILang.accLogin+":</label></td>"+
+					"<td class=\"alr\"><input type='text' id='"+name+"_lmlogin' class='TextboxLarge' maxlength='32' disabled='true' /></td>"+
 				"</tr>"+
 				"<tr>"+
-					"<td><label id='lbl_"+name+"_password' for='"+name+"_password' class='disabled'>"+theUILang.accPassword+":</label></td>"+
-					"<td class=\"alr\"><input type='password' id='"+name+"_password' class='TextboxLarge' maxlength='32' disabled='true' /></td>"+
+					"<td><label id='lbl_"+name+"_lmpassword' for='"+name+"_lmpassword' class='disabled'>"+theUILang.accPassword+":</label></td>"+
+					"<td class=\"alr\"><input type='lmpassword' id='"+name+"_lmpassword' class='TextboxLarge' maxlength='32' disabled='true' /></td>"+
 				"</tr>"+
 			"</table>"+
 		"</fieldset>";

plugins/extsearch/init.js
@@ -263,7 +263,7 @@ plugin.correctCounter = function(id,count)
 					count++;
 		}
 		$("#"+id+"-c").text(count);
-		$("#"+id).attr("title",plugin.tegs[id].val+" (0)");
+		$("#"+id).attr("title",plugin.tegs[id].val+" ("+count+")");
 	}
 }
 
@@ -630,8 +630,8 @@ if(plugin.enabled && plugin.canChangeOptions())
 				$('#'+ndx+'_enabled').attr("checked", (val.enabled==1));
 				$('#'+ndx+'_global').attr("checked", (val.global==1));
 				$('#'+ndx+'_limit').val(val.limit);
-				$('#'+ndx+'_enabled').change();
 				$('#'+ndx+'_global').change();
+				$('#'+ndx+'_enabled').change();
 			});
 		}
 		plugin.andShowSettings.call(theWebUI,arg);
@@ -749,8 +749,8 @@ plugin.onLangLoaded = function()
 			contPublic +=
 				"<fieldset>"+
 					"<legend>"+ndx+"</legend>"+
-					"<div class='checkbox'><input type='checkbox' id='"+ndx+"_enabled' checked='true' onchange=\"linked(this, 0, ['"+ndx+"_global','"+ndx+"_limit']);\"/><label for='"+ndx+"_enabled' id='lbl_"+ndx+"_enabled'>"+theUILang.Enabled+"</label></div>"+
-					"<div class='checkbox'><input type='checkbox' id='"+ndx+"_global' checked='true' onchange=\"linked(this, 0, ['"+ndx+"_limit']);\"/><label for='"+ndx+"_global' id='lbl_"+ndx+"_global'>"+theUILang.exsGlobal+"</label></div>"+
+					"<div class='checkbox'><input type='checkbox' id='"+ndx+"_enabled' onchange=\"linked(this, 0, ['"+ndx+"_global','"+ndx+"_limit']);\"/><label for='"+ndx+"_enabled' id='lbl_"+ndx+"_enabled'>"+theUILang.Enabled+"</label></div>"+
+					"<div class='checkbox'><input type='checkbox' id='"+ndx+"_global' onchange=\"linked(this, 0, ['"+ndx+"_limit']);\"/><label for='"+ndx+"_global' id='lbl_"+ndx+"_global'>"+theUILang.exsGlobal+"</label></div>"+
 					"<div class='checkbox'><label for='"+ndx+"_limit' id='lbl_"+ndx+"_limit'>"+theUILang.exsLimit+":</label><input type='text' class='Textbox num' maxlength=6 id='"+ndx+"_limit'/></div>"+
 				"</fieldset>";
 		else
@@ -758,8 +758,8 @@ plugin.onLangLoaded = function()
 			contPrivate +=  
 				"<fieldset>"+
 					"<legend>"+ndx+"</legend>"+
-					"<div class='checkbox'><input type='checkbox' id='"+ndx+"_enabled' checked='true' onchange=\"linked(this, 0, ['"+ndx+"_global','"+ndx+"_limit']);\"/><label for='"+ndx+"_enabled' id='lbl_"+ndx+"_enabled'>"+theUILang.Enabled+"</label></div>"+
-					"<div class='checkbox'><input type='checkbox' id='"+ndx+"_global' checked='true' onchange=\"linked(this, 0, ['"+ndx+"_limit']);\"/><label for='"+ndx+"_global' id='lbl_"+ndx+"_global'>"+theUILang.exsGlobal+"</label></div>"+
+					"<div class='checkbox'><input type='checkbox' id='"+ndx+"_enabled' onchange=\"linked(this, 0, ['"+ndx+"_global','"+ndx+"_limit']);\"/><label for='"+ndx+"_enabled' id='lbl_"+ndx+"_enabled'>"+theUILang.Enabled+"</label></div>"+
+					"<div class='checkbox'><input type='checkbox' id='"+ndx+"_global' onchange=\"linked(this, 0, ['"+ndx+"_limit']);\"/><label for='"+ndx+"_global' id='lbl_"+ndx+"_global'>"+theUILang.exsGlobal+"</label></div>"+
 					"<div class='checkbox'><label for='"+ndx+"_limit' id='lbl_"+ndx+"_limit'>"+theUILang.exsLimit+":</label><input type='text' class='Textbox num' maxlength=6 id='"+ndx+"_limit'/></div>";
 			if(val.cookies)
 			{

plugins/extsearch/engines.php
@@ -45,12 +45,12 @@ class commonEngine
 		$client->use_gzip = HTTP_USE_GZIP;
 		return($client);
 	}
-	public function fetch($url,$encode = true)
+	public function fetch($url, $encode = 1, $method="GET", $content_type="", $body="")
 	{
 		$client = $this->makeClient($url);
 		if($encode)
 			$url = Snoopy::linkencode($url);
-		$client->fetchComplex($url);
+		$client->fetchComplex($url, $method, $content_type, $body);
 		if($client->status>=200 && $client->status<300)
 		{
 			ini_set( "pcre.backtrack_limit", max(strlen($client->results),100000) );
@@ -208,19 +208,19 @@ class engineManager
 					$name = basename($file,".php");
 					$this->engines[$name] = array( "name"=>$name, "path"=>fullpath($dir.'/'.$file), "object"=>$name."Engine", "enabled"=>true, "global"=>true, "limit"=>100 );
 					$obj = $this->getObject($name);
-					$this->engines[$name]["enabled"] = $obj->defaults["public"];
-					$this->engines[$name]["public"] = $obj->defaults["public"];
+					$this->engines[$name]["enabled"] = intval($obj->defaults["public"]);
+					$this->engines[$name]["public"] = intval($obj->defaults["public"]);
 					$this->engines[$name]["limit"] = $obj->defaults["page_size"];
 					$this->engines[$name]["cats"] = $obj->categories;
 					$this->engines[$name]["cookies"] = (array_key_exists("cookies",$obj->defaults) ? $obj->defaults["cookies"] : '');
 					$this->engines[$name]["auth"] = (array_key_exists("auth",$obj->defaults) ? 1 : 0);
 					if(array_key_exists("disabled",$obj->defaults) && $obj->defaults["disabled"])
-						$this->engines[$name]["enabled"] = false;
+						$this->engines[$name]["enabled"] = 0;
 					if(array_key_exists($name,$oldEngines) && array_key_exists("limit",$oldEngines[$name]))
 					{
-						$this->engines[$name]["enabled"] = $oldEngines[$name]["enabled"];
-						$this->engines[$name]["global"] = $oldEngines[$name]["global"];
-						$this->engines[$name]["limit"] = $oldEngines[$name]["limit"];
+						$this->engines[$name]["enabled"] = intval($oldEngines[$name]["enabled"]);
+						$this->engines[$name]["global"] = intval($oldEngines[$name]["global"]);
+						$this->engines[$name]["limit"] = intval($oldEngines[$name]["limit"]);
 					}
 
 					global $theSettings;
@@ -270,9 +270,9 @@ class engineManager
 		foreach( $this->engines as $name=>$nfo )
 		{
 			if(isset($_REQUEST[$name."_enabled"]))
-				$this->engines[$name]["enabled"] = $_REQUEST[$name."_enabled"];
+				$this->engines[$name]["enabled"] = intval($_REQUEST[$name."_enabled"]);
 			if(isset($_REQUEST[$name."_global"]))
-				$this->engines[$name]["global"] = $_REQUEST[$name."_global"];
+				$this->engines[$name]["global"] = intval($_REQUEST[$name."_global"]);
 			if(isset($_REQUEST[$name."_limit"]))
 				$this->engines[$name]["limit"] = intval($_REQUEST[$name."_limit"]);
 		}

plugins/extsearch/engines/HDDream.php
@@ -1,0 +1,62 @@
+<?php
+
+class HDDreamEngine extends commonEngine
+{
+       	public $defaults = array( "public"=>false, "page_size"=>15, "auth"=>1 );
+
+	public $categories = array( 'all'=>'0', 'Animes'=>'9', 'BD5'=>'4', 'BD9'=>'12', 'Bluray'=>'3', 'Clip/Concert/Spectacles HD'=>'15',
+		'Covers HD'=>'8', 'Divers'=>'10', 'DOCUMENTAIRES'=>'18', 'Flac'=>'11', 'HD 1080p'=>'14', 'HD 720p'=>'5',
+		'Logiciels'=>'7', 'Piste-Son'=>'6', 'REMUX'=>'20', 'Series HD'=>'13', 'XxX HD'=>'16'
+		); 
+
+	public function action($what,$cat,&$ret,$limit,$useGlobalCats)
+	{
+		$added = 0;
+		$url = 'http://hd-dream.net';
+		if($useGlobalCats)
+			$categories = array( 'all'=>'0', 'anime'=>'9' );
+		else
+			$categories = &$this->categories;
+		if(!array_key_exists($cat,$categories))
+			$cat = $categories['all'];
+		else
+			$cat = $categories[$cat];
+		$what = rawurlencode(self::fromUTF(rawurldecode($what),"ISO-8859-1"));
+		for($pg = 1; $pg<10; $pg++)
+		{
+			$cli = $this->fetch( $url.'/browse.php?do=search&keywords='.$what.'&search_type=t_name&sort_order=yes&sortby=seeders&orderby=DESC&page='.$pg.'&category='.$cat );
+			if($cli===false)
+				break;
+			$res = preg_match_all('/<td width="5%" align="center" rowspan="2">\n\s*<a href="http:\/\/hd-dream\.net\/browse\.php\?browse_categories&amp;category=\d+" target="_self" \/><img src.*alt=".*: (?P<cat>.*)".*'.
+				'<a href="#" id="quickmenu\d+" \/>(?P<name>.*)<\/a>.*'.
+				'<td width="10%">.*<\/td>.*<\/tr>.*<tr>.*'.
+				'<td width="15%"><b>(?P<size>.*)<\/b>.*'.
+				'<td width="20%">(?P<seeds>.*) Seeder\(s\) et (?P<leech>.*) Leecher\(s\).*'.
+				'<a href="http:\/\/hd-dream\.net\/download.php\?id=(?P<id>.*)"/siU', $cli->results, $matches);
+			if($res)
+			{
+				for($i=0; $i<$res; $i++)
+				{
+					$link = $url."/download.php?id=".$matches["id"][$i];
+					if(!array_key_exists($link,$ret))
+					{
+						$item = $this->getNewEntry();
+						$item["cat"] = self::toUTF(self::removeTags($matches["cat"][$i]),"ISO-8859-1");
+						$item["desc"] = $url."/details.php?id=".$matches["id"][$i];
+						$item["name"] = self::toUTF(self::removeTags($matches["name"][$i]),"ISO-8859-1");
+						$item["size"] = self::formatSize($matches["size"][$i]);
+						$item["seeds"] = intval(self::removeTags($matches["seeds"][$i]));
+						$item["peers"] = intval(self::removeTags($matches["leech"][$i]));
+						$ret[$link] = $item;
+						$added++;
+						if($added>=$limit)
+							return;
+					}
+				}
+			}
+			else
+				break;
+		}
+	}
+}
+?>

plugins/extsearch/engines/RUTracker.php
@@ -1,0 +1,75 @@
+<?php
+
+class ruTrackerEngine extends commonEngine
+{
+       	public $defaults = array( "public"=>false, "page_size"=>50, "auth"=>1 );
+
+	public $categories = array( 'all'=>"&f[]=-1" );
+
+	protected function parseTList($results,&$added,&$ret,$limit)
+	{
+		if( strpos($results, ">�� �������</td>")!==false )
+			break;
+		$res = preg_match_all('/<a class="gen f" href="tracker\.php\?f=\d+">(?P<cat>.*)<\/a><\/td>.*'.
+			'<a class="med tLink" href="\.\/viewtopic\.php\?t=(?P<id>\d+)"><b>(?P<name>.*)<\/b><\/a>.*'.
+			'<u>(?P<size>.*)<\/u>\n\s*'.
+			'<a class="small tr-dl dl-stub" href="(?P<link>.*)">.*'.
+			'<td class="row4 seedmed"><b>(?P<seeds>.*)<\/b><\/td>\n\s*'.
+			'<td class="row4 leechmed" title=".*"><b>(?P<leech>.*)<\/b><\/td>.*'.
+			'<u>(?P<date>.*)<\/u>/siU', $results, $matches);
+		if($res)
+		{
+			for($i=0; $i<$res; $i++)
+			{
+				$link = $matches["link"][$i];
+				if(!array_key_exists($link,$ret))
+				{
+					$item = $this->getNewEntry();
+					$item["cat"] = self::toUTF(self::removeTags($matches["cat"][$i],"CP1251"),"CP1251");
+					$item["desc"] = "http://rutracker.org/forum/viewtopic.php?t=".$matches["id"][$i];
+					$item["name"] = self::toUTF(self::removeTags($matches["name"][$i],"CP1251"),"CP1251");
+					$item["size"] = floatval($matches["size"][$i]);
+					$item["time"] = floatval($matches["date"][$i]);
+					$item["seeds"] = intval(self::removeTags($matches["seeds"][$i]));
+					$item["peers"] = intval(self::removeTags($matches["leech"][$i]));
+					$ret[$link] = $item;
+					$added++;
+					if($added>=$limit)
+						return(false);
+				}
+			}
+			return(true);
+		}
+		else
+			return(false);
+	}
+
+	public function action($what,$cat,&$ret,$limit,$useGlobalCats)
+	{
+		$added = 0;
+		$url = 'http://rutracker.org';
+		if($useGlobalCats)
+			$categories = array( 'all'=>'&f[]=-1' );
+		else
+			$categories = &$this->categories;
+		if(!array_key_exists($cat,$categories))
+			$cat = $categories['all'];
+		else
+			$cat = $categories[$cat];
+		$what = rawurlencode(self::fromUTF(rawurldecode($what),"CP1251"));
+		$cli = $this->fetch( $url.'/forum/tracker.php', 0, "POST", "application/x-www-form-urlencoded", 
+			'prev_my=0&prev_new=0&prev_oop=1'.$cat.'&o=10&s=2&oop=1&nm='.$what.'&submit=%CF%EE%E8%F1%EA' );
+		if(($cli!==false) && $this->parseTList($cli->results,$added,$ret,$limit))
+		{
+			$res = preg_match_all('/<a class="pg" href="tracker.php\?search_id=(?P<next>[^"]*)">/siU', $cli->results, $next);
+			$next = array_unique($next["next"]);
+			for($pg = 0; $pg<count($next); $pg++)	
+			{
+				$cli = $this->fetch( $url.'/forum/tracker.php?search_id='.self::removeTags($next[$pg]) );
+				if(($cli==false) || !$this->parseTList($cli->results,$added,$ret,$limit))
+					break;
+			}
+		}
+	}
+}
+?>

plugins/loginmgr/accounts.php
@@ -9,6 +9,7 @@ class privateData
 {
 	public $hash = '';
 	public $cookies = null;
+	public $referer = null;
 	public $loaded = false;
 
 	static public function load( $owner, $client = null )
@@ -20,6 +21,7 @@ class privateData
 			if($cache->get($rt))
 			{
 				$client->cookies = $rt->cookies;
+				$client->referer = $rt->referer;
 				$rt->loaded = true;
 			}
 		}
@@ -41,6 +43,7 @@ class privateData
 	public function store( $client )
 	{
 	        $this->cookies = $client->cookies;
+		$this->referer = $client->referer;
 		$cache = new rCache('/accounts');
 		return($cache->set($this));
 	}
@@ -69,7 +72,7 @@ abstract class commonAccount
 	public function fetch( $client, $url, $login, $password, $method, $content_type, $body )
 	{
 		$data = privateData::load( $this->getName(), $client );
-		return( ($data->loaded && 
+		$ret = ( ($data->loaded && 
 				$this->updateCached($client,$url) && 
 				$client->fetch($url,$method, $content_type, $body) &&
 				$this->isOK($client)) ||
@@ -80,6 +83,9 @@ abstract class commonAccount
                                 $client->fetch($url,$method, $content_type, $body) &&
 				$this->isOK($client) &&
 				$data->store($client)) );
+		if(!$ret)
+			$data->remove();
+		return($ret);
 	}
 	
 }

commit a10ad30ed3456abe6683cfe4e663ad5f01e9395f

loginmgr plugin: minor fix
fixes: c9f7016300ec3babe7e8bbde06d5e4518a5c7f88 ("add RUTracker and HDDream engines to plugin extsearch")

plugins/loginmgr/init.js
@@ -71,7 +71,7 @@ plugin.onLangLoaded = function()
 			"<legend>"+name+"</legend>"+
 			"<table>"+
 				"<tr>"+
-					"<td><input type='checkbox' id='"+name+"_lmenabled' onchange=\"linked(this, 0, ['"+name+"_lmlogin','"+name+"_lmpassword','"+name+"_lmauto']);\"/><label for='"+name+"_enabled' id='lbl_"+name+"_lmenabled'>"+theUILang.Enabled+"</label></td>"+
+					"<td><input type='checkbox' id='"+name+"_lmenabled' onchange=\"linked(this, 0, ['"+name+"_lmlogin','"+name+"_lmpassword','"+name+"_lmauto']);\"/><label for='"+name+"_lmenabled' id='lbl_"+name+"_lmenabled'>"+theUILang.Enabled+"</label></td>"+
 				"</tr>"+
 				"<tr>"+
 					"<td><label id='lbl_"+name+"_lmlogin' for='"+name+"_lmlogin' class='disabled'>"+theUILang.accLogin+":</label></td>"+