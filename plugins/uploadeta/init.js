Novik/ruTorrent

commit b52da98080ff13fa779a3257c554ca6ea2d33945

multi-language support for 'uploadeta' plugin

plugins/uploadeta/init.js
@@ -1,37 +1,54 @@
+plugin.loadLang();
 
-if(plugin.canChangeColumns())
-{
-	/* Add the columns to the torrent list */
-	theWebUI.tables.trt.columns.push({text: "UL target", width: "60px", id: "upload_target", type: TYPE_NUMBER});
-	theWebUI.tables.trt.columns.push({text: "UL remaining", width: "60px", id: "upload_remaining", type: TYPE_NUMBER});
-	theWebUI.tables.trt.columns.push({text: "UL ETA", width: "60px", id: "upload_eta", type: TYPE_NUMBER});
+if(plugin.canChangeColumns()) {
+	plugin.config = theWebUI.config;
+	theWebUI.config = function(data) {
+		/* Add the columns to the torrent list */
+		theWebUI.tables.trt.columns.push({text: "UL target", width: "100px", id: "upload_target", type: TYPE_NUMBER});
+		theWebUI.tables.trt.columns.push({text: "UL remaining", width: "100px", id: "upload_remaining", type: TYPE_NUMBER});
+		theWebUI.tables.trt.columns.push({text: "UL ETA", width: "100px", id: "upload_eta", type: TYPE_NUMBER});
 
-	/* Use the formatter to add data to the columns */
-	plugin.trtFormat = theWebUI.tables.trt.format;
-	theWebUI.tables.trt.format = function(table, arr) {
-		for(var i in arr) {
-			switch(table.getIdByCol(i)){
-				case "upload_target":
-					plugin.upload_target = parseInt(arr[2]) * (theWebUI.uploadtarget/100);
-					arr[i] = theConverter.bytes(plugin.upload_target, 2);
-					break;
-				case "upload_remaining":
-					plugin.upload_remaining = plugin.upload_target - arr[5];
-					arr[i] = theConverter.bytes(plugin.upload_remaining, 2);
-					break;
-				case "upload_eta":
-					arr[i] = (arr[8]>0) ? theConverter.time(Math.floor(plugin.upload_remaining/arr[8])) : "\u221e";
-					break;
+		/* Use the formatter to add data to the columns */
+		plugin.trtFormat = theWebUI.tables.trt.format;
+		theWebUI.tables.trt.format = function(table, arr) {
+			for(var i in arr) {
+				switch(table.getIdByCol(i)){
+					case "upload_target":
+						plugin.upload_target = parseInt(arr[2]) * (theWebUI.uploadtarget/100);
+						arr[i] = theConverter.bytes(plugin.upload_target, 2);
+						break;
+					case "upload_remaining":
+						plugin.upload_remaining = plugin.upload_target - arr[5];
+						arr[i] = theConverter.bytes(plugin.upload_remaining, 2);
+						break;
+					case "upload_eta":
+						arr[i] = (arr[8]>0) ? theConverter.time(Math.floor(plugin.upload_remaining/arr[8])) : "\u221e";
+						break;
+				}
 			}
+			return(plugin.trtFormat(table, arr));
 		}
-		return(plugin.trtFormat(table, arr));
+		plugin.config.call(this,data);
+		plugin.trtRenameColumn();
 	}
 }
 
+plugin.trtRenameColumn = function() {
+	if(plugin.allStuffLoaded) {
+		theWebUI.getTable("trt").renameColumnById("upload_target",theUILang.ULtarget);
+		theWebUI.getTable("trt").renameColumnById("upload_remaining",theUILang.ULremaining);
+		theWebUI.getTable("trt").renameColumnById("upload_eta",theUILang.ULETA);
+	}
+	else
+		setTimeout(arguments.callee,1000);
+}
+
 /* Create option page */
-var input = "<fieldset><legend>Upload target</legend><div><input id='uploadtarget' type='text' size=1/>%</div></fieldset>";
-var description = "<div>This plugin only will show the amount of data and time that is left to an upload ratio target. It will not automatically remove a torrent for you. Look at ratio.min.set in your rtorrent configuration file to remove a torrent when a upload target has been reached.</div>";
-rPlugin.prototype.attachPageToOptions($("<div>").attr("id","st_uploadeta").html(input + description)[0], 'Upload ETA');
+plugin.onLangLoaded = function() {
+var input = "<fieldset><legend>"+theUILang.uploadtarget+"</legend><div><input id='uploadtarget' type='text' size=1/>%</div></fieldset>";
+var description = "<div>"+theUILang.ULdescription+"</div>";
+this.attachPageToOptions($("<div>").attr("id","st_uploadeta").html(input + description)[0], theUILang.uploadeta);
+}
 
 /* Field shoud be numbers only */
 $('#uploadtarget').keypress(function(event){

plugins/ratio/lang/ru.js
@@ -7,7 +7,7 @@
  */
 
  theUILang.ratios		= "Группы ratio";
- theUILang.ratio		= "ГруппаRatio";
+ theUILang.ratio		= "Группа Ratio";
  theUILang.mnuRatio		= "Установить ratio";
  theUILang.mnuRatioUnlimited	= "Нет";
  theUILang.ratioName		= "Имя";

plugins/uploadeta/lang/cs.js
@@ -1,0 +1,16 @@
+﻿/*
+ * PLUGIN UploadETA
+ *
+ * Czech language file.
+ *
+ * Author: 
+ */
+
+ theUILang.uploadeta		= "Upload ETA";
+ theUILang.uploadtarget		= "Upload Target";
+ theUILang.ULtarget		= "UL Target";
+ theUILang.ULremaining		= "UL Remaining";
+ theUILang.ULETA		= "UL ETA";
+ theUILang.ULdescription	= "This plugin only will show the amount of data and time that is left to an upload ratio target. It will not automatically remove a torrent for you. Look at 'ratio.min.set' in your rTorrent configuration file to remove a torrent when a upload target has been reached.";
+
+thePlugins.get("uploadeta").langLoaded();

plugins/uploadeta/lang/da.js
@@ -1,0 +1,16 @@
+﻿/*
+ * PLUGIN UploadETA
+ *
+ * Danish language file.
+ *
+ * Author: 
+ */
+
+ theUILang.uploadeta		= "Upload ETA";
+ theUILang.uploadtarget		= "Upload Target";
+ theUILang.ULtarget		= "UL Target";
+ theUILang.ULremaining		= "UL Remaining";
+ theUILang.ULETA		= "UL ETA";
+ theUILang.ULdescription	= "This plugin only will show the amount of data and time that is left to an upload ratio target. It will not automatically remove a torrent for you. Look at 'ratio.min.set' in your rTorrent configuration file to remove a torrent when a upload target has been reached.";
+
+thePlugins.get("uploadeta").langLoaded();

plugins/uploadeta/lang/de.js
@@ -1,0 +1,16 @@
+﻿/*
+ * PLUGIN UploadETA
+ *
+ * German language file.
+ *
+ * Author: 
+ */
+
+ theUILang.uploadeta		= "Upload ETA";
+ theUILang.uploadtarget		= "Upload Target";
+ theUILang.ULtarget		= "UL Target";
+ theUILang.ULremaining		= "UL Remaining";
+ theUILang.ULETA		= "UL ETA";
+ theUILang.ULdescription	= "This plugin only will show the amount of data and time that is left to an upload ratio target. It will not automatically remove a torrent for you. Look at 'ratio.min.set' in your rTorrent configuration file to remove a torrent when a upload target has been reached.";
+
+thePlugins.get("uploadeta").langLoaded();

plugins/uploadeta/lang/el.js
@@ -1,0 +1,16 @@
+﻿/*
+ * PLUGIN UploadETA
+ *
+ * Greek language file.
+ *
+ * Author: 
+ */
+
+ theUILang.uploadeta		= "Upload ETA";
+ theUILang.uploadtarget		= "Upload Target";
+ theUILang.ULtarget		= "UL Target";
+ theUILang.ULremaining		= "UL Remaining";
+ theUILang.ULETA		= "UL ETA";
+ theUILang.ULdescription	= "This plugin only will show the amount of data and time that is left to an upload ratio target. It will not automatically remove a torrent for you. Look at 'ratio.min.set' in your rTorrent configuration file to remove a torrent when a upload target has been reached.";
+
+thePlugins.get("uploadeta").langLoaded();

plugins/uploadeta/lang/en.js
@@ -1,0 +1,16 @@
+﻿/*
+ * PLUGIN UploadETA
+ *
+ * English language file.
+ *
+ * Author: 
+ */
+
+ theUILang.uploadeta		= "Upload ETA";
+ theUILang.uploadtarget		= "Upload Target";
+ theUILang.ULtarget		= "UL Target";
+ theUILang.ULremaining		= "UL Remaining";
+ theUILang.ULETA		= "UL ETA";
+ theUILang.ULdescription	= "This plugin only will show the amount of data and time that is left to an upload ratio target. It will not automatically remove a torrent for you. Look at 'ratio.min.set' in your rTorrent configuration file to remove a torrent when a upload target has been reached.";
+
+thePlugins.get("uploadeta").langLoaded();

plugins/uploadeta/lang/es.js
@@ -1,0 +1,16 @@
+﻿/*
+ * PLUGIN UploadETA
+ *
+ * Spanish language file.
+ *
+ * Author: 
+ */
+
+ theUILang.uploadeta		= "Upload ETA";
+ theUILang.uploadtarget		= "Upload Target";
+ theUILang.ULtarget		= "UL Target";
+ theUILang.ULremaining		= "UL Remaining";
+ theUILang.ULETA		= "UL ETA";
+ theUILang.ULdescription	= "This plugin only will show the amount of data and time that is left to an upload ratio target. It will not automatically remove a torrent for you. Look at 'ratio.min.set' in your rTorrent configuration file to remove a torrent when a upload target has been reached.";
+
+thePlugins.get("uploadeta").langLoaded();

commit 3ef8c5be298e2e01e36cbe6d406999e31a21dbaa

uploadeta plugin: add missing translation
fixes: b52da98080ff13fa779a3257c554ca6ea2d33945 ("multi-language support for 'uploadeta' plugin")

plugins/uploadeta/init.js
@@ -47,6 +47,30 @@ plugin.trtRenameColumn = function() {
 		theWebUI.getTable("trt").renameColumnById("upload_target",theUILang.ULtarget);
 		theWebUI.getTable("trt").renameColumnById("upload_remaining",theUILang.ULremaining);
 		theWebUI.getTable("trt").renameColumnById("upload_eta",theUILang.ULETA);
+		if(thePlugins.isInstalled("rss"))
+			plugin.rssRenameColumn();
+		if(thePlugins.isInstalled("extsearch"))
+			plugin.tegRenameColumn();
+	}
+	else
+		setTimeout(arguments.callee,1000);
+}
+
+plugin.rssRenameColumn = function() {
+	if(theWebUI.getTable("rss").created) {
+		theWebUI.getTable("rss").renameColumnById("upload_target",theUILang.ULtarget);
+		theWebUI.getTable("rss").renameColumnById("upload_remaining",theUILang.ULremaining);
+		theWebUI.getTable("rss").renameColumnById("upload_eta",theUILang.ULETA);
+	}
+	else
+		setTimeout(arguments.callee,1000);
+}
+
+plugin.tegRenameColumn = function() {
+	if(theWebUI.getTable("teg").created) {
+		theWebUI.getTable("teg").renameColumnById("upload_target",theUILang.ULtarget);
+		theWebUI.getTable("teg").renameColumnById("upload_remaining",theUILang.ULremaining);
+		theWebUI.getTable("teg").renameColumnById("upload_eta",theUILang.ULETA);
 	}
 	else
 		setTimeout(arguments.callee,1000);