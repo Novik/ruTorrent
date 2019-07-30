5e786815d37ba75a0e8780f2c61c4dc94727b57b
koblack <koblack@users.noreply.github.com>
2019-07-30 20:51:43 +0200

    extsearch plugin: update ImmortalSeed

--- plugins/extsearch/engines/ImmortalSeed.php
+++ plugins/extsearch/engines/ImmortalSeed.php
@@ -2,37 +2,85 @@
 
 class ImmortalSeedEngine extends commonEngine
 {
-       	public $defaults = array( "public"=>false, "page_size"=>30, "auth"=>1 );
-       	
-	public $categories = array( 'all'=>'0', 'All Nuked'=>'3', 'Anime'=>'32', 'Apps'=>'23', 'Audiobooks'=>'35', 'Childrens/Cartoons'=>'31',
-		'Ebooks'=>'22', '|-- Comics'=>'41', '|-- Magazines'=>'46', 'Games'=>'25', '|-- Games Xbox'=>'29', '|-- Games-PC ISO'=>'26',
-		'|-- Games-PC Rips'=>'27', '|-- Games-PSx'=>'28', 'Movies-HD'=>'16', '|-- Non-English'=>'18', 'Movies-Low Def'=>'17',
-		'|-- Non-English'=>'34', 'Movies-SD'=>'14', '|-- Non-English'=>'33', 'Music'=>'30', '|-- FLAC'=>'37', '|-- MP3'=>'36',
-		'|-- Other'=>'39', '|-- Video'=>'38', 'Other'=>'45', 'Pre edit torrents'=>'5', 'Sports Tv'=>'7', '|-- Fitness-Instructional'=>'44',
-		'TV - 480p'=>'47', 'TV - High Definition'=>'8', 'TV SD - x264'=>'48', 'TV SD - XviD'=>'9', 'TV Season Packs - HD'=>'4',
-		'TV Season Packs - SD'=>'6' );
+	public $defaults = array( "public"=>false, "page_size"=>30, "auth"=>1 );
+
+	public $categories = array(
+		'All'=>'',
+		'All Nuked'=>'&selectedcats2=3',
+		'Anime'=>'&selectedcats2=32',
+		'Apps'=>'&selectedcats2=23',
+		'Audiobooks'=>'&selectedcats2=35',
+		'Childrens/Cartoons'=>'&selectedcats2=31',
+		'Documentary - HD'=>'&selectedcats2=54',
+		'Documentary - SD'=>'&selectedcats2=53',
+		'Ebooks'=>'&selectedcats2=22',
+		'|-- Comics'=>'&selectedcats2=41',
+		'|-- Magazines'=>'&selectedcats2=46',
+		'Games'=>'&selectedcats2=25',
+		'|-- Games Xbox'=>'&selectedcats2=29',
+		'|-- Games-PC ISO'=>'&selectedcats2=26',
+		'|-- Games-PC Rips'=>'&selectedcats2=27',
+		'|-- Games-PSx'=>'&selectedcats2=28',
+		'Mobile'=>'&selectedcats2=49',
+		'|-- Android'=>'&selectedcats2=51',
+		'|-- IOS'=>'&selectedcats2=50',
+		'|-- Windows'=>'&selectedcats2=52',
+		'Movies-4k'=>'&selectedcats2=59',
+		'|-- Non-English'=>'&selectedcats2=60',
+		'Movies-HD'=>'&selectedcats2=16',
+		'|-- Non-English'=>'&selectedcats2=18',
+		'Movies-Low Def'=>'&selectedcats2=17',
+		'|-- Non-English'=>'&selectedcats2=34',
+		'Movies-SD'=>'&selectedcats2=14',
+		'|-- Non-English'=>'&selectedcats2=33',
+		'Music'=>'&selectedcats2=30',
+		'|-- FLAC'=>'&selectedcats2=37',
+		'|-- MP3'=>'&selectedcats2=36',
+		'|-- Other'=>'&selectedcats2=39',
+		'|-- Video'=>'&selectedcats2=38',
+		'Other'=>'&selectedcats2=45',
+		'Sports Tv'=>'&selectedcats2=7',
+		'|-- Fitness-Instructional'=>'&selectedcats2=44',
+		'|-- Olympics'=>'&selectedcats2=58',
+		'TV - 480p'=>'&selectedcats2=47',
+		'TV - High Definition'=>'&selectedcats2=8',
+		'TV SD - x264'=>'&selectedcats2=48',
+		'TV SD - XviD'=>'&selectedcats2=9',
+		'TV Season Packs - HD'=>'&selectedcats2=4',
+		'TV Season Packs - SD'=>'&selectedcats2=6'
+		);
 
 	public function action($what,$cat,&$ret,$limit,$useGlobalCats)
 	{
 		$url = 'https://immortalseed.me';
 		$added = 0;
 		if($useGlobalCats)
-			$categories = array( 'all'=>'0', 'music'=>'30', 'anime'=>'32', 'software'=>'23', 'pictures'=>'31', 'books'=>'22' );
+			$categories = array(
+				'all'=>'',
+				'movies'=>'&selectedcats2=59,16,17,14',
+				'tv'=>'&selectedcats2=47,8,48,9,4,6',
+				'music'=>'&selectedcats2=30',
+				'games'=>'&selectedcats2=25',
+				'anime'=>'&selectedcats2=32',
+				'software'=>'&selectedcats2=23,49',
+				'books'=>'&selectedcats2=35,22'
+				);
 		else
 			$categories = &$this->categories;
 		if(!array_key_exists($cat,$categories))
 			$cat = $categories['all'];
 		else
 			$cat = $categories[$cat];
-		for($pg = 1; $pg<10; $pg++)
+		for($pg = 1; $pg<11; $pg++)
 		{
-			$cli = $this->fetch( $url.'/browse.php?include_dead_torrents=no&keywords='.$what.'&search_type=t_name&page='.$pg.'&category='.$cat );
-			if($cli==false || (strpos($cli->results, '<input type="password" name="password" class="inputPassword"')!==false)) 
+			$cli = $this->fetch( $url.'/browse.php?include_dead_torrents=no&keywords='.$what.'&search_type=t_name'.$cat.'&sort=seeders&order=desc&page='.$pg );
+			if($cli==false || (strpos($cli->results, '<input type="password" name="password" class="inputPassword"')!==false))
 				break;
-			$res = preg_match_all('`<img src="'.$url.'/images/categories/.*" border="0" alt="(?P<cat>.*)".*'.
+			$res = preg_match_all('`<td align="center" style="width: 40px; height: 36px;" class="unsortable2">.*'.
+				'<img src=".*" border="0" alt="(?P<cat>.*)" title=".*" width="40" height="36" />.*'.
 				'<div style="text-align:left; margin-top: 5px">(?P<name>.*)</div>.*'.
 				'</span>\s*(?P<date>.*)\s*</div>.*'.
-				'<a href="'.$url.'/download\.php\?id=(?P<id>\d+)["&].*'.
+				'<a href=".*id=(?P<id>\d+)&type=ssl">.*'.
 				'<td align="center" class="unsortable2">\s*(?P<size>.*)\s*</td>.*'.
 				'title="Seeders">(?P<seeds>.*)</a>.*'.
 				'title="Leechers">(?P<leech>.*)</a>'.
