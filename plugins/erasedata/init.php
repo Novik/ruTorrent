f9064eddd025f918f4a2004e8cdd8192062e115a
Novik <novik65@gmail.com>
2020-04-09 21:21:07 +0300

    optimize plugin erasedata

--- plugins/erasedata/init.php
+++ plugins/erasedata/init.php
@@ -9,12 +9,20 @@ $thisDir = dirname(__FILE__);
 
 $req = new rXMLRPCRequest( array(
 	$theSettings->getOnEraseCommand(array('erasedata0'.getUser(),
-		getCmd('d.open').'= ; '.getCmd('branch=').getCmd('d.get_custom5').'=,"'.getCmd('f.multicall').'=,\"'.getCmd('execute').'={'.$thisDir.'/cat.sh,'.$listPath.',$system.pid=,$'.getCmd('f.get_frozen_path').'=}\""')),
+		getCmd('d.open').'= ; '.getCmd('branch=').getCmd('d.get_custom5').'=,"'.
+			getCmd('f.multicall').'=,\"'.getCmd('file.append').'=(cat,'.$listPath.'/,$'.getCmd('system.pid').'=,.tmp),$'.getCmd('f.get_frozen_path').'=\""')),
 	$theSettings->getOnEraseCommand(array('erasedata1'.getUser(),
-		getCmd('branch=').getCmd('d.get_custom5').'=,"'.getCmd('execute').'={'.$thisDir.'/fin.sh,'.$listPath.',$'.getCmd('system.pid').'=,$'.getCmd('d.get_hash').'=,$'.getCmd('d.get_base_path').'=,$'.getCmd('d.is_multi_file').'=,$'.getCmd('d.get_custom5').'=}"')),
+		getCmd('branch=').getCmd('d.get_custom5').'=,"'.
+			getCmd('execute').'={'.$thisDir.'/fin.sh,'.
+				$listPath.',$'.
+				getCmd('system.pid').'=,$'.
+				getCmd('d.get_hash').'=,$'.
+				getCmd('d.get_base_path').'=,$'.
+				getCmd('d.is_multi_file').'=,$'.
+				getCmd('d.get_custom5').'=}"')),
 	$theSettings->getAbsScheduleCommand("erasedata",$garbageCheckInterval,
 		getCmd('execute').'={sh,-c,'.escapeshellarg(getPHP()).' '.escapeshellarg($thisDir.'/update.php').' '.escapeshellarg(getUser()).' &}' )
-	));
+	) );
 if($req->success())
 {
 	$theSettings->registerPlugin($plugin["name"],$pInfo["perms"]);
