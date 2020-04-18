142d27a9634e66c9219ed7172f62940d233d2de7
koblack <koblack@users.noreply.github.com>
2020-04-18 12:06:16 +0200

    loginmgr plugin: update RevolutionTT

--- plugins/loginmgr/accounts/RevolutionTT.php
+++ plugins/loginmgr/accounts/RevolutionTT.php
@@ -4,7 +4,7 @@
  *@author AceP1983
 */
 
-class RevTTAccount extends commonAccount
+class RevolutionTTAccount extends commonAccount
 {
 	public $url = "https://revolutiontt.me";
 
@@ -13,20 +13,20 @@ class RevTTAccount extends commonAccount
 		return(strpos($client->results, '<form id="login" method="post" action="takelogin.php">')===false);
 	}
 	protected function login($client,$login,$password,&$url,&$method,&$content_type,&$body,&$is_result_fetched)
-        {
-                $is_result_fetched = false;
-                if($client->fetch( $this->url ))
-                {
-                        $client->setcookies();
-                        $client->referer = $this->url;
+	{
+		$is_result_fetched = false;
+		if($client->fetch( $this->url ))
+		{
+			$client->setcookies();
+			$client->referer = $this->url;
 
-                        if($client->fetch( $this->url."/takelogin.php","POST","application/x-www-form-urlencoded",
-                                "username=".rawurlencode($login)."&password=".rawurlencode($password) ))
-                        {
-                                $client->setcookies();
-                                return(true);
-                        }
-                }
-                return(false);
-        }
+			if($client->fetch( $this->url."/takelogin.php","POST","application/x-www-form-urlencoded",
+				"username=".rawurlencode($login)."&password=".rawurlencode($password) ))
+			{
+				$client->setcookies();
+				return(true);
+			}
+		}
+		return(false);
+	}
 }
