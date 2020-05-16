e3d3defb5c0b452850d19b2aab62ddd5047a1c0f
mickkael <19755421+mickkael@users.noreply.github.com>
2020-05-16 22:44:35 +0800

    Add recaptcha config for cloudscraper

--- plugins/_cloudflare/conf.php
+++ plugins/_cloudflare/conf.php
@@ -4,3 +4,13 @@ if(empty($pathToExternals['python']))	// May be path already defined?
 {
 	$pathToExternals['python'] = '';// Something like /usr/bin/python. If empty, will be found in PATH.
 }
+//For using recaptcha solver plugin with cloudscaper. Needed fields in https://github.com/VeNoMouS/cloudscraper
+//Need python library: python_anticaptcha
+//
+//$recaptcha_solving_enabled = true
+//$cloudscraper_recaptcha = array(
+// "provider" => "",
+// "api_key" => "",
+// "username" => "",
+// "password" => ""
+// );
