a5f5bb811ca0203c398af0260f2aef5a9d9e6af9
Novik <novik65@gmail.com>
2020-05-18 16:44:50 +0300

    minor fix"

--- plugins/_cloudflare/conf.php
+++ plugins/_cloudflare/conf.php
@@ -5,7 +5,7 @@ if(empty($pathToExternals['python']))	// May be path already defined?
 	$pathToExternals['python'] = '';// Something like /usr/bin/python. If empty, will be found in PATH.
 }
 
-$recaptcha_solving_enabled = false
+$recaptcha_solving_enabled = false;
 
 //For using recaptcha solver plugin with cloudscaper. Needed fields in https://github.com/VeNoMouS/cloudscraper
 //Need python library: python_anticaptcha
