c92fc4708c9013e3da554adae1da71b1a9cf668b
stickz <stickman002@mail.com>
2022-09-16 12:56:10 -0400

    Integrate 'DarkBetter' & 'MaterialDesign' themes
    This commit removes the DarkBetter and MaterialDesign themes from the project as submodules. It retains the commit history for compatibility with third-party repositories.
    
    It adds the DarkBetter and MaterialDesign themes directly to the main project instead. This is required because GitHub developers have chosen not to properly support submodules.
    
    Submodules are great for developers but terrible for end users.
    
    When users click the 'Download Zip' button on the GitHub repository, the submodules do not download. This can create breakage when upgrading the ruTorrent version.
    
    Furthermore, our repository releases do not draft with the submodules included. It would be required to add and maintain third-party code to automate this process. It is not feasible to draft releases manually without the risk of human error.

---
+++ plugins/theme/themes/MaterialDesign/init.js
@@ -0,0 +1,29 @@
+/*
+ *  Material Design theme for ruTorrent
+ *  Based on "Oblivion" theme, by LesBleus
+ *  Icons from "small-n-flat" pack by paomedia ( https://github.com/paomedia/small-n-flat )
+ *  Author: Phlo
+ */ 
+
+plugin.materialdesignAllDone = plugin.allDone;
+plugin.allDone = function()
+{
+	plugin.materialdesignAllDone.call(this);
+	$.each(["diskspace","quotaspace","cpuload"], function(ndx,name)
+	{
+		var plg = thePlugins.get(name);
+		if(plg && plg.enabled)
+		{
+			plg.prgStartColor = new RGBackground("#8fbc00");
+			plg.prgEndColor = new RGBackground("#d76000");
+		}
+	});
+}
+
+plugin.oldTableCreate = dxSTable.prototype.create;
+dxSTable.prototype.create = function(ele, styles, aName)
+{
+	plugin.oldTableCreate.call(this, ele, styles, aName);
+	this.prgStartColor = new RGBackground("#d76000");
+	this.prgEndColor = new RGBackground("#8fbc00");
+}
