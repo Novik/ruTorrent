83d9ab4a006f6b25848c93fed36dc19d58b7111c
stickz <stickman002@mail.com>
2023-01-01 23:43:49 -0500

    Revert "Add support for raspberrypi unrar"
    This reverts commit c78a3c39ac085e6abb86ad4ab6001570a4f59552.

--- plugins/unpack/unrar_dir.sh
+++ plugins/unpack/unrar_dir.sh
@@ -24,40 +24,13 @@ process_directory()
 	return $ret
 }
 
-process_directory_rp()
-{
-	"$1" -x "$2." "$3"
-	last=$?
-	[ $last -ge 1 ] && [ $last -ne 10 ] && ret=$last
-	for fn in "$2"* ; do
-		if [ -d "${fn}" ] && [ ! -L "${fn}" ] ; then
-			name=$(basename "${fn}")
-			process_directory "$1" "${fn}/" "$3${name}/"
-			last=$?
-			[ $last -ge 1 ] && [ $last -ne 10 ] && ret=$last
-		fi
-	done
-	return $ret
-}
-
-if [ "$(uname -a)" = "*raspberrypi*" ] ; then
-	if [ "$6" != '' ] ; then
-		process_directory_rp "$1" "$2" "$6"
-		ret=$?
-	else
-		process_directory_rp "$1" "$2" "$3"
-		ret=$?
-	fi
+if [ "$6" != '' ] ; then
+	process_directory "$1" "$2" "$6"
+	ret=$?
 else
-	if [ "$6" != '' ] ; then
-		process_directory "$1" "$2" "$6"
-		ret=$?
-	else
-		process_directory "$1" "$2" "$3"
-		ret=$?
-	fi
+	process_directory "$1" "$2" "$3"
+	ret=$?
 fi
-
 
 if [ $ret -eq 0 ] && [ "$5" != '' ] ; then
 	OIFS=$IFS
