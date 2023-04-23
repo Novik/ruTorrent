56c22770c295e7fae4bdf9c3c818164248138dfa
TrimmingFool <lowquill@protonmail.com>
2023-04-24 00:07:20 +0200

    webui: Fix speed graph with removed tab
    Fixes: https://github.com/Novik/ruTorrent/issues/2492

--- js/webui.js
+++ js/webui.js
@@ -391,13 +391,14 @@ var theWebUI =
 			this.assignEvents();
 			this.resize();
 			this.update();
-			this.createSpeedGraph();
 		}
 	},
 	
 	createSpeedGraph: function()
 	{
-		this.speedGraph.create($("#Speed"));		
+		const speedTab = $("#Speed");
+		if (speedTab.length)
+			this.speedGraph.create(speedTab);
 	},
 
 	config: function()
@@ -464,6 +465,7 @@ var theWebUI =
 			}
 			return sorter;
 		}
+		this.createSpeedGraph();
 		const tab = theWebUI.settings['webui.selected_tab.keep'] ?
 					theWebUI.settings['webui.selected_tab.last'] : 'lcont';
 		theTabs.show(tab);
