513082b0a8d773484da8ed165d0ec2accc4cdb60
stickz <stickman002@mail.com>
2024-03-16 15:03:00 -0400

    Add internal hybrid creation of torrents
    This commit adds a feature to create hybrid torrents using the internal method. It allows the external program to continue to be used for regular torrents. The benefit is added compatibility and optimization. This feature will be enabled by default.

--- plugins/create/conf.php
+++ plugins/create/conf.php
@@ -12,3 +12,8 @@ $useExternal = false;		// Valid choices:
 $pathToCreatetorrent = '';	// Something like /bin/createtorrent, or /bin/transmissioncli. If empty, program will be found in PATH.
 
 $recentTrackersMaxCount = 15;
+
+// Sets wether to use internal realization, when the external program doesn't support hybrid torrents.
+// This option is slower, but adds compatibility for hybrid torrents, where the external program lacks support. 
+// It's recommended to enable with option and use an external program such as "mktorrent" to accelerate torrent creation.
+$useInternalHybrid = true;
