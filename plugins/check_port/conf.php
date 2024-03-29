d780e34f7d5c99d671dcc6134e6b924f6d4d8ac2
stickz <stickman002@mail.com>
2024-03-29 14:59:47 -0400

    Restore portchecker to yougetsignal

--- plugins/check_port/conf.php
+++ plugins/check_port/conf.php
@@ -1,7 +1,7 @@
 <?php
 // configuration parameter
 
-$useWebsite = "portchecker";	// Valid choices:
+$useWebsite = "yougetsignal";	// Valid choices:
 				// false - disable check_port plugin
 				// "yougetsignal" - use https://www.yougetsignal.com/tools/open-ports/
 				// "portchecker" - use https://portchecker.co/
