5d92f7630271916200f6ab8a147642e37d2dce83
jevenski <chiaotun.yienfeifei@gmail.com>
2024-06-24 08:23:09 +0800

    Use a ternary operator for conditional assignment, and fix spacing issues

--- plugins/_getdir/utils.js
+++ plugins/_getdir/utils.js
@@ -5,15 +5,11 @@ function filterDir(e) {
 	let keyword = e.currentTarget.value.toUpperCase();
 	dirs = document.querySelector(".rmenuobj").querySelectorAll("div");
 	for (let i = 0; i < dirs.length; i++) {
-		dir = dirs[i]
-    if (dir) {
-      txtValue = dir.textContent || dir.innerText;
-      if (txtValue.toUpperCase().indexOf(keyword) > -1) {
-        dirs[i].style.display = "";
-      } else {
-        dirs[i].style.display = "none";
-      }
-    }
+		dir = dirs[i];
+		if (dir) {
+			txtValue = dir.textContent || dir.innerText;
+			dirs[i].style.display = txtValue.toUpperCase().indexOf(keyword) > -1 ? "" : "none";
+		}
 	}
 }
 
