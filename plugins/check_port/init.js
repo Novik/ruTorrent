b42e996839c676818fbbc651f0316a952259746b
jevenski <chiaotun.yienfeifei@gmail.com>
2024-07-07 17:08:12 +0800

    Click IP address to reveal and hide

--- plugins/check_port/init.js
+++ plugins/check_port/init.js
@@ -14,9 +14,27 @@ plugin.update = function()
 
 plugin.getPortStatus = function(d)
 {
-	$("#port-holder").prop("title",d.ip+":"+d.port+": "+theUILang.portStatus[d.status]);
 	$("#port-status-icon").get(0).className = "pstatus"+d.status;
-	$("#port-ip-text").text(d.ip+':'+d.port);
+	$("#port-ip-text").data({ipAddr: d.ip + ":" + d.port, status: d.status});
+	this.diaplayText();
+}
+
+plugin.diaplayText = function()
+{
+	const ipAddr = $("#port-ip-text");
+	if(ipAddr.hasClass("censored")) {
+		ipAddr.text("*.*.*.*:*");
+		$("#port-holder").prop("title", `*.*.*.*:* - ${theUILang.portStatus[ipAddr.data("status")]} (${theUILang.clickReveal})`);
+	} else {
+		ipAddr.text(ipAddr.data("ipAddr"));
+		$("#port-holder").prop("title", `${ipAddr.text()} - ${theUILang.portStatus[ipAddr.data("status")]} (${theUILang.clickHide})`);
+	}
+}
+
+plugin.toggleIPAddress = function()
+{
+	$("#port-ip-text").toggleClass("censored");
+	this.diaplayText();
 }
 
 rTorrentStub.prototype.initportcheck = function()
@@ -49,7 +67,7 @@ plugin.onLangLoaded = function()
 	plugin.addPaneToStatusbar(
 		$("<div>").addClass("status-cell").attr({id: "port-holder"}).append(
 			$("<div>").attr({id: "port-status-icon"}),
-			$("<span>").addClass("stval").attr({id: "port-ip-text"}),
+			$("<span>").addClass("stval censored").attr({id: "port-ip-text"}).on("click", () => plugin.toggleIPAddress()),
 		),
 		"st_fd",
 	);
