d057537f136527e443abc835d7d40aa955da6e0e
jevenski <26022962+jevenski@users.noreply.github.com>
2024-10-04 00:04:40 +0800

    Fix spectrogram init.js indent

--- plugins/spectrogram/init.js
+++ plugins/spectrogram/init.js
@@ -1,64 +1,53 @@
 plugin.loadLang();
 
-if(plugin.canChangeMenu())
-{
+if (plugin.canChangeMenu()) {
 	plugin.createFileMenu = theWebUI.createFileMenu;
-	theWebUI.createFileMenu = function( e, id )
-	{
-		if(plugin.createFileMenu.call(this, e, id))
-		{
-			if(plugin.enabled && plugin.allStuffLoaded)
-			{
+	theWebUI.createFileMenu = function(e, id) {
+		if (plugin.createFileMenu.call(this, e, id)) {
+			if (plugin.enabled && plugin.allStuffLoaded) {
 				var fno = null;
 				var table = this.getTable("fls");
-				if((table.selCount == 1)  && (theWebUI.dID.length==40))
-				{
+				if ((table.selCount == 1)  && (theWebUI.dID.length==40)) {
 					var fid = table.getFirstSelected();
 					var ext = '';
 					var s = table.getRawValue(fid,0);
 					var pos = s.lastIndexOf(".");
-					if(pos>0)
-					{
+					if (pos>0) {
 						ext = s.substring(pos+1);
 						s = s.substring(0,pos);
 					}
 					$('#soximgfile').val(s);
-					if(this.settings["webui.fls.view"])
-					{
+					if (this.settings["webui.fls.view"]) {
 						var arr = fid.split('_f_');
 						fno = arr[1];
-					}
-					else
-					if(!this.dirs[this.dID].isDirectory(fid))
+					} else if (!this.dirs[this.dID].isDirectory(fid))
 						fno = fid.substr(3);
-					if($.inArray( ext.toLowerCase(), plugin.extensions )==-1)
+					if ($.inArray( ext.toLowerCase(), plugin.extensions )==-1)
 						fno = null;
 				}
 				theContextMenu.add( [theUILang.exsox,  (fno==null) ? null : "theWebUI.filesox('" + theWebUI.dID + "',"+fno+")"] );
 			}
-			return(true);
+			return true;
 		}
-		return(false);
+		return false;
 	}
 
-	theWebUI.filesox = function(hash,no)
-	{
-	        this.startConsoleTask( "sox", plugin.name,
-	        	{ "hash" : hash, "no" : no },
-	        	{ noclose: true } );
+	theWebUI.filesox = function(hash,no) {
+		this.startConsoleTask(
+			"sox", plugin.name,
+			{ "hash" : hash, "no" : no },
+			{ noclose: true }
+		);
 	}
 
-	plugin.onTaskShowLog = function(task,line,id,ndx)
-	{
-		if(id=='tskcmdlog')
-		{
-			if(line=="-=*=-")
-			{
+	plugin.onTaskShowLog = function(task,line,id,ndx) {
+		if (id=='tskcmdlog') {
+			if (line=="-=*=-") {
 				plugin.gotImage = true;
-				return('');
+				return '';
 			}
 		}
-		return(escapeHTML(line)+'<br>');
+		return escapeHTML(line)+'<br>';
 	}
 
 	plugin.onTaskFinished = function(task,onBackground) {
@@ -81,51 +70,39 @@ if(plugin.canChangeMenu())
 					$('#soxgetimg').trigger('submit');
 				});
 				$('.soxplay').show();
-			}
-			else
-			$('.soxplay').hide();
+			} else
+				$('.soxplay').hide();
+		}
 	}
-}
 
-plugin.onTaskShowInterface = function(task)
-{
-	plugin.gotImage = false;
-	plugin.saveConsoleSize();
-}
+	plugin.onTaskShowInterface = function(task) {
+		plugin.gotImage = false;
+		plugin.saveConsoleSize();
+	}
 
-plugin.onTaskHideInterface = function(task)
-{
-	plugin.setConsoleSize(null);
-	$('.soxplay').hide();
-	$('#tskcmdlog').removeClass('soxframe_cont');
-}
+	plugin.onTaskHideInterface = function(task) {
+		plugin.setConsoleSize(null);
+		$('.soxplay').hide();
+		$('#tskcmdlog').removeClass('soxframe_cont');
+	}
 
-plugin.saveConsoleSize = function()
-{
-	if(!plugin.consoleWidth)
-		{
+	plugin.saveConsoleSize = function() {
+		if (!plugin.consoleWidth) {
 			plugin.consoleWidth = $('#tskConsole').width();
 			plugin.deltaWidth = $('#tskConsole').width() - $('#tskcmdlog').width();
-		}
-		else
-		plugin.setConsoleSize(null);
-}
+		} else
+			plugin.setConsoleSize(null);
+	}
 
-plugin.setConsoleSize = function(img)
-{
-	if(plugin.consoleWidth &&
-		plugin.deltaWidth)
-		{
-			if(img)
-				{
-					$('#tskConsole').width(img.naturalWidth+plugin.deltaWidth+window.scrollbarWidth);
-					$('#tskcmdlog').width(img.naturalWidth+window.scrollbarWidth);
-				}
-				else
-				{
-					$('#tskcmdlog').width(plugin.consoleWidth-plugin.deltaWidth);
-					$('#tskConsole').width(plugin.consoleWidth);
-				}
+	plugin.setConsoleSize = function(img) {
+		if (plugin.consoleWidth && plugin.deltaWidth) {
+			if (img) {
+				$('#tskConsole').width(img.naturalWidth+plugin.deltaWidth+window.scrollbarWidth);
+				$('#tskcmdlog').width(img.naturalWidth+window.scrollbarWidth);
+			} else {
+				$('#tskcmdlog').width(plugin.consoleWidth-plugin.deltaWidth);
+				$('#tskConsole').width(plugin.consoleWidth);
+			}
 			theDialogManager.center('tskConsole');
 		}
 	}
