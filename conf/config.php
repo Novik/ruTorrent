90225f467b1c0858145a6b980a8ed594824df4dc
koblack <koblack@users.noreply.github.com>
2024-10-13 08:48:23 +0000

    update default user agent

--- conf/config.php
+++ conf/config.php
@@ -2,7 +2,7 @@
 	// configuration parameters
 
 	// for snoopy client
-	$httpUserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36';
+	$httpUserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36';
 	$httpTimeOut = 30;			// in seconds
 	$httpUseGzip = true;
 	$httpIP = null;				// IP string. Or null for any.
