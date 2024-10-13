913c0c8b55c7de08fb63544e9cf7c68d484c9cd1
jevenski <26022962+jevenski@users.noreply.github.com>
2024-10-14 00:04:32 +0800

    Add default options for add torrent

--- js/webui.js
+++ js/webui.js
@@ -178,6 +178,10 @@ var theWebUI =
 		"webui.show_open_status":	1,
 		"webui.show_view_panel": 1,
 		"webui.register_magnet":	0,
+		"webui.not_add_path":		0,
+		"webui.torrents_start_stopped":	0,
+		"webui.fast_resume":		0,
+		"webui.randomize_hash":	0,
 		...(() => {
 			const defaults = {};
 			const units = ['default', 'kb', 'mb', 'gb', 'tb', 'pb'];
@@ -734,8 +738,7 @@ var theWebUI =
 					if((/^webui\./).test(i))
 					{
 						needSave = true;
-						switch(i)
-						{
+						switch(i) {
 						        case "webui.effects":
 						        {
 								theDialogManager.setEffects( iv(nv)*200 );
