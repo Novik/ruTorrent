34946cf050bd31be8729fd2a4f34548c18da4334
stickz <stickman002@mail.com>
2024-11-03 08:37:01 -0500

    Bump versions for v5.1-Beta4

--- lang/langs.js
+++ lang/langs.js
@@ -110,7 +110,7 @@ function loadUILang(onLoadFunc)
 			translateDOM();
 		}
 	};
-	langScript.src = `./lang/${lang}.js?v=51B3`;
+	langScript.src = `./lang/${lang}.js?v=51B4`;
 	document.head.appendChild(langScript);
 }
 
