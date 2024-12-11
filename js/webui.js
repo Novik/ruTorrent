0ddb6afb04a3dd6ee7117628d18f2b3f5b1e20de
stickz <stickman002@mail.com>
2024-12-11 10:33:20 -0500

    Bump versions for v5.1.3

--- js/webui.js
+++ js/webui.js
@@ -5,7 +5,7 @@
 
 var theWebUI =
 {
-  	version: "5.1.2",
+  	version: "5.1.3",
 	tables:
 	{
 		trt:
--- lang/langs.js
+++ lang/langs.js
@@ -110,7 +110,7 @@ function loadUILang(onLoadFunc)
 			translateDOM();
 		}
 	};
-	langScript.src = `./lang/${lang}.js?v=512`;
+	langScript.src = `./lang/${lang}.js?v=513`;
 	document.head.appendChild(langScript);
 }
 
