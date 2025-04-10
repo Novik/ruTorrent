d4daca2bcc9db172d03557152d20ef6906cef93e
stickz <stickman002@mail.com>
2025-04-10 10:23:51 -0400

    Bump versions for 5.1.7

--- lang/langs.js
+++ lang/langs.js
@@ -110,7 +110,7 @@ function loadUILang(onLoadFunc)
 			translateDOM();
 		}
 	};
-	langScript.src = `./lang/${lang}.js?v=516`;
+	langScript.src = `./lang/${lang}.js?v=517`;
 	document.head.appendChild(langScript);
 }
 
