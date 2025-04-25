09ccb12c14e3f32d24e3be678ad4ddffa8131dac
ranirahn <34799179+ranirahn@users.noreply.github.com>
2025-04-25 20:25:36 +0300

    Create log.js

---
+++ js/log.js
@@ -0,0 +1,24 @@
+function stripTimestamp(line) {
+        return line.replace(/\[[^\]]*\]\s*/, '');
+}
+
+function fetchLogLines(initialLoad = false) {
+        fetch('php/log.php')
+                .then(response => {
+                        if (!response.ok) throw new Error('Network response was not ok');
+                        return response.text();
+                })
+                .then(text => {
+                        const lines = text.trim().split('\n');
+
+                        lines.forEach(line => {
+                                const cleanLine = stripTimestamp(line);
+                                noty(cleanLine, "info");
+                        });
+                })
+                .catch(err => {
+                        noty("Log fetch error: " + err.message,"error");
+                });
+}
+
+fetchLogLines(true);
