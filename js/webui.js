e861a4eebfe138642676e48816ff2b0800ac14d4
ranirahn <34799179+ranirahn@users.noreply.github.com>
2025-05-15 11:53:28 +0300

    Changint Log TAB to file to plugin.

--- js/webui.js
+++ js/webui.js
@@ -2546,7 +2546,6 @@ $(document).ready(function()
 	Promise.all([
 		import('./backgroundtask.js'),
 		import('./category-list.js'),
-		import('./log_history.js'),
 	]).then(([bgModule, catModule]) => {
 		theWebUI.taskAddTorrents = new bgModule.BackgroundTask();
 		theWebUI.categoryList = createCategoryList(catModule);
