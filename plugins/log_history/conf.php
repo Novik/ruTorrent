b2876c4bd8bafc83b2b0ccdc24a3cd539c404924
ranirahn <34799179+ranirahn@users.noreply.github.com>
2025-05-15 12:05:50 +0300

    Create conf.php

---
+++ plugins/log_history/conf.php
@@ -0,0 +1,3 @@
+<?php
+//max_entries is the value of now many logs will be saved. log_count is the count of logs that will be loaded on UI startup.
+$LogTab_array = ['max_entries' => 100, 'log_count' => 5];
