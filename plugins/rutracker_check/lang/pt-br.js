ccd66944cbda44e89edcf31b1e1dc284f5a24ffd
cantalupo555 <cantalupo@protonmail.com>
2025-05-25 00:22:20 -0300

    feat(i18n): Add pt-BR translations for multiple plugins (#2938)
    This commit adds Brazilian Portuguese translations for 41 ruTorrent plugins, covering core functionality like GeoIP, RSS feeds, scheduling, traffic monitoring, and other core features.
    
    The translations have been reviewed to ensure natural language and terminological consistency within the ruTorrent context.

--- plugins/rutracker_check/lang/pt-br.js
+++ plugins/rutracker_check/lang/pt-br.js
@@ -6,19 +6,19 @@
  * Author: 
  */
 
- theUILang.checkTorrent		= "Check for Update";
- theUILang.chkHdr		= "Torrent Update Check";
- theUILang.checkedAt		= "Last Checked";
- theUILang.checkedResult	= "Result";
+ theUILang.checkTorrent		= "Verificar Atualização";
+ theUILang.chkHdr		= "Verificação de Atualização do Torrent";
+ theUILang.checkedAt		= "Última Verificação";
+ theUILang.checkedResult	= "Resultado";
  theUILang.chkResults		= [
- 				  "In progress",
- 				  "Updated",
- 				  "No update required",
- 				  "Probably deleted",
- 				  "Error accessing the tracker",
- 				  "Error interacting with rTorrent",
- 				  "No need",
- 				  "Ignored"
+ 				  "Em progresso",
+ 				  "Atualizado",
+ 				  "Nenhuma atualização necessária",
+ 				  "Provavelmente excluído",
+ 				  "Erro ao acessar o rastreador",
+ 				  "Erro ao interagir com o rTorrent",
+ 				  "Não é necessário",
+ 				  "Ignorado"
  				  ];
 
 thePlugins.get("rutracker_check").langLoaded();
