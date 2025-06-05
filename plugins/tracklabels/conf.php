fa9df8d6fe2ec2a5203cf787f6db21ef6597ea21
Toha <tohenk@yahoo.com>
2025-06-05 20:29:12 +0700

    Allows to use known icons instead of duplicate it. (#2948)
    Instead of duplicate the same icon, we now can alias it to known icons.
    Both label and tracker can benefit from this behavior. As an example,
    `example.com` tracker using this domain as the website, while the tracker
    itself is served from `example.net` or `example.org` domain.
    
    ```
    <?php
    
    $labelAliases = [
      'example.com' => ['example.net', 'example.org'],
    ];
    ```
    
    Also tracker icon now updated to look for either `favicon.ico` or `favicon.png`.
    
    Signed-off-by: Toha <tohenk@yahoo.com>

---
+++ plugins/tracklabels/conf.php
@@ -0,0 +1,14 @@
+<?php
+
+// Label aliases is used:
+// 1. Map a label to already known label
+// 2. Get the favourite icon (favicon.ico or favicon.png) using provided alias
+//    instead of the tracker name
+//
+// Example:
+// $labelAliases = [
+//   'serie'       => ['anime'],
+//   'tracker.com' => ['tracker.org', 'tracker.net'],
+// ];
+$labelAliases = [
+];
