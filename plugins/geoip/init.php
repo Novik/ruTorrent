2cc676a138d1ad6d092bef4c7a7cc83176cb1704
Xirvik <community@xirvik.net>
2026-02-20 23:08:38 +0000

    Xirvik: rewrite geoip plugin for GeoIP2/MaxMindDB
    Replace legacy PECL geoip extension (broken on PHP 8+) with GeoIp2
    composer library (pure PHP, already deployed on all servers via
    0551-geoip-database).
    
    Changes:
    - conf.php: add geoip2Autoloader, geoip2CityDb, geoip2IspDb paths
    - init.php: check autoloader + city db file instead of geoip_country_code_by_name()
    - lookup.php: use GeoIp2 Database Reader for country/city/ISP lookups,
      remove deprecated utf8_encode() calls, handle IPv4/IPv6 natively

--- plugins/geoip/init.php
+++ plugins/geoip/init.php
@@ -4,7 +4,12 @@ require_once( "sqlite.php" );
 
 eval( FileUtil::getPluginConf( $plugin["name"] ) );
 
-$retrieveCountry = ($retrieveCountry && function_exists("geoip_country_code_by_name"));
+// GeoIP2: check that the autoloader and at least the city database exist
+$geoip2Available = (
+	isset($geoip2Autoloader) && file_exists($geoip2Autoloader) &&
+	isset($geoip2CityDb) && file_exists($geoip2CityDb)
+);
+$retrieveCountry = ($retrieveCountry && $geoip2Available);
 $retrieveComments = ($retrieveComments && sqlite_exists());
 
 if( $retrieveHost || $retrieveCountry || $retrieveComments )
