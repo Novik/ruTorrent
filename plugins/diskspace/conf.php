c21aecab056ffb9b900ed9caf7048b093059e877
ranirahn <34799179+ranirahn@users.noreply.github.com>
2026-05-07 21:20:13 +0300

    Enhance comments for partition directory setup
    Added comments to clarify partition directory configuration options.

--- plugins/diskspace/conf.php
+++ plugins/diskspace/conf.php
@@ -4,4 +4,18 @@ $diskUpdateInterval = 10;	// in seconds
 $notifySpaceLimit = 512;	// in Mb
 $partitionDirectory = null;	// if null, then we will check rtorrent download directory (or $topDirectory if rtorrent is unavailable)
 				// otherwise, set this to the absolute path for checked partition. 
+// To have multiple mountpoint checks add them in array like this
+//$partitionDirectory = [
+//    '/mnt/disk1',
+//    '/mnt/disk2',
+//    '/mnt/disk3'
+//];
+//
+//or like this
+//
+//$partitionDirectory = array(
+//    '/mnt/disk1',
+//    '/mnt/disk2',
+//    '/mnt/disk3'
+//);
 $freeBytesInMeter = false;	// show free space instead of %
