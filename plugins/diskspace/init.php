846d180a00e50b904aafe306e4e5f79366cb38c2
ranirahn <34799179+ranirahn@users.noreply.github.com>
2026-05-07 21:35:07 +0300

    Fix variable reference for disk space checks

--- plugins/diskspace/init.php
+++ plugins/diskspace/init.php
@@ -14,9 +14,12 @@ if( is_null($partitionDirectory) )
 		$partitionDirectory = &$topDirectory; // Else, we show $topDirectory by default as fallback
 	}
 }
+$checkDirectory = $partitionDirectory;
+if (is_array($partitionDirectory))
+	$checkDirectory = reset($partitionDirectory);
 
 if(!function_exists('disk_total_space') || !function_exists('disk_free_space') ||
-	(disk_total_space($partitionDirectory)===false) || (disk_free_space($partitionDirectory)===false))
+	(disk_total_space(checkDirectory)===false) || (disk_free_space(checkDirectory)===false))
 	$jResult .= "plugin.disable();";
 else
 {
