2f0dda747f0b5be771303f26b016b0dd428dcd19
Anthony Ryan <anthonyryan1@gmail.com>
2026-06-21 20:16:48 -0400

    Update JSON::safeEncode to sanitize, rather than attempting convert/repair
    UTF::utf8ize previously attempted to "repair" as many mangled strings
    as possible, but I've observed a number of examples over the years where
    it choked and broke rutorrent.
    
    I would argue that taking arbitrary strings of bytes, and attempting
    to convert them with perfect accuracy into valid UTF8 (when those strings
    come to us mangled, corrupt and always with unknown encodings) isn't possible.
    
    Here's a handful of examples of of byte sequences that choke the UTF8 repair code:
    
    ```php
    require('php/utility/json.php');
    var_dump(JSON::safeEncode("\xC0\x80"));
    var_dump(JSON::safeEncode("\xED\xA0\x80"));
    var_dump(JSON::safeEncode("\xED\xBF\xBF"));
    var_dump(JSON::safeEncode("\xF5\x80\x80\x80"));
    var_dump(JSON::safeEncode("\xF7\xBF\xBF\xBF"));
    ```
    
    The problem is that I think this list is nowhere near comprehensive, and
    even if we fix all of these, there will be many more corrupt sequences in
    the wild we can't predict.
    
    Instead of trying to repair all this corrupt data, let's just use the xFFFD
    replacement character. Users who add files with corrupt strings will see the
    replacement character (adding some awareness), but that becomes a content
    problem and not a "ruTorrent is broken" problem.

--- php/utility/json.php
+++ php/utility/json.php
@@ -6,7 +6,6 @@ class JSON
 {	
 	public static function safeEncode($value)
 	{
-		$encoded = json_encode($value);
-		return(!function_exists('json_last_error') || json_last_error()==JSON_ERROR_NONE ? $encoded : json_encode(UTF::utf8ize($value)));
+		return json_encode($value, JSON_THROW_ON_ERROR|JSON_INVALID_UTF8_SUBSTITUTE);
 	}
 }
