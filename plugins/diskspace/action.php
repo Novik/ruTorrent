f9c016ada2ca7c15aed98284ad2abd6e613fa3ea
ranirahn <34799179+ranirahn@users.noreply.github.com>
2026-07-13 11:54:46 +0300

    Refactor disk space handling in action.php

--- plugins/diskspace/action.php
+++ plugins/diskspace/action.php
@@ -1,28 +1,45 @@
 <?php
 	require_once( '../../php/util.php' );
 	require_once( '../../php/settings.php' );
+	require_once( dirname(__FILE__)."/disk.php" );
 	eval( FileUtil::getPluginConf( 'diskspace' ) );
 
-	if( is_null($partitionDirectory) )
+	if( !isset($partitionDirectory) || is_null($partitionDirectory) )
 	{
 		// If we run locally && we the download directory seems to exists
 		if ( User::isLocalMode() && rTorrentSettings::get()->linkExist && file_exists(rTorrentSettings::get()->directory) ) 
 		{
-			$partitionDirectory = rTorrentSettings::get()->directory; // Then we can show the disk space of the download directory
+			$partitionDirectory = array( rTorrentSettings::get()->directory ); // Then we can show the disk space of the download directory
 		} 
 		else 
 		{
-			$partitionDirectory = &$topDirectory; // Else, we show $topDirectory by default as fallback
+			$partitionDirectory = array( $topDirectory); // Else, we show $topDirectory by default as fallback
 		}
 	}
-	$ret = array
-	(
-		"total" => 0,
-		"free" => 0
-	);
-	if( is_dir($partitionDirectory) )
+	elseif( !is_array($partitionDirectory) )
 	{
-		$ret["total"] = disk_total_space($partitionDirectory);
-		$ret["free"] = disk_free_space($partitionDirectory);
+		$partitionDirectory = array($partitionDirectory);
 	}
+
+	$diskSpace = rDiskSpace::load($partitionDirectory);
+
+	$current = $diskSpace->updateOldest();
+
+	$ret = array(
+		"total" => 0,
+		"free" => 0,
+		"path" => "",
+		"updated" => 0,
+		"all" => array()
+	);
+
+	if( $current )
+	{
+		$ret["total"] = $current["total"];
+		$ret["free"] = $current["free"];
+		$ret["path"] = $current["path"];
+		$ret["updated"] = $current["updated"];
+	}
+
+	$ret["all"] = $diskSpace->getAll();
 	CachedEcho::send(JSON::safeEncode($ret),"application/json");
