b45ed33036b45e3b12b85736901eeeee2bc8bdd7
xirvik <community@xirvik.net>
2026-08-18 17:34:41 +0000

    Bump version to 5.3.12

--- lang/langs.js
+++ lang/langs.js
@@ -110,7 +110,7 @@ function loadUILang(onLoadFunc)
 			translateDOM();
 		}
 	};
-	langScript.src = `./lang/${lang}.js?v=5311`;
+	langScript.src = `./lang/${lang}.js?v=5312`;
 	document.head.appendChild(langScript);
 }
 
