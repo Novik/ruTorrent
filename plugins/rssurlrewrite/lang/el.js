e26b04dab1bbc6d5e163bec71aac681dc275f479
ChriZathens <c_kan1@hotmail.com>
2026-09-01 23:02:24 +0300

    Update el.js

--- plugins/rssurlrewrite/lang/el.js
+++ plugins/rssurlrewrite/lang/el.js
@@ -8,7 +8,7 @@
 
  theUILang.rssNewRule		= "Νέος κανόνας";
  theUILang.mnu_rssurlrewrite	= "Αντικατάσταση URL στα RSS";
- theUILang.rssRulesManager	= "Διαχειριστής Κανόνων RSS";
+ theUILang.rssRulesManager	= "Διαχείριση Κανόνων RSS";
  theUILang.rssAddRule		= "Προσθήκη";
  theUILang.rssDelRule		= "Διαγραφή";
  theUILang.rssCheckRule		= "Δοκιμή";
