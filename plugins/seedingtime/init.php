d894345f1092f1e55962ac2035c4e6a6d23ca128
Rid <rid@cylo.io>
2026-09-24 22:05:41 +0100

    Fix Finished time for complete-data loads on rTorrent 0.16

--- plugins/seedingtime/init.php
+++ plugins/seedingtime/init.php
@@ -7,8 +7,9 @@ $req = new rXMLRPCRequest( array(
 		getCmd('d.set_custom').'=addtime,"$'.getCmd('execute_capture').'={date,+%s}"')),
 
 	$theSettings->getOnHashdoneCommand(array("seedingtimecheck".User::getUser(),
-		getCmd('branch=').'$'.getCmd('not=').'$'.getCmd('d.get_complete=').',,'.
-		getCmd('d.get_custom').'=seedingtime,,"'.getCmd('d.set_custom').'=seedingtime,$'.getCmd('d.get_custom').'=addtime'.'"')),
+		getCmd('branch=').getCmd('d.get_complete=').',"'.
+		getCmd('branch=').getCmd('d.get_custom').'=seedingtime,,\"'.
+		getCmd('d.set_custom').'=seedingtime,$'.getCmd('d.get_custom').'=addtime\""')),
 	));
 if($req->success())
         $theSettings->registerPlugin($plugin["name"],$pInfo["perms"]);
